39231a20fff9faa74b595e4dfced6399f2b4c309
Sam Aaron <samaaron@gmail.com>
2014-01-28 07:05:04 +0000

    Fix up server->ws comms.

--- app/scripts/ws.rb
+++ app/scripts/ws.rb
@@ -84,20 +84,47 @@ class RcvDispatch
 end
 
 $rd = RcvDispatch.new($sp, ws_out)
+$clients = []
+
+Thread.new do
+  loop do
+    begin
+      message = ws_out.pop
+      if debug_mode
+        raise "message not a Hash!" unless message.is_a? Hash
+      end
+      message[:ts] = Time.now.strftime("%H:%M:%S")
+
+      if debug_mode
+        puts "sending:"
+        puts "#{message.to_edn}"
+        puts "---"
+      end
+      $clients.each{|c| c.send(message.to_edn)}
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.inspect
+    end
+  end
+end
 
 server = Rubame::Server.new("0.0.0.0", 25252)
+
 while true
   server.run do |client|
     client.onopen do
       client.send({:type => :message, :val => "Connection initiated..."}.to_edn)
-      puts "Server reports:  client open"
+      $clients << client
+      puts "New Websocket Client"
+
     end
     client.onmessage do |msg|
       puts "====> #{msg}" if debug_mode
       $rd.dispatch EDN.read(msg)
     end
     client.onclose do
-        warn("Connection closed...")
+      $clients.delete client
+      warn("Connection closed...")
     end
   end
 end
