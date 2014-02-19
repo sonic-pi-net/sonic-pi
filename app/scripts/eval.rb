e2e2657fd2b2357eeed6f014c73c40bec1e4e1c0
Sam Aaron <samaaron@gmail.com>
2014-02-19 23:07:54 +0000

    Add simple eval program - which takes a Sonic Pi rb file and executes it.

---
+++ app/scripts/eval.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+require_relative "core.rb"
+
+require_relative "sonicpi/scsynth"
+require_relative "sonicpi/studio"
+require_relative "sonicpi/spider"
+require_relative "sonicpi/server"
+require_relative "sonicpi/util"
+require_relative "sonicpi/rcv_dispatch"
+
+Thread.abort_on_exception=true
+
+include SonicPi::Util
+
+ws_out = Queue.new
+$scsynth = SonicPi::SCSynth.instance
+
+$c = OSC::Client.new("localhost", 4556)
+$c.send(OSC::Message.new("/d_loadDir", synthdef_path))
+sleep 2
+
+$sp =  SonicPi::Spider.new "localhost", 4556, ws_out, 5
+
+$rd = SonicPi::RcvDispatch.new($sp, ws_out)
+$clients = []
+
+# Send stuff out from Sonic Pi jobs out to GUI
+out_t = Thread.new do
+  continue = true
+  while continue
+    begin
+      message = ws_out.pop
+      message[:ts] = Time.now.strftime("%H:%M:%S")
+
+      if message[:type] == :exit
+        continue = false
+      else
+        puts message
+      end
+    rescue Exception => e
+      puts "Exception!"
+      puts e.message
+      puts e.backtrace.inspect
+    end
+  end
+end
+
+$rd.dispatch({:cmd => "run-code",
+               :val => ARGF.read})
+
+
+at_exit do
+  $c.send(OSC::Message.new("/quit"))
+end
+
+
+out_t.join
