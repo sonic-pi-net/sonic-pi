fbcd4a5dce279848d2379a7c0763832b7f2b5f30
Sam Aaron <samaaron@gmail.com>
2014-02-26 10:05:05 +0000

    Simplify blue monday example by using with_synth_defaults

--- etc/examples/blue_monday.rb
+++ etc/examples/blue_monday.rb
@@ -17,16 +17,16 @@ end
 
 def synths
   with_synth "saw_beep"
+  with_synth_defaults :amp, 0.5, :attack, 0.01, :release, 0.75, :cutoff, 130
   notes = [:F, :C, :D, :D, :G, :C, :D, :D]
   notes.each do |n|
     2.times do
-      play note(n, 1), :amp, 0.5, :attack, 0.01, :release, 0.5
-      play note(n, 2), :amp, 0.5, :attack, 0.01, :release, 0.75
-
+      play note(n, 1)
+      play note(n, 2)
       sleep 0.25
-      play note(n, 2), :amp, 0.5, :attack, 0.01, :release, 0.5
-      play note(n, 3), :amp, 0.5, :attack, 0.01, :release, 0.75
 
+      play note(n, 2)
+      play note(n, 3)
       sleep 0.25
     end
   end
