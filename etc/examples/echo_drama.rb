3ea2049bc8b5e2c05bb7bf0904b92a6ca85642bc
Sam Aaron <samaaron@gmail.com>
2014-03-19 18:53:46 +0000

    Allow easy rate change in echo_drama example

--- etc/examples/echo_drama.rb
+++ etc/examples/echo_drama.rb
@@ -1,8 +1,11 @@
-with_synth "prophet"
+use_synth "tb303"
 
-with_fx(:echo, :delay, 0.5, :decay, 4) do
+rate = 0.7
+rate_mul = 1.0 / rate
+
+with_fx(:echo, :delay, 0.5 * rate_mul, :decay, 4) do
   loop do
-    play chord([:b1, :b2, :e1, :e2, :b3, :e3].choose, :minor).choose, :cutoff, rrand(40, 100), :amp, 0.5
-    sleep [0.25, 0.5, 1].choose
+    play chord([:b1, :b2, :e1, :e2, :b3, :e3].choose, :minor).choose, :cutoff, rrand(40, 100), :amp, 0.5, :attack, 0, :release, 2
+    sleep [0.25, 0.5, 0.5, 0.5, 1, 1].choose * rate_mul
   end
 end
