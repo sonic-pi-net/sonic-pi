2a94bd2ab1c772b26d3c74a18bf13e41779facd2
Sam Aaron <samaaron@gmail.com>
2014-03-23 16:09:17 +0000

    Meta-glasses from our hero Why to help us see more clearly...

--- app/scripts/core.rb
+++ app/scripts/core.rb
@@ -136,3 +136,21 @@ class Array
     self.sample
   end
 end
+
+
+# Meta-glasses from our hero Why to help us
+# see more clearly..
+class Object
+  # The hidden singleton lurks behind everyone
+  def metaclass; class << self; self; end; end
+
+  def meta_eval &blk; metaclass.instance_eval &blk; end
+  # Adds methods to a metaclass
+  def meta_def name, &blk
+    meta_eval { define_method name, &blk }
+  end
+  # Defines an instance method within a class
+  def class_def name, &blk
+    class_eval { define_method name, &blk }
+  end
+end
