78189ecc65e4255d43c083b0b93eb39a740a304f
Sam Aaron <samaaron@gmail.com>
2014-03-28 11:24:07 +0000

    Wooh, look, no parens!

--- etc/examples/jungle.rb
+++ etc/examples/jungle.rb
@@ -1,10 +1,10 @@
-with_fx(:lpf, cutoff: 90) do
-  with_fx(:reverb, mix: 0.5) do
-    with_fx(:compressor, amp: 40) do
-      with_fx(:distortion, distort: 0.4) do
+with_fx :lpf, cutoff: 90 do
+  with_fx :reverb, mix: 0.5 do
+    with_fx :compressor, amp: 40 do
+      with_fx :distortion, distort: 0.4 do
         loop do
           sample :loop_amen, rate: 1.35
-          sleep sample_duration(:loop_amen, rate: 1.35)
+          sleep sample_duration :loop_amen, rate: 1.35
         end
       end
     end
