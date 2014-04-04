3c832985d4cf93465f6824424054773203807b88
Sam Aaron <samaaron@gmail.com>
2014-04-04 21:47:50 +0100

    Add fm noise example

---
+++ etc/examples/fm_noise.rb
@@ -0,0 +1,6 @@
+use_synth :fm
+loop do
+  p = play chord(:Eb3, :minor).choose - [0, 12, -12].choose, :divisor, 0.01, :div_slide, rrand(100), :depth, rrand(0.001, 2), :attack, 0.01, :release, rrand(5), :amp, 0.5
+  p.ctl :divisor, rrand(0.001, 50)
+  sleep [0.25, 0.125].choose
+end
