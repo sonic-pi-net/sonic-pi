423be726ddaa5f6d85fba6007f916baf0a482d9d
Sam Aaron <samaaron@gmail.com>
2014-04-05 23:08:51 +0100

    Add new example - ambient:
    * takes advantage of new sample discovery functions

---
+++ etc/examples/ambient.rb
@@ -0,0 +1,11 @@
+load_samples(sample_names :ambi)
+
+with_fx :reverb, mix: 0.3 do
+  loop do
+    s_name = choose sample_names :ambi
+    s_time = rrand_i 1, 2
+    s = sample s_name, rate: choose([0.5, 1]), pan: rrand(-1, 1), pan_slide: s_time
+    control s, pan: rrand(-1, 1)
+    sleep s_time
+  end
+end
