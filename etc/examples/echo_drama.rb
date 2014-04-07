aa45d4b3eaba16b36fc83c5677de1ba750e789aa
Sam Aaron <samaaron@gmail.com>
2014-04-07 23:18:33 +0100

    fix minor syntax error

--- etc/examples/echo_drama.rb
+++ etc/examples/echo_drama.rb
@@ -5,7 +5,7 @@ rate_mul = 1.0 / rate
 
 with_fx(:echo, delay: 0.5 * rate_mul, decay: 4) do
   loop do
-    play chord([:b1, :b2, :e1, :e2, :b3, :e3].choose, :minor).choose, cutoff: rrand(40, 100), amp: 0.5, attack: 0, release:, 2
+    play chord([:b1, :b2, :e1, :e2, :b3, :e3].choose, :minor).choose, cutoff: rrand(40, 100), amp: 0.5, attack: 0, release: 2
     sleep [0.25, 0.5, 0.5, 0.5, 1, 1].choose * rate_mul
   end
 end
