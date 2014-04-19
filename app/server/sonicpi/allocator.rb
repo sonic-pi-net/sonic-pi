1e0c2d8faf40c2f16ec5b73da7e23586339e0cd5
Sam Aaron <samaaron@gmail.com>
2014-04-19 14:46:55 +0100

    Extract out allocator from bus allocator:
    * Remove all bus specific behaviour - now allocator is a generic id
      allocator to be used in other places such as buffer id allocation

---
+++ app/server/sonicpi/allocator.rb
@@ -0,0 +1,76 @@
+#--
+# This file is part of Sonic Pi: http://sonic-pi.net
+# Full project source: https://github.com/samaaron/sonic-pi
+# License: https://github.com/samaaron/sonic-pi/blob/master/LICENSE.md
+#
+# Copyright 2013, 2014 by Sam Aaron (http://sam.aaron.name).
+# All rights reserved.
+#
+# Permission is granted for use, copying, modification, distribution,
+# and distribution of modified versions of this work as long as this
+# notice is included.
+#++
+require_relative "atom"
+require "hamster/vector"
+
+module SonicPi
+  class AllocationError < Exception ; end
+  class Allocator
+    attr_reader :idx_offset, :max_id
+    def initialize(max_id, idx_offset)
+      @idx_offset = idx_offset
+      @max_id = max_id
+      tmp_ids = [false] * @max_id
+      @ids_A = Atom.new(Hamster.vector(*tmp_ids))
+      @num_allocations = 0
+    end
+
+    def allocate(num_adj_ids)
+      idx = nil
+      ids = @ids_A.swap! do |ids|
+        idx = find_gap(0, num_adj_ids, ids)
+        new_ids = (idx...idx+num_adj_ids).reduce(ids) {|is, i| is.set(i, true)}
+      end
+      new_id = idx + @idx_offset
+      @num_allocations += num_adj_ids
+      new_id
+    end
+
+    def release!(idx, num_adj_ids)
+      @ids_A.swap! do |ids|
+        (idx...idx+num_adj_ids).reduce(ids) {|is, i| is.set(i, false)}
+      end
+      @num_allocations -= num_adj_ids
+    end
+
+    def reset!
+      old_ids = @ids_A.swap_returning_old! {|bs| Hamster.vector}
+      old_ids.each {|b| b.free}
+      @num_busses_allocated = 0
+      @busses_A.reset! Hamster.vector
+    end
+
+    def num_allocations
+      @num_allocations
+    end
+
+    private
+
+    def valid_gap?(idx, gap_size, ids)
+      seg = ids.to_a[idx...idx+gap_size]
+      seg.all?{|el| !el}
+    end
+
+    def find_gap(idx, gap_size, ids)
+      while idx < @max_id
+        if valid_gap?(idx, gap_size, ids)
+          return idx
+        else
+          idx += 1
+        end
+      end
+
+      raise AllocationError, "Unable to allocate bus"
+    end
+  end
+end
--- app/server/sonicpi/busallocator.rb
+++ app/server/sonicpi/busallocator.rb
@@ -12,32 +12,21 @@
 #++
 require_relative "bus"
 require_relative "atom"
+require_relative "allocator"
 require "hamster/vector"
 
 module SonicPi
-  class BusAllocationError < Exception ; end
-  class BusAllocator
+  class BusAllocator < Allocator
     def initialize(max_bus_id, idx_offset)
-      @IDX_OFFSET = idx_offset
-      @MAX_BUS_ID = max_bus_id
-      tmp_ids = [false] * @MAX_BUS_ID
-      @bus_ids_A = Atom.new(Hamster.vector(*tmp_ids))
+      super
       @busses_A = Atom.new(Hamster.vector)
-      @num_busses_allocated = 0
     end
 
     def allocate(num_adj_busses)
-      idx = nil
-      ids = @bus_ids_A.swap! do |bids|
-        idx = find_gap(0, num_adj_busses, bids)
-        new_bus_ids = (idx...idx+num_adj_busses).reduce(bids) {|bs, i| bs.set(i, true)}
-      end
-
-      offsetted = idx + @IDX_OFFSET
-      bus = bus_class.new(offsetted, num_adj_busses, self){|id, size| release!(id-@IDX_OFFSET, size)}
+      new_id = super(num_adj_busses)
+      bus = bus_class.new(new_id, num_adj_busses, self){|id, size| release!(id-self.idx_offset, size)}
 
       @busses_A.swap! {|bs| bs.add(bus)}
-      @num_busses_allocated += num_adj_busses
       bus
     end
 
@@ -46,45 +35,20 @@ module SonicPi
     end
 
     def reset!
+      super
       old_busses = @busses_A.swap_returning_old! {|bs| Hamster.vector}
       old_busses.each {|b| b.free}
-      @num_busses_allocated = 0
       @busses_A.reset! Hamster.vector
     end
 
     def num_busses_allocated
-      @num_busses_allocated
+      num_allocations
     end
 
     private
 
     def bus_class
       raise "Implement me!"
-    end
-
-    def release!(idx, num_adj_busses)
-      @bus_ids_A.swap! do |bids|
-        (idx...idx+num_adj_busses).reduce(bids) {|bs, i| bs.set(i, false)}
-      end
-      @num_busses_allocated -= num_adj_busses
-    end
-
-    def valid_gap?(idx, gap_size, bids)
-      bus_seg = bids.to_a[idx...idx+gap_size]
-      bus_seg.all?{|el| !el}
-    end
-
-    def find_gap(idx, gap_size, bids)
-
-      while idx < @MAX_BUS_ID
-        if valid_gap?(idx, gap_size, bids)
-          return idx
-        else
-          idx += 1
-        end
-      end
-
-      raise BusAllocationError, "Unable to allocate bus"
     end
   end
 end
