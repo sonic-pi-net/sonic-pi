700e585d87fd25f513558419edcc9fc94018f8ea
Sam Aaron <samaaron@gmail.com>
2014-04-26 09:05:35 +0100

    add handy helper method for debugging: __describe_threads:
    * returns a list of all threads mapped to their thread group name

--- app/server/sonicpi/spider.rb
+++ app/server/sonicpi/spider.rb
@@ -212,6 +212,10 @@ module SonicPi
 
     end
 
+    def __describe_threads
+      __message Thread.list.map{|t| t.thread_variable_get(:sonic_pi_thread_group)}
+    end
+
     private
 
     def reg_job(job_id, t)
