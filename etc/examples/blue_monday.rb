addb1b654eef1bd62158b20520346cce54d3b472
Sam Aaron <samaaron@gmail.com>
2014-04-30 23:04:59 +0100

    use symbol for synth name rather than string

--- etc/examples/blue_monday.rb
+++ etc/examples/blue_monday.rb
@@ -16,7 +16,7 @@ define :snare do
 end
 
 define :synths do
-  use_synth "saw_beep"
+  use_synth :saw_beep
   use_synth_defaults amp: 0.5, attack: 0.01, release: 0.75, cutoff: 130
   notes = [:F, :C, :D, :D, :G, :C, :D, :D]
   notes.each do |n|
