770a11939f764f765f20862cb81ce86ba5b583e2
Sam Aaron <samaaron@gmail.com>
2014-05-19 17:49:05 +0100

    Display synth names as symbols

--- app/server/sonicpi/mods/sound.rb
+++ app/server/sonicpi/mods/sound.rb
@@ -1143,7 +1143,7 @@ set_volume! 2 # Set the main system volume to 2",
          end
 
          unless Thread.current.thread_variable_get(:sonic_pi_mod_sound_synth_silent)
-           __delayed_message "play #{synth_name.inspect}, #{arg_h_pp(args_h)}"
+           __delayed_message "synth #{synth_name.to_sym.inspect}, #{arg_h_pp(args_h)}"
          end
          trigger_synth(synth_name, args_h, group, validation_fn)
        end
