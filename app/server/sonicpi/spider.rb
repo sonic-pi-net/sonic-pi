a2542e353944bc9f8e3cd774c8e1498e3ffca08b
Sam Aaron <samaaron@gmail.com>
2014-05-29 19:45:33 +0100

    inform user when run has completed

--- app/server/sonicpi/spider.rb
+++ app/server/sonicpi/spider.rb
@@ -299,6 +299,7 @@ module SonicPi
           @user_jobs.job_completed(id)
 
           @events.event("/job-completed", {:id => id, :thread => job})
+          __info "Finished run #{id}"
           deregister_job_and_return_subthreads(id)
           @msg_queue.push({type: :job, jobid: id, action: :completed, jobinfo: info})
         rescue Exception => e
