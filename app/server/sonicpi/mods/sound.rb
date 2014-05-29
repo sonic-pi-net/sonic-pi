a3e5432bc0f45c2a28f1ad93c7d70d1011c200eb
Sam Aaron <samaaron@gmail.com>
2014-05-29 22:18:49 +0100

    Further docstring work

--- app/server/sonicpi/mods/sound.rb
+++ app/server/sonicpi/mods/sound.rb
@@ -463,11 +463,63 @@ play_pattern [:d3, :c1, :Eb5], amp: 0.5, cutoff: 90"]
          notes.each_with_index{|note, idx| play(note, *args) ; sleep(times[idx % times.size])}
        end
        doc name:          :play_pattern_timed,
-           doc:           "Play list of notes one after another with specified times",
+           doc:           "Play each note in a list of notes one after another with specified times between them. The notes should be a list of MIDI numbers or symbols such as :E4 - identical to the first parameter of the play function. The times should be a list of times between the notes in seconds.
+
+If the list of times is smaller than the number of gaps between notes, the list is repeated again. If the list of times is longer than the number of gaps between notes, then some of the times are ignored. See examples for more detail.
+
+Accepts optional args for modification of the synth being played. See each synth's documentation for synth-specific opts. See use_synth and with_synth for changing the current synth.",
            args:          [[:notes, :list], [:times, :list]],
            opts:          DEFAULT_PLAY_OPTS,
            accepts_block: false,
-           examples:      []
+           examples:      ["
+play_pattern_timed [40, 42, 44, 46], [1, 2, 3]
+
+# same as:
+
+play 40
+sleep 1
+play 42
+sleep 2
+play 44
+sleep 3
+play 46",
+
+"play_pattern_timed [40, 42, 44, 46, 49], [1, 0.5]
+
+# same as:
+
+play 40
+sleep 1
+play 42
+sleep 0.5
+play 44
+sleep 1
+play 46
+sleep 0.5
+play 49",
+
+"play_pattern_timed [40, 42, 44, 46], [0.5]
+
+# same as:
+
+play 40
+sleep 0.5
+play 42
+sleep 0.5
+play 55
+sleep 0.5",
+
+"play_pattern_timed [40, 42, 44], [1, 2, 3, 4, 5]
+
+#same as:
+
+play 40
+sleep 1
+play 42
+sleep 2
+play 44"]
+
+
 
 
        def play_chord(notes, *args)
@@ -477,11 +529,30 @@ play_pattern [:d3, :c1, :Eb5], amp: 0.5, cutoff: 90"]
          trigger_chord(synth_name, shifted_notes, args)
        end
        doc name:          :play_chord,
-           doc:           "add docs",
+           doc:           "Play a list of notes at the same time.
+
+Accepts optional args for modification of the synth being played. See each synth's documentation for synth-specific opts. See use_synth and with_synth for changing the current synth.",
            args:          [[:notes, :list]],
            opts:          DEFAULT_PLAY_OPTS,
            accepts_block: false,
-           examples:      []
+           examples:      ["
+play_chord [40, 45, 47]
+
+# same as:
+
+play 40
+play 45
+play 47",
+
+"play_chord [40, 45, 47], amp: 0.5
+
+# same as:
+
+play 40, amp: 0.5
+play 45, amp: 0.5
+play 47, amp: 0.5",
+
+"play_chord chord(:e3, :minor)"]
 
 
        def repeat(&block)
