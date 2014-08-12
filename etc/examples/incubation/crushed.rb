5168068b4508357f556c897fde2dbf1e0c8dca99
Sam Aaron <samaaron@gmail.com>
2014-08-12 13:06:09 +0100

    add new incubation example using bitcrusher FX - need to test on RP

---
+++ etc/examples/incubation/crushed.rb
@@ -0,0 +1,10 @@
+with_fx :bitcrusher do |b|
+  loop do
+    use_synth :mod_fm
+    play 50 + [5, 0].choose, mod_phase: 0.25, release: 1, mod_range: [24, 27, 12].choose
+    sleep 0.5
+    use_synth :mod_dsaw
+    play 50, mod_phase: 0.25, release: 1.5, mod_range: [24, 27].choose, attack: 0.5
+    sleep 1.5
+  end
+end
