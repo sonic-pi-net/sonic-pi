a3dc88ae1451d39cf8a7fce8b7f88ffbe599d4fc
Sam Aaron <samaaron@gmail.com>
2014-08-14 22:32:44 +0100

    Another incubation example…

---
+++ etc/examples/incubation/wob_rhyth.rb
@@ -0,0 +1,21 @@
+with_fx :reverb do
+  in_thread do
+    loop do
+      r = [0.5, 1.0/3, 3.0/5].choose
+      8.times do
+        sample :ambi_choir, rate: r, pan: rrand(-1, 1)
+        sleep 0.5
+      end
+    end
+  end
+end
+
+with_fx :wobble, phase: 2 do
+  with_fx :echo, mix: 0.6 do
+    loop do
+      sample :drum_heavy_kick
+      sample :bass_hit_c, rate: 0.8, amp: 0.4
+      sleep 1
+    end
+  end
+end
