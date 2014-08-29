99aa3abc6b7550eaa1fca89e7a617e0d51c40b92
Sam Aaron <samaaron@gmail.com>
2014-08-29 21:17:23 +0100

    update blip_ryhthm example

--- etc/examples/incubation/blip_rhythm.rb
+++ etc/examples/incubation/blip_rhythm.rb
@@ -2,12 +2,34 @@ load_samples [:drum_heavy_kick, :elec_plip, :elec_blip]
 use_bpm 100
 
 with_fx :reverb do
-  loop do
-    with_fx [:echo, :none].choose, room: 0.8, phase: 0.25, mix: 0.8 do
+  in_thread do
+    with_fx [:echo].choose, room: 0.8, decay: 8, phase: 1, mix: 0.4 do
+      loop do
+
+        n = [:e2, :e2, :a3].choose
+        with_synth :dsaw do
+          with_transpose -12 do
+            play n, attack: 0.4, release: 0.8, detune: rrand(0, 0.1)
+          end
+        end
+
+        sleep 4
+
+        with_synth :tri do
+          play chord(n, :m7), amp: 0.6, release: 0.8
+        end
+
+        sleep 2
+      end
+    end
+  end
+
+
+  with_fx [:echo].choose, room: 0.8, decay: 8, phase: 0.25, mix: 0.4 do
+    loop do
       sample :drum_heavy_kick, amp: 0.5
       sample :elec_plip, rate: [0.5, 2, 1, 4].choose * [1, 2, 3, 10].choose, amp: 0.6
-      sample :elec_blip, rate: [0.5, 1, 2].choose, amp: 0.8
-      sleep 0.5
+      sleep 2
     end
   end
 end
