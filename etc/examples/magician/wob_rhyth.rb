0dc916a0d63e0860bb00cedc0ca8cdf2925fc4cf
Sam Aaron <samaaron@gmail.com>
2014-09-02 13:11:40 +0100

    use correct indentation

--- etc/examples/magician/wob_rhyth.rb
+++ etc/examples/magician/wob_rhyth.rb
@@ -16,7 +16,7 @@ with_fx :wobble, phase: 2 do |w|
       sample :drum_heavy_kick
       sample :bass_hit_c, rate: 0.8, amp: 0.4
       sleep 1
-#control w, phase: [0.5, 1, 2].choose
+      #control w, phase: [0.5, 1, 2].choose
     end
   end
 end
