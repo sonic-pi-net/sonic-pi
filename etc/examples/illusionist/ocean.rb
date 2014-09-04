da34c594942768a200d0bc817a270fcab9105f7c
Sam Aaron <samaaron@gmail.com>
2014-09-04 09:15:49 +0100

    improve ocean sounds :-)

--- etc/examples/illusionist/ocean.rb
+++ etc/examples/illusionist/ocean.rb
@@ -1,7 +1,7 @@
 with_fx :reverb, mix: 0.5 do
   loop do
-    s = synth [:bnoise, :cnoise, :gnoise].choose, attack: rrand(0, 1), sustain: rrand(0.5, 5), cutoff: rrand(60, 100), pan: rrand(-1, 1), pan_slide: 1, amp: rrand(0.5, 1)
-    control s, pan: rrand(-1, 1)
+    s = synth [:bnoise, :cnoise, :gnoise].choose, amp: rrand(0.5, 1.5), attack: rrand(0, 4), sustain: rrand(0, 2), release: rrand(0, 5), cutoff_slide: rrand(0, 5), cutoff: rrand(60, 100), pan: rrand(-1, 1), pan_slide: 1, amp: rrand(0.5, 1)
+    control s, pan: rrand(-1, 1), cutoff: rrand(60, 110)
     sleep rrand(2, 4)
   end
 end
