387dc86e3a66532af7bf284775b307bf513cb049
Sam Aaron <samaaron@gmail.com>
2014-09-08 22:12:19 +0100

    only add method after we’ve checked to see if it exists

--- app/server/sonicpi/lib/sonicpi/spiderapi.rb
+++ app/server/sonicpi/lib/sonicpi/spiderapi.rb
@@ -112,12 +112,12 @@ play bar # plays 80"]
 
     def define(name, &block)
       raise "define must be called with a code block" unless block
-      @user_methods.send(:define_method, name, &block)
       if @user_methods.method_defined? name
         __info "Redefining #{name}"
       else
         __info "Defining #{name}"
       end
+      @user_methods.send(:define_method, name, &block)
     end
     doc name:           :define,
         introduced:     Version.new(2,0,0),
