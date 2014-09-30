ef2eea8a4fdf17138c056b8308e1c6e5d8381864
jweather <jweather@xidus.net>
2014-09-30 16:57:36 -0400

    Windows build support work in progress

---
+++ app/gui/qt/rp-build-windows.bat
@@ -0,0 +1,9 @@
+cd %~dp0
+
+copy /Y ruby_help.tmpl ruby_help.h
+ruby ../../server/bin/qt-doc.rb -o ruby_help.h
+qmake -o Makefile SonicPi.pro
+nmake
+nmake install
+cd release
+windeployqt Sonic-Pi.exe -printsupport
