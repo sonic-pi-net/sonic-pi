821fd20841ae6e3e9687bc9d6ba461f98a91eedb
Joseph Wilk <joe@josephwilk.net>
2014-10-02 14:27:21 +0200

    live_loop supports loop increment count

--- app/server/sonicpi/lib/sonicpi/spiderapi.rb
+++ app/server/sonicpi/lib/sonicpi/spiderapi.rb
@@ -24,9 +24,15 @@ module SonicPi
       define(name, &block)
 
       in_thread(name: name) do
+        inc = 0
         loop do
           cue name
-          send(name)
+          if self.method(name).arity == 1
+            send(name, inc)
+          else
+            send(name)
+          end
+          inc += 1
         end
       end
     end
