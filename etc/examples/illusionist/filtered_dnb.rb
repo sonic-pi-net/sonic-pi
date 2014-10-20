292b0743e302fe58e130a92a3b29050512545e3d
Sam Aaron <samaaron@gmail.com>
2014-10-20 15:52:32 +0100

    Fix slide timing in examples given new slide semantics

--- etc/examples/illusionist/filtered_dnb.rb
+++ etc/examples/illusionist/filtered_dnb.rb
@@ -1,8 +1,8 @@
-with_fx(:rlpf, cutoff: 10, cutoff_slide: 20) do |c|
+with_fx(:rlpf, cutoff: 10, cutoff_slide: 4) do |c|
   loop do
     sample :bass_dnb_f, amp: 5
     sample :loop_amen, amp: 5
     sleep sample_duration :loop_amen
-    control c, cutoff: rrand(40, 120), cutoff_slide: rrand(1, 20)
+    control c, cutoff: rrand(40, 120), cutoff_slide: rrand(1, 4)
   end
 end
--- etc/examples/illusionist/ocean.rb
+++ etc/examples/illusionist/ocean.rb
@@ -1,6 +1,6 @@
 with_fx :reverb, mix: 0.5 do
   loop do
-    s = synth [:bnoise, :cnoise, :gnoise].choose, amp: rrand(0.5, 1.5), attack: rrand(0, 4), sustain: rrand(0, 2), release: rrand(0, 5), cutoff_slide: rrand(0, 5), cutoff: rrand(60, 100), pan: rrand(-1, 1), pan_slide: 1, amp: rrand(0.5, 1)
+    s = synth [:bnoise, :cnoise, :gnoise].choose, amp: rrand(0.5, 1.5), attack: rrand(0, 4), sustain: rrand(0, 2), release: rrand(1, 5), cutoff_slide: rrand(0, 5), cutoff: rrand(60, 100), pan: rrand(-1, 1), pan_slide: rrand(1, 5), amp: rrand(0.5, 1)
     control s, pan: rrand(-1, 1), cutoff: rrand(60, 110)
     sleep rrand(2, 4)
   end
