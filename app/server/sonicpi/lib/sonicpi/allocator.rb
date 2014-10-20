068a44a80d9ed3451f365a00520fee0266384cf4
Sam Aaron <samaaron@gmail.com>
2014-10-20 16:13:08 +0100

    reimplement Allocator without crazy non-blocking constructs for efficiency.

--- app/server/sonicpi/lib/sonicpi/allocator.rb
+++ app/server/sonicpi/lib/sonicpi/allocator.rb
@@ -10,48 +10,46 @@
 # and distribution of modified versions of this work as long as this
 # notice is included.
 #++
-require_relative "atom"
-require "hamster/vector"
 
 module SonicPi
   class AllocationError < Exception ; end
   class Allocator
-    attr_reader :max_id, :idx_offset
+    attr_reader :max_id, :idx_offset, :num_allocations
     def initialize(max_id, idx_offset=0)
       @idx_offset = idx_offset
       @max_id = max_id
-      tmp_ids = [false] * @max_id
-      @ids_A = Atom.new(Hamster.vector(*tmp_ids))
+      @ids = [false] * @max_id
+      @ids_mut = Mutex.new
       @num_allocations = 0
     end
 
     def allocate(num_adj_ids=1)
       idx = nil
-      ids = @ids_A.swap! do |ids|
-        idx = find_gap(0, num_adj_ids, ids)
-        new_ids = (idx...idx+num_adj_ids).reduce(ids) {|is, i| is.set(i, true)}
+      @ids_mut.synchronize do
+        idx = find_gap(0, num_adj_ids, @ids)
+        (idx...idx+num_adj_ids).each do |i|
+          @ids[i] = true
+        end
+        new_id = idx + @idx_offset
+        @num_allocations += num_adj_ids
+        new_id
       end
-      new_id = idx + @idx_offset
-      @num_allocations += num_adj_ids
-      new_id
     end
 
     def release!(idx, num_adj_ids=1)
-      @ids_A.swap! do |ids|
-        (idx...idx+num_adj_ids).reduce(ids) {|is, i| is.set(i, false)}
+      @ids_mut.synchronize do
+        (idx...idx+num_adj_ids).each do |i|
+          @ids[i] = false
+        end
+        @num_allocations -= num_adj_ids
       end
-      @num_allocations -= num_adj_ids
     end
 
     def reset!
-      old_ids = @ids_A.swap_returning_old! {|bs| Hamster.vector}
-      old_ids.each {|b| b.free}
-      @num_busses_allocated = 0
-      @busses_A.reset! Hamster.vector
-    end
-
-    def num_allocations
-      @num_allocations
+      @ids_mut.synchronize do
+        @num_busses_allocated = 0
+        @ids = [false] * @max_id
+      end
     end
 
     def to_s
