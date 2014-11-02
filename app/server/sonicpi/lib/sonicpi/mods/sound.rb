42c8eceb07a401e38d0020a96305c761d7f6458f
Sam Aaron <samaaron@gmail.com>
2014-11-02 09:48:14 +0000

    ensure original trackers are copied back to the appropriate thread

--- app/server/sonicpi/lib/sonicpi/mods/sound.rb
+++ app/server/sonicpi/lib/sonicpi/mods/sound.rb
@@ -2665,10 +2665,11 @@ end
          trackers = current_trackers
          orig_trackers = trackers.clone
          trackers << tracker
-
+         parent_t = Thread.current
          #run the block
          with_crash_safe_finaliser lambda{yield} do
-           Thread.current.thread_variable_set(:sonic_pi_mod_sound_trackers, orig_trackers)
+
+           parent_t.thread_variable_set(:sonic_pi_mod_sound_trackers, orig_trackers)
 
            # snapshot threads created by block
            updated_subthreads = Thread.current.thread_variable_get(:sonic_pi_spider_subthreads)
