9ac65ffbc998dfcfe39b6ed2723341d160e8e338
Sam Aaron <samaaron@gmail.com>
2014-11-02 01:36:30 +0000

    Further work on code associated with with_fx -
    * Updated synth trackers to just track by id
    * Teach synth trackers to be able to ignore synths
    * add get_t to Promise which doesn’t throw an exception on timeout
    * add robustness to with_fx bock execution and safely handling internal thread deaths
    * fixes thread leak introduced in very recent commits
    * fix up tracker usage i.e work with clones where necessary

--- app/server/sonicpi/lib/sonicpi/promise.rb
+++ app/server/sonicpi/lib/sonicpi/promise.rb
@@ -23,7 +23,9 @@ module SonicPi
     end
 
     def get(timeout=nil)
+      # timeout is in seconds
       return @value if @delivered
+
       @prom_sem.synchronize do
         return @value if @delivered
         @received.wait(@prom_sem, timeout)
@@ -31,6 +33,21 @@ module SonicPi
           return @value
         else
           raise "Promise timeout"
+        end
+      end
+    end
+
+    def get_t(timeout)
+      # timeout is in seconds
+      return @value if @delivered
+
+      @prom_sem.synchronize do
+        return @value if @delivered
+        @received.wait(@prom_sem, timeout)
+        if @delivered
+          return @value
+        else
+          return nil
         end
       end
     end
