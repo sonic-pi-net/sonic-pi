bee97aac5b88f449b5ee2a757a7e765b2c9dd2ad
Sam Aaron <samaaron@gmail.com>
2014-11-06 12:49:42 +0000

    Add (bounded) caches for all outgoing OSC types:
    * String cache is now bounded
    * Integers and floats are now cached

--- app/server/sonicpi/lib/sonicpi/oscencode.rb
+++ app/server/sonicpi/lib/sonicpi/oscencode.rb
@@ -13,9 +13,17 @@
 
 module SonicPi
   class OscEncode
-    def initialize(use_cache = false)
+    def initialize(use_cache = false, cache_size=1000)
       @use_cache = use_cache
+      @integer_cache = {}
       @string_cache = {}
+      @float_cache = {}
+      @cache_size = cache_size
+
+      @num_cached_integers = 0
+      @num_cached_floats = 0
+      @num_cached_strings = 0
+
       @bundle_header = get_from_or_add_to_string_cache("#bundle")
     end
 
@@ -46,10 +54,18 @@ module SonicPi
       case arg
       when Integer
         tags << 'i'
-        args_encoded << [arg].pack('N').force_encoding("BINARY")
+        if @use_cache
+          args_encoded << get_from_or_add_to_integer_cache(arg)
+        else
+          args_encoded << encode_integer(arg)
+        end
       when Float
         tags << 'f'
-        args_encoded << [arg].pack('g').force_encoding("BINARY")
+        if @use_cache
+          args_encoded << get_from_or_add_to_float_cache(arg)
+        else
+          args_encoded << encode_float(arg)
+        end
       when String, Symbol
         arg = arg.to_s
         tags << 's'
@@ -63,14 +79,62 @@ module SonicPi
       end
     end
 
+    def get_from_or_add_to_float_cache(f)
+      if cached = @float_cache[f]
+        return cached
+      else
+        res = encode_float(f)
+        if @num_cached_floats < @cache_size
+          # only cache the first @max_cached_floats floats
+          # to avoid a massive memory leak as floats are
+          # often randomly generated
+          @float_cache[f] = res
+          @num_cached_floats += 1
+          # log "caching float #{f}"
+        end
+        return res
+      end
+    end
+
     def get_from_or_add_to_string_cache(s)
       if cached = @string_cache[s]
         return cached
       else
         res = encode_string(s)
-        @string_cache[s] = res
+        if @num_cached_strings < @cache_size
+          # only cache the first @cache_size strings to avoid a memory
+          # memory leak.
+          @string_cache[s] = res
+          @num_cached_strings += 1
+          # log "caching string #{s}"
+        end
         return res
       end
+    end
+
+    def get_from_or_add_to_integer_cache(i)
+      if cached = @integer_cache[i]
+        return cached
+      else
+        res = encode_integer(i)
+        if @num_cached_integers < @cache_size
+          # only cache the first @cache_size integers to avoid a memory
+          # memory leak.
+          @integer_cache[i] = res
+          @num_cached_integers += 1
+          # log "caching integer #{i}"
+        end
+        return res
+      end
+
+    end
+
+    def encode_float(f)
+      [f].pack('g').force_encoding("BINARY")
+    end
+
+    def encode_integer(i)
+      [i].pack('N').force_encoding("BINARY")
     end
 
     def encode_string(s)
