64c8bee399fed586cca855f7ecfee9ac75cbc229
Sam Aaron <samaaron@gmail.com>
2014-11-10 19:12:08 +0000

    Further work to reduce the readability of oscencode:
    * inline more fns to increase efficiency
    * remove superfluous calls to force_encoding

--- app/server/sonicpi/lib/sonicpi/oscencode.rb
+++ app/server/sonicpi/lib/sonicpi/oscencode.rb
@@ -13,6 +13,10 @@
 
 module SonicPi
   class OscEncode
+    # Apologies for the density of this code - I've inlined a lot of the
+    # code to reduce method dispatch overhead and to increase efficiency.
+    # See http://opensoundcontrol.org for spec.
+
     def initialize(use_cache = false, cache_size=1000)
       @use_cache = use_cache
       @integer_cache = {}
@@ -28,73 +32,81 @@ module SonicPi
     end
 
     def encode_single_message(address, args=[])
-      message = ""
+      message, args_encoded, tags = "", "", ","
+      message.force_encoding("BINARY")
       address = get_from_or_add_to_string_cache(address)
-      tags = ","
-      args_encoded = ""
 
-      args.each { |a| append_encoded_arg!(tags, args_encoded, a) }
+      args.each do |arg|
+        case arg
+        when Integer
+          tags << 'i'
+
+          if @use_cache
+            if cached = @integer_cache[arg]
+              args_encoded << cached
+            else
+              res = [arg].pack('N')
+              if @num_cached_integers < @cache_size
+                @integer_cache[arg] = res
+                @num_cached_integers += 1
+                # log "caching integer #{arg}"
+              end
+              args_encoded << res
+            end
+          else
+            args_encoded << [arg].pack('N')
+          end
+        when Float
+          tags << 'f'
+          if @use_cache
+            if cached = @float_cache[arg]
+              args_encoded << cached
+            else
+              res = [arg].pack('g')
+              if @num_cached_floats < @cache_size
+                @float_cache[arg] = res
+                @num_cached_floats += 1
+                # log "caching float #{arg}"
+              end
+              args_encoded << res
+            end
+          else
+            args_encoded << [arg].pack('g')
+          end
+        when String, Symbol
+          arg = arg.to_s
+          tags << 's'
+          if @use_cache
+            if cached = @string_cache[arg]
+              args_encoded << cached
+            else
+              res = encode_string(arg)
+              if @num_cached_strings < @cache_size
+                @string_cache[arg] = res
+                @num_cached_strings += 1
+                # log "caching string #{arg}"
+              end
+              args_encoded << res
+            end
+          else
+            args_encoded << encode_string(arg)
+          end
+        else
+          raise "Unknown arg type to encode: #{arg}"
+        end
+      end
 
       tags_encoded = get_from_or_add_to_string_cache(tags)
-
       message << address << tags_encoded << args_encoded
     end
 
     def encode_single_bundle(ts, address, args=[])
       message = encode_single_message(address, args)
       message_encoded = [message.size].pack('N') << message
-
-      bundle = ""
-      bundle << @bundle_header << time_encoded(ts) << message_encoded
+      "" << @bundle_header << time_encoded(ts) << message_encoded
     end
 
     private
-
-    def append_encoded_arg!(tags, args_encoded, arg)
-      case arg
-      when Integer
-        tags << 'i'
-        if @use_cache
-          args_encoded << get_from_or_add_to_integer_cache(arg)
-        else
-          args_encoded << encode_integer(arg)
-        end
-      when Float
-        tags << 'f'
-        if @use_cache
-          args_encoded << get_from_or_add_to_float_cache(arg)
-        else
-          args_encoded << encode_float(arg)
-        end
-      when String, Symbol
-        arg = arg.to_s
-        tags << 's'
-        if @use_cache
-          args_encoded << get_from_or_add_to_string_cache(arg)
-        else
-          args_encoded << encode_string(arg)
-        end
-      else
-        raise "Unknown arg type to encode: #{arg}"
-      end
-    end
-
-    def get_from_or_add_to_float_cache(f)
-      if cached = @float_cache[f]
-        return cached
-      else
-        res = encode_float(f)
-        if @num_cached_floats < @cache_size
-          # only cache the first @max_cached_floats floats
-          # to avoid a massive memory leak as floats are
-          # often randomly generated
-          @float_cache[f] = res
-          @num_cached_floats += 1
-          # log "caching float #{f}"
-        end
-        return res
-      end
-    end
 
     def get_from_or_add_to_string_cache(s)
       if cached = @string_cache[s]
@@ -108,46 +120,18 @@ module SonicPi
           @num_cached_strings += 1
           # log "caching string #{s}"
         end
-        return res
+        return  res
       end
-    end
-
-    def get_from_or_add_to_integer_cache(i)
-      if cached = @integer_cache[i]
-        return cached
-      else
-        res = encode_integer(i)
-        if @num_cached_integers < @cache_size
-          # only cache the first @cache_size integers to avoid a memory
-          # memory leak.
-          @integer_cache[i] = res
-          @num_cached_integers += 1
-          # log "caching integer #{i}"
-        end
-        return res
-      end
-
-    end
-
-    def encode_float(f)
-      [f].pack('g').force_encoding("BINARY")
-    end
-
-    def encode_integer(i)
-      [i].pack('N').force_encoding("BINARY")
     end
 
     def encode_string(s)
-      padding(s.sub(/\000.*\z/, '') + "\000").force_encoding("BINARY")
-    end
-
-    def padding(s)
-      s + ("\000" * ((4 - (s.size % 4)) % 4))
+      s = s.sub(/\000.*\z/, '')
+      s << "\000"
+      (s << ("\000" * ((4 - (s.size % 4)) % 4)))
     end
 
     def time_encoded(time)
-      ntp = time.to_f + 2208988800
-      t1, fr = ntp.divmod(1)
+      t1, fr = (time.to_f + 2208988800).divmod(1)
       # 2 ** 32 == 4294967296
       t2 = (fr * 4294967296).to_i
       [t1, t2].pack('N2')
