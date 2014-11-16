63751609370690da1653f2f09c9386ee77361a92
Sam Aaron <samaaron@gmail.com>
2014-11-16 13:08:58 +0000

    Add the atomic library

--- app/server/bin/compile-extensions.rb
+++ app/server/bin/compile-extensions.rb
@@ -36,11 +36,11 @@ FileUtils.mkdir_p native_dir
 native_ext_dirs = [
   File.expand_path(File.dirname(__FILE__) + '/../vendor/rugged/ext/rugged'),
   File.expand_path(File.dirname(__FILE__) + '/../vendor/ffi/ext/ffi_c'),
-  File.expand_path(File.dirname(__FILE__) + '/../vendor/interception/ext/'),
+  File.expand_path(File.dirname(__FILE__) + '/../vendor/atomic/ext'),
+  File.expand_path(File.dirname(__FILE__) + '/../vendor/ruby-prof/ext/ruby_prof/'),
 
-  File.expand_path(File.dirname(__FILE__) + '/../vendor/did_you_mean/ext/did_you_mean'),
-  File.expand_path(File.dirname(__FILE__) + '/../vendor/ruby-prof/ext/ruby_prof/')
-  File.expand_path(File.dirname(__FILE__) + '/../vendor/ffi/ext/ffi_c')
+  File.expand_path(File.dirname(__FILE__) + '/../vendor/interception/ext/'),
+  File.expand_path(File.dirname(__FILE__) + '/../vendor/did_you_mean/ext/did_you_mean')
 ]
 
 native_ext_dirs.each do |ext_dir|
