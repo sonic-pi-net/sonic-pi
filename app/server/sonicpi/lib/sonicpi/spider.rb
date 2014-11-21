813ba646109740b10f4105de17a4ed01851922a0
Sam Aaron <samaaron@gmail.com>
2014-11-21 16:04:05 +0000

    v2.1 here we are!

--- app/server/sonicpi/lib/sonicpi/spider.rb
+++ app/server/sonicpi/lib/sonicpi/spider.rb
@@ -43,7 +43,7 @@ module SonicPi
 
     def initialize(hostname, port, msg_queue, max_concurrent_synths, user_methods)
 
-      @version = Version.new(2, 1, 0, "dev")
+      @version = Version.new(2, 1, 0)
 
       @life_hooks = LifeCycleHooks.new
       @msg_queue = msg_queue
