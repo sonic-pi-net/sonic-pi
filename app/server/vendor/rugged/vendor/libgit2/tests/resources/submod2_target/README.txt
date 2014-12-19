3b65d65cd2b87ce0cc8cb88b7e1b4ebf3f1f2955
Sam Aaron <samaaron@gmail.com>
2014-12-19 16:02:51 +0000

    Further rugged cleaning

--- app/server/vendor/rugged/vendor/libgit2/tests/resources/submod2_target/README.txt
+++
@@ -1,3 +0,0 @@
-This is the target for submod2 submodule links.
-Don't add commits casually because you make break tests.
-
--- app/server/vendor/rugged/vendor/libgit2/tests/threads/thread_helpers.h
+++
@@ -1,8 +0,0 @@
-#include "thread-utils.h"
-
-void run_in_parallel(
-	int repeats,
-	int threads,
-	void *(*func)(void *),
-	void (*before_test)(void),
-	void (*after_test)(void));
