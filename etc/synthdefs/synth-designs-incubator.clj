e090be746555f2827f231c40e0fcfa4b54ee9cca
Joseph Wilk <joe@josephwilk.net>
2015-01-18 13:54:22 +0100

    Adding singer synth. Not support for vowel changes yet.

--- etc/synthdefs/synth-designs-incubator.clj
+++ etc/synthdefs/synth-designs-incubator.clj
@@ -461,4 +461,67 @@
     (sonic-pi-dark_sea_horn :attack 1 :release 8 :note 40)
     (save-to-pi sonic-pi-dark_sea_horn)
     )
+
+  (without-namespace-in-synthdef
+   (defsynth sonic-pi-singer
+     "TODO: support vowel changes"
+     [out_bus 0
+
+      vibrato_speed 6
+      vibrato_depth 4
+
+      pan 0
+      pan_slide 0
+      pan_slide_shape 5
+      pan_slide_curve 0
+
+      amp 1
+      amp_slide 0
+      amp_slide_shape 5
+      amp_slide_curve 0
+
+      attack 1
+      decay 0
+      sustain 0
+      release 4.0
+      attack_level 1
+      sustain_level 1
+      env_curve 2
+
+      note 40
+      note_slide 0
+      note_slide_shape 5
+      note_slide_curve 0
+
+      freq0 400 freq1 750 freq2 2400 freq3 2600 freq4 2900
+      amps0 1 amps1 0.28 amps2 0.08 amps3 0.1 amps4 0.01
+      qs0 0.1 qs1 0.1 qs2 0.04 qs3 0.04 qs4 0.04
+      lag-val 0.5]
+     (let [pan  (varlag pan pan_slide pan_slide_curve pan_slide_shape)
+           note (varlag note note_slide note_slide_curve note_slide_shape)
+           amp (varlag amp amp_slide amp_slide_curve amp_slide_shape)
+           freq (midicps note)
+
+           freqs-list (map #(lag:kr % lag-val) [freq0 freq1 freq2 freq3 freq4])
+           amps-list  (map #(lag:kr (dbamp %) lag-val) [amps0 amps1 amps2 amps3 amps4])
+           qs-list    (map #(lag:kr % lag-val) [qs0 qs1 qs2 qs3 qs4])
+
+           vibrato (* vibrato_depth (sin-osc:kr vibrato_speed))
+           in (saw:ar (lag:kr (+ freq vibrato) 0.2))
+
+           env (env-gen:kr (env-adsr-ng attack decay sustain release attack_level sustain_level env_curve) :action FREE)
+           snd (mix (* amps-list (bpf:ar in freqs-list qs-list)))
+           snd (* snd amp)]
+       (out out_bus (* (pan2 snd pan) env)))))
+
+  (comment
+    (defn bass          [] (singer :freq 100))
+    (defn tenor         [] (singer :freq 280))
+    (defn alto          [] (singer :freq 380))
+    (defn soprano       [] (singer :freq 580))
+    (def v (bass))
+
+    (save-to-pi sonic-pi-singer)
+    )
+
 )
