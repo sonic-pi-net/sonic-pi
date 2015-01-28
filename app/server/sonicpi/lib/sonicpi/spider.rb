1621a6c5c5d8425f786e44bed8c0be7a332fedc5
Sam Aaron <samaaron@gmail.com>
2015-01-28 13:42:58 +0000

    Update internal version number to v2.3.0

--- app/server/sonicpi/lib/sonicpi/spider.rb
+++ app/server/sonicpi/lib/sonicpi/spider.rb
@@ -48,7 +48,7 @@ module SonicPi
 
     def initialize(hostname, port, msg_queue, max_concurrent_synths, user_methods)
       @settings = Settings.new
-      @version = Version.new(2, 3, 0, "dev")
+      @version = Version.new(2, 3, 0)
       @server_version = __server_version
       @life_hooks = LifeCycleHooks.new
       @msg_queue = msg_queue
