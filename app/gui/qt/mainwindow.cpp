d52849c9057f9fb95a93ea1a7b6a908d9e935ead
Hanno Zulla <hanno.zulla@epublica.de>
2015-02-10 23:26:17 +0100

    i18n - capitalize status bar msg strings

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -744,14 +744,14 @@ void MainWindow::reloadServerCode()
 
 void MainWindow::enableCheckUpdates()
 {
-  statusBar()->showMessage(tr("enabling update checking...."), 2000);
+  statusBar()->showMessage(tr("Enabling update checking...."), 2000);
   Message msg("/enable-update-checking");
   sendOSC(msg);
 }
 
 void MainWindow::disableCheckUpdates()
 {
-  statusBar()->showMessage(tr("disabling update checking...."), 2000);
+  statusBar()->showMessage(tr("Disabling update checking...."), 2000);
   Message msg("/disable-update-checking");
   sendOSC(msg);
 }
