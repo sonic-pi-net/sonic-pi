b5b038b65985483e1e6fa0306c140861e69ad49e
Sam Aaron <samaaron@gmail.com>
2015-02-22 18:43:03 +0000

    Re-enable spark tests

--- app/server/sonicpi/test/test_spark.rb
+++ app/server/sonicpi/test/test_spark.rb
@@ -20,13 +20,13 @@ module SonicPi
     include SonicPi::SpiderAPI
 
     def test_spark
-      assert_equal("▁▃▅▇", __spark(1, 2, 3, 4))
-      assert_equal("▁▃▅▇", __spark([1, 2, 3, 4]))
-      assert_equal("▁", __spark(1))
-      assert_equal("▁▁▁", __spark(3, 3, 3))
-      assert_equal("", __spark([]))
-      assert_equal("spark error: can't use nested arrays", __spark([1, 2], 3, 4))
-      assert_equal("spark error: arguments should be numeric", __spark('foo', 'apple', 'banana', {silly: :hash}))
+      assert_equal("▁▃▅▇", spark(1, 2, 3, 4))
+      assert_equal("▁▃▅▇", spark([1, 2, 3, 4]))
+      assert_equal("▁", spark(1))
+      assert_equal("▁▁▁", spark(3, 3, 3))
+      assert_equal("", spark([]))
+      assert_equal("spark error: can't use nested arrays", spark([1, 2], 3, 4))
+      assert_equal("spark error: arguments should be numeric", spark('foo', 'apple', 'banana', {silly: :hash}))
     end
   end
 end
