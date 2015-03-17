95390b587b87d034df8d820aedada466f9b00367
Sam Aaron <samaaron@gmail.com>
2015-03-17 15:37:20 +0000

    Tests - fix failure caused by spark rename

--- app/server/sonicpi/test/test_spark.rb
+++ app/server/sonicpi/test/test_spark.rb
@@ -20,13 +20,13 @@ module SonicPi
     include SonicPi::SpiderAPI
 
     def test_spark
-      assert_equal("▁▃▅▇", spark(1, 2, 3, 4))
-      assert_equal("▁▃▅▇", spark([1, 2, 3, 4]))
-      assert_equal("▁", spark(1))
-      assert_equal("▁▁▁", spark(3, 3, 3))
-      assert_equal("", spark([]))
-      assert_equal("spark error: can't use nested arrays", spark([1, 2], 3, 4))
-      assert_equal("spark error: arguments should be numeric", spark('foo', 'apple', 'banana', {silly: :hash}))
+      assert_equal("▁▃▅▇", spark_graph(1, 2, 3, 4))
+      assert_equal("▁▃▅▇", spark_graph([1, 2, 3, 4]))
+      assert_equal("▁", spark_graph(1))
+      assert_equal("▁▁▁", spark_graph(3, 3, 3))
+      assert_equal("", spark_graph([]))
+      assert_equal("spark error: can't use nested arrays", spark_graph([1, 2], 3, 4))
+      assert_equal("spark error: arguments should be numeric", spark_graph('foo', 'apple', 'banana', {silly: :hash}))
     end
   end
 end
