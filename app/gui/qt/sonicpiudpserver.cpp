94aa955df4879bf58acc917e7edaf8d18d817227
Sam Aaron <samaaron@gmail.com>
2015-03-25 22:05:16 +0000

    Housekeeping - fix indentation

--- app/gui/qt/sonicpiudpserver.cpp
+++ app/gui/qt/sonicpiudpserver.cpp
@@ -16,23 +16,23 @@ void SonicPiUDPServer::stopServer(){
 
 void SonicPiUDPServer::startServer(){
   std::cout << "[GUI] - starting UDP OSC Server on port 4558..." << std::endl;
-    int PORT_NUM = 4558;
-    oscpkt::UdpSocket sock;
-    sock.bindTo(PORT_NUM);
-    if (!sock.isOk()) {
-      std::cout << "[GUI] - unable to listen to UDP OSC messages on port 4558" << std::endl;
-      parent->invokeStartupError(tr("Is Sonic Pi already running?  Can't open UDP port 4558."));
-      return;
+  int PORT_NUM = 4558;
+  oscpkt::UdpSocket sock;
+  sock.bindTo(PORT_NUM);
+  if (!sock.isOk()) {
+    std::cout << "[GUI] - unable to listen to UDP OSC messages on port 4558" << std::endl;
+    parent->invokeStartupError(tr("Is Sonic Pi already running?  Can't open UDP port 4558."));
+    return;
+  }
+
+  std::cout << "[GUI] - UDP OSC Server listening" << std::endl;
+
+  osc_incoming_port_open = true;
+
+  while (sock.isOk() && continueListening()) {
+    if (sock.receiveNextPacket(30 /* timeout, in ms */)) {
+      handler->oscMessage(sock.buffer);
+      std::vector<char>().swap(sock.buffer);
     }
-
-    std::cout << "[GUI] - UDP OSC Server listening" << std::endl;
-
-    osc_incoming_port_open = true;
-
-    while (sock.isOk() && continueListening()) {
-      if (sock.receiveNextPacket(30 /* timeout, in ms */)) {
-        handler->oscMessage(sock.buffer);
-        std::vector<char>().swap(sock.buffer);
-      }
-    }
+  }
 }
