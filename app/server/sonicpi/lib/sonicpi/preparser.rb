bd7a1b851be05b73fd283260f43e762f4fe2c559
Sam Aaron <samaaron@gmail.com>
2015-04-11 21:40:35 +0100

    Syntax - teach prepares to add space when munching ring fns
    * previously the following broke:
    
    pitch_ratio(scale :e3, ;minor)
    
    as it was turned to:
    
    pitch_ratioscale(:e3, :minor)
    
    now with this commit it is converted to:
    
    pitch_ratio scale(:e3, :minor)

--- app/server/sonicpi/lib/sonicpi/preparser.rb
+++ app/server/sonicpi/lib/sonicpi/preparser.rb
@@ -23,7 +23,7 @@ module SonicPi
     def self.preparse(rb)
       SonicPi::SpiderAPI.ring_fns.each do |fn|
         fn = fn[:name].to_s
-        rb.gsub!(/\((\s*)#{fn}(\s)/, '\1' + fn + '(\2')
+        rb.gsub!(/\((\s*)#{fn}(\s)/, '\1' + ' ' + fn + '(\2')
         raise PreParseError, "You may not use the built-in fn names as variable names.\n You attempted to use: #{fn}" if rb.match(/\W#{fn}\s*=[\s\w]/)
       end
       rb
