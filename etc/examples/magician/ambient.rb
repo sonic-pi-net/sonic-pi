b0a5b9f3dcabbd0144a68b8bc4a47a7f8048401d
Sam Aaron <samaaron@gmail.com>
2015-04-17 17:38:32 +0100

    Examples - improve Magician - Ambient

--- etc/examples/magician/ambient.rb
+++ etc/examples/magician/ambient.rb
@@ -3,7 +3,7 @@
 load_samples(sample_names :ambi)
 sleep 2
 
-with_fx :reverb, mix: 0.3 do
+with_fx :reverb, mix: 0.8 do
   live_loop :foo do
     # try changing the sp_ vars..
     sp_name = choose sample_names :ambi
@@ -13,7 +13,7 @@ with_fx :reverb, mix: 0.3 do
     sp_rate = 1
     #sp_rate = 4
 
-    s = sample sp_name, rate: sp_rate * choose([0.5, 1]), pan: rrand(-1, 1), pan_slide: sp_time
+    s = sample sp_name, cutoff: rrand(70, 130), rate: sp_rate * choose([0.5, 1]), pan: rrand(-1, 1), pan_slide: sp_time
     control s, pan: rrand(-1, 1)
     sleep sp_time
   end
