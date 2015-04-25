243cec2a1772e87a880febb2491df8ae26ccc8a2
Sam Aaron <samaaron@gmail.com>
2015-04-25 22:11:45 +0100

    Synth Designs - add pdf renderings of synth designs.

---
+++ etc/synthdefs/graphviz/render.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby -wKU
+
+#--
+# This file is part of Sonic Pi: http://sonic-pi.net
+# Full project source: https://github.com/samaaron/sonic-pi
+# License: https://github.com/samaaron/sonic-pi/blob/master/LICENSE.md
+#
+# Copyright 2013, 2014, 2015 by Sam Aaron (http://sam.aaron.name).
+# All rights reserved.
+#
+# Permission is granted for use, copying, modification, and
+# distribution of modified versions of this work as long as this
+# notice is included.
+#++
+
+path = File.expand_path(File.dirname(__FILE__))
+
+Dir["#{path}/*.dot"].each do |f|
+#  `dot #{f} -Tpng -o #{f[0..-5]}.png`
+  `dot #{f} -Tpdf -o #{f[0..-5]}.pdf`
+end
