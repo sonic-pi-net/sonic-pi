a96d6fc9bab0eecd5ee1ca5ad92b4824b4fe0cc5
Xavier Riley <xavriley@hotmail.com>
2015-05-12 22:03:56 +0100

    Allow tuning system to handle floats

--- app/server/sonicpi/lib/sonicpi/mods/sound.rb
+++ app/server/sonicpi/lib/sonicpi/mods/sound.rb
@@ -812,6 +812,81 @@ synth :dsaw, note: 50 # Play note 50 of the :dsaw synth with a release of 5"]
          init_args_h = {}
          args_h = resolve_synth_opts_hash_or_array(args)
 
+         if tuning_system = Thread.current.thread_variable_get(:sonic_pi_mod_sound_tuning)
+           if tuning_system != :equal
+             fundamental_sym = Thread.current.thread_variable_get(:sonic_pi_mod_sound_tuning_fundamental_note)
+             fundamental = Note.resolve_midi_note_without_octave(fundamental_sym)
+
+             # http://en.wikipedia.org/wiki/Musical_tuning#Systems_for_the_twelve-note_chromatic_scale
+             @tunings = {:just => [
+                           1, #c
+                           16.0/15.0, #cs
+                           9.0/8.0, #d
+                           6.0/5.0, #ds
+                           5.0/4.0, #e
+                           4.0/3.0, #f
+                           45.0/32.0, #fs
+                           3.0/2.0, #g
+                           8.0/5.0, #gs
+                           5.0/3.0, #a
+                           9.0/5.0, #as
+                           15.0/8.0],
+                         :pythagorean => [
+                           1.0/1, #unison
+                           2187.0/2048, #apotome
+                           9.0/8, #major whole tone
+                           19683.0/16384, #Pythagorean augmented second
+                           81.0/64, #Pythagorean major third
+                           4.0/3, #perfect fourth
+                           729.0/512, #Pythagorean tritone
+                           3.0/2, #perfect fifth
+                           6561.0/4096, #Pythagorean augmented fifth
+                           27.0/16, #Pythagorean major sixth
+                           59049.0/32768, #Pythagorean augmented sixth
+                           243.0/128],
+                         :meantone => [
+                           1.00000,
+                           1.0449,
+                           1.1180,
+                           1.1963,
+                           1.2500,
+                           1.3375,
+                           1.3975,
+                           1.4953,
+                           1.5625,
+                           1.6719,
+                           1.7889,
+                           1.8692]}
+
+             # midi note 0 is a :c
+             # by getting a fundamental without an octave
+             # we know it's in the middle register between
+             # 60 and 72
+             fundamental_offset = fundamental - 60
+
+             # create an array of all the tuned notes
+             @@tuned_notes ||= {}
+             @@tuned_notes[tuning_system] ||= (0..150).to_a.map {|midi_note|
+               # vary the fundamental here
+               note_idx = (midi_note - fundamental_offset) % 12
+               # tuning systems are ratios of the fundamental note
+               tuned_note = midi_to_hz(midi_note - note_idx) * @tunings[tuning_system][note_idx]
+               hz_to_midi(tuned_note)
+             }
+
+             #select the tuned note for a given midi note
+             if n.is_a? Float
+               note_above = @@tuned_notes[tuning_system][n.ceil]
+               note_below = @@tuned_notes[tuning_system][n.floor]
+               n_diff = n - n.floor
+
+               n = hz_to_midi(midi_to_hz(note_below) + ((midi_to_hz(note_above) - midi_to_hz(note_below)) * n_diff))
+             else
+               n = @@tuned_notes[tuning_system][n]
+             end
+           end
+         end
+
          if shift = Thread.current.thread_variable_get(:sonic_pi_mod_sound_transpose)
            n += shift
          end
