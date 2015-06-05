7ebccbc67ad6b19a0fecc2eb9de59177b6156a04
Hanno Zulla <hanno.zulla@epublica.de>
2015-06-05 23:27:01 +0200

    handle upper/lowercase confusion of QSettings::IniFormat

--- app/gui/qt/sonicpitheme.cpp
+++ app/gui/qt/sonicpitheme.cpp
@@ -10,15 +10,16 @@ void SonicPiTheme::readTheme(QString filename) {
   theme.clear();
   QSettings settings(filename, QSettings::IniFormat);
   foreach(QString key, settings.allKeys()) {
+    key = key.toLower();
     theme[key] = settings.value(key).toString();
   }
   isInitialized = true;
 }
 
 QColor SonicPiTheme::color(QString key) {
-  
+  key = key.toLower();
   if ( !theme.contains(key) && isInitialized) {
-    std::cerr << "[GUI] - colours.ini contains no value for " << key.toStdString() << "\n";
+    std::cerr << "[GUI] - colours.ini contains no value for \"" << key.toStdString() << "\"\n";
   }
   
   return QColor(theme[key]);
