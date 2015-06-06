636e70e908b512a111a986e305ea8572ed771df1
Joseph Wilk <joe@josephwilk.net>
2015-06-06 19:12:47 +0200

    EVERYTHING. RP Experiments

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -1234,6 +1234,10 @@ void MainWindow::changeTheme(){
     p.setColor(QPalette::Button,        QColor("#000"));
     p.setColor(QPalette::ButtonText,    QColor("#fff"));
     p.setColor(QPalette::Shadow,        QColor("#333"));
+    p.setColor(QPalette::Mid, QColor("#333"));
+    p.setColor(QPalette::Dark, QColor("#333"));
+    p.setColor(QPalette::Midlight, QColor("#333"));
+    p.setColor(QPalette::Light, QColor("#333"));
 
     QApplication::setPalette(p);
 
