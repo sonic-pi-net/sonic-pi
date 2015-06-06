6beca7dc759df89972d57d7958eaceeea7d83277
Joseph Wilk <joe@josephwilk.net>
2015-06-06 14:38:13 +0200

    Override OS styling of QGroupbox. This means dark mode perfs are not exactly aligned with light modes positions. This is required to get pesky RP to not render a nasty white line around the boxes

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -1247,7 +1247,7 @@ void MainWindow::changeTheme(){
     statusBar()->setStyleSheet( QString("QStatusBar{background-color: %1; border-top: 1px solid %2;}").arg(windowColor, windowBorder));
     outputPane->setStyleSheet(  QString("QTextEdit{background-color: %1; color: %2; border: 0px;}").arg(paneColor, windowForegroundColor));
     outputWidget->setStyleSheet(widgetTitleStyling);
-    prefsWidget->setStyleSheet( QString(widgetTitleStyling + "QGroupBox{font-size: 11px; color: %1;}").arg(windowForegroundColor));
+    prefsWidget->setStyleSheet( QString(widgetTitleStyling + "QGroupBox:title{subcontrol-origin: margin; top:0px; padding: 0px 0 20px 5px; font-size: 11px; color: %1;} QGroupBox{padding: 0 0 0 0; subcontrol-origin: margin; margin-top: 15px; margin-bottom: 0px; font-size: 11px; background-color:#1c2325; border: 1px solid #000; color: %1;}").arg(windowForegroundColor));
     tabs->setStyleSheet(        tabStyling);
     docsCentral->setStyleSheet( tabStyling);
     docWidget->setStyleSheet(   QString(widgetTitleStyling + "QDockWidget QListView {color: %2; background: %1;}").arg(paneColor, windowForegroundColor));
