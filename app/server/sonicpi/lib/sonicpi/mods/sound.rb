0aa1c1b8be8a8dcf5a913a7244a57fa304f96c6c
Xavier Riley <xavriley@hotmail.com>
2015-06-15 09:45:44 +0100

    Fix example

--- app/server/sonicpi/lib/sonicpi/mods/sound.rb
+++ app/server/sonicpi/lib/sonicpi/mods/sound.rb
@@ -646,7 +646,7 @@ play 64 # Plays note 64"]
          examples:      ["
 use_tuning :equal, :c
 play :e4 # Plays note 64
-with_tuning :equal, :c do
+with_tuning :just, :c do
   play :e4 # Plays note 63.8631
   sleep 1
   play :c4 # Plays note 60
