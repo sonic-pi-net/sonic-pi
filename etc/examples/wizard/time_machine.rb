485c29aa8aeab20aa38453d639f862af9aecfc4e
Sam Aaron <samaaron@gmail.com>
2015-06-25 18:44:59 +0100

    Examples - update Time Machine to use ticks

--- etc/examples/wizard/time_machine.rb
+++ etc/examples/wizard/time_machine.rb
@@ -2,28 +2,24 @@
 
 use_debug false
 
-live_loop :time do |idx|
-  synth :tb303, release: 8, note: :e1, cutoff: (range 90, 60, -10)[idx]
+live_loop :time do
+  synth :tb303, release: 8, note: :e1, cutoff: (range 90, 60, -10).tick
   sleep 8
-  idx += 1
 end
 
-live_loop :machine do |idx|
-  sample :loop_garzul, rate: (knit 1, 3, -1, 1)[idx]
+live_loop :machine do
+  sample :loop_garzul, rate: (knit 1, 3, -1, 1).tick
   sleep 8
-  idx += 1
 end
 
-live_loop :vortex, auto_cue: false do |idx|
+live_loop :vortex, auto_cue: false do
   use_synth [:pulse, :beep].choose
   sleep 0.125 / 2
-  play scale(:e1, :minor_pentatonic)[idx], attack: 0.125, release: 0, amp: 2, cutoff: (ring 70, 90, 100, 130)[idx]
+  play scale(:e1, :minor_pentatonic).tick, attack: 0.125, release: 0, amp: 2, cutoff: (ring 70, 90, 100, 130).look
   sleep 0.125 / 2
-  idx += 1
 end
 
-live_loop :moon_bass, auto_cue: false do |idx|
+live_loop :moon_bass, auto_cue: false do
   sample :bd_haus, amp: 1.5
   sleep 0.5
-  idx += 1
 end
