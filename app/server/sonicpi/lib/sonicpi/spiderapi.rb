9008e7fe1a2ce2345de5e99f3f8eab50bef90b81
Sam Aaron <samaaron@gmail.com>
2015-07-02 09:40:42 +0100

    Docs - improve docstring and examples for *_random_seed

--- app/server/sonicpi/lib/sonicpi/spiderapi.rb
+++ app/server/sonicpi/lib/sonicpi/spiderapi.rb
@@ -1438,16 +1438,45 @@ shuffle \"foobar\"  #=> Would return something like: \"roobfa\""    ]
     doc name:          :use_random_seed,
         introduced:    Version.new(2,0,0),
         summary:       "Set random seed generator to known seed",
-        doc:           "Resets the random number generator to the specified seed. All subsequently generated random numbers will use this new generator and the current generator is discarded. Use this to change the sequence of random numbers in your piece in a way that can be reproduced",
+        doc:           "Resets the random number generator to the specified seed. All subsequently generated random numbers and randomisation functions such as `shuffle` and `choose` will use this new generator and the current generator is discarded. Use this to change the sequence of random numbers in your piece in a way that can be reproduced. Especially useful if combined with iteration. See examples.",
         args:          [[:seed, :number]],
         opts:          nil,
         accepts_block: false,
         examples:      ["
+## Basic usage
+
 use_random_seed 1 # reset random seed to 1
 puts rand # => 0.417022004702574
 use_random_seed 1 # reset random seed back to 1
 puts rand  #=> 0.417022004702574
-"]
+",
+"
+## Generating melodies
+notes = (scale :eb3, :minor_pentatonic)  # Create a set of notes to choose from.
+                                         # Scales work well for this
+
+with_fx :reverb do
+  live_loop :repeating_melody do         # Create a live loop
+
+    use_random_seed 300                  # Set the random seed to a known value every
+                                         # time around the loop. This seed is the key
+                                         # to our melody. Try changing the number to
+                                         # something else. Different numbers produce
+                                         # different melodies
+
+    8.times do                           # Now iterate a number of times. The size of
+                                         # the iteration will be the length of the
+                                         # repeating melody.
+
+      play notes.choose, release: 0.1    # 'Randomly' choose a note from our ring of
+                                         # notes. See how this isn't actually random
+                                         # but uses a reproducible method! These notes
+                                         # are therefore repeated over and over...
+      sleep 0.125
+    end
+  end
+end
+"  ]
 
 
 
@@ -1466,7 +1495,7 @@ puts rand  #=> 0.417022004702574
     doc name:           :with_random_seed,
         introduced:     Version.new(2,0,0),
         summary:        "Specify random seed for code block",
-        doc:            "Resets the random number generator to the specified seed for the specified code block. All generated random numbers within the code block will use this new generator. Once the code block has completed, the original generator is restored and the code block generator is discarded.",
+        doc:            "Resets the random number generator to the specified seed for the specified code block. All generated random numbers and randomisation functions such as `shuffle` and `choose` within the code block will use this new generator. Once the code block has completed, the original generator is restored and the code block generator is discarded. Use this to change the sequence of random numbers in your piece in a way that can be reproduced. Especially useful if combined with iteration. See examples.",
         args:           [[:seed, :number]],
         opts:           nil,
         accepts_block:  true,
@@ -1482,7 +1511,41 @@ with_random_seed 1 do # reset seed back to 1 just for this block
   puts rand #=> 0.7203244934421581
 end
 puts rand # => 0.7203244934421581
-          # notice how the original generator is restored"]
+          # notice how the original generator is restored",
+"
+## Generating melodies
+notes = (scale :eb3, :minor_pentatonic, num_octaves: 2)  # Create a set of notes to choose from.
+                                         # Scales work well for this
+
+with_fx :reverb do
+  live_loop :repeating_melody do         # Create a live loop
+
+    with_random_seed 300 do              # Set the random seed to a known value every
+                                         # time around the loop. This seed is the key
+                                         # to our melody. Try changing the number to
+                                         # something else. Different numbers produce
+                                         # different melodies
+
+      8.times do                         # Now iterate a number of times. The size of
+                                         # the iteration will be the length of the
+                                         # repeating melody.
+
+        play notes.choose, release: 0.1  # 'Randomly' choose a note from our ring of
+                                         # notes. See how this isn't actually random
+                                         # but uses a reproducible method! These notes
+                                         # are therefore repeated over and over...
+        sleep 0.125
+      end
+    end
+
+    play notes.choose, amp: 1.5, release: 0.5 # Note that this line is outside of
+                                              # the with_random_seed block and therefore
+                                              # the randomisation never gets reset and this
+                                              # part of the melody never repeats.
+  end
+end
+"
+    ]
 
 
 
