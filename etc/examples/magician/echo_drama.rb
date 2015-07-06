23cb90d187de9112511cc046b34242452e185a55
Sam Aaron <samaaron@gmail.com>
2015-07-06 17:30:24 +0100

    Examples - update echo_drama to work on RP 1

--- etc/examples/magician/echo_drama.rb
+++ etc/examples/magician/echo_drama.rb
@@ -3,11 +3,12 @@
 use_synth :tb303
 use_bpm 45
 use_random_seed 3
+use_debug false
 
 with_fx :reverb do
-  live_loop :echoes do
-    with_fx(:echo, delay: 0.5, decay: 4) do
-      play chord([:b1, :b2, :e1, :e2, :b3, :e3].choose, :minor).choose, cutoff: rrand(40, 100), amp: 0.5, attack: 0, release: 2, cutoff_max: 110
+  with_fx(:echo, delay: 0.5, decay: 4) do
+    live_loop :echoes do
+      play chord([:b1, :b2, :e1, :e2, :b3, :e3].choose, :minor).choose, cutoff: rrand(40, 100), amp: 0.5, attack: 0, release: rrand(1, 2), cutoff_max: 110
       sleep [0.25, 0.5, 0.5, 0.5, 1, 1].choose
     end
   end
