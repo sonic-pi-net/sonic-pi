3c3960d19e4dd2c5bcd4a2d17f7294830d53fa15
Sam Aaron <samaaron@gmail.com>
2015-07-09 19:20:06 +0100

    Examples - Reich add comment to easily switch to piano

--- etc/examples/illusionist/reich_phase.rb
+++ etc/examples/illusionist/reich_phase.rb
@@ -1,6 +1,7 @@
 # Steve Reich's Piano Phase
 # See: https://en.wikipedia.org/wiki/Piano_Phase
 
+# use_synth :piano
 notes = (ring :E4, :Fs4, :B4, :Cs5, :D5, :Fs4, :E4, :Cs5, :B4, :Fs4, :D5, :Cs5)
 
 live_loop :slow do
