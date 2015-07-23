7edc9d82fac3f7ae57ce5826b3b6a30b4c864829
Xavier Riley <xavriley@hotmail.com>
2015-07-23 09:16:20 +0100

    Fixup tuning docs - bad version no. and list tunings
    Fixes #584 - thanks @rbnpi!

--- app/server/sonicpi/lib/sonicpi/mods/sound.rb
+++ app/server/sonicpi/lib/sonicpi/mods/sound.rb
@@ -618,7 +618,7 @@ play 80 # Plays note 83
        doc name:          :use_tuning,
            introduced:    Version.new(2,6,0),
            summary:       "Use alternative tuning systems",
-           doc:           "In most music we make semitones by dividing the octave into 12 equal parts, which is known as equal temperament. However there are lots of other ways to tune the 12 notes. This method adjusts each midi note into the specified tuning system. Because the ratios between notes aren't always equal, be careful to pick a center note that is in the key of the music you're making for the best sound.",
+           doc:           "In most music we make semitones by dividing the octave into 12 equal parts, which is known as equal temperament. However there are lots of other ways to tune the 12 notes. This method adjusts each midi note into the specified tuning system. Because the ratios between notes aren't always equal, be careful to pick a center note that is in the key of the music you're making for the best sound. Currently available tunings are :just, :pythagorean, :meantone and the default of :equal",
            args:          [[:tuning, :symbol], [:fundamental_note, :symbol_or_number]],
            opts:          nil,
            accepts_block: false,
@@ -648,7 +648,7 @@ play 64 # Plays note 64"]
          Thread.current.thread_variable_set(:sonic_pi_mod_sound_tuning, [curr_tuning, curr_fundamental])
        end
        doc name:          :with_tuning,
-         introduced:    Version.new(2,0,0),
+         introduced:    Version.new(2,6,0),
          summary:       "Block-level tuning modification",
          doc:           "Similar to use_tuning except only applies to code within supplied `do`/`end` block. Previous tuning value is restored after block.",
          args:          [[:tuning, :symbol], [:fundamental_note, :symbol_or_number]],
