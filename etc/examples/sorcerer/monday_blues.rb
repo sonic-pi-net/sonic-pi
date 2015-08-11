1243d4b4add631c0f9f943e024990287682747ba
Sam Aaron <samaaron@gmail.com>
2015-08-11 11:39:41 +0100

    Examples - update blue monday to use new thread delay opt
    * No more in_thread! live_loops all the way!

--- etc/examples/sorcerer/monday_blues.rb
+++ etc/examples/sorcerer/monday_blues.rb
@@ -3,27 +3,22 @@
 use_debug false
 load_samples [:drum_heavy_kick, :drum_snare_soft]
 
-define :drums do
-  cue :slow_drums
+
+live_loop :drums do
+  puts "slow drums"
   6.times do
     sample :drum_heavy_kick, rate: 0.8
     sleep 0.5
   end
 
-  cue :fast_drums
+  puts "fast drums"
   8.times do
     sample :drum_heavy_kick, rate: 0.8
     sleep 0.125
   end
 end
 
-define :snare do
-  cue :snare
-  sample :drum_snare_soft
-  sleep 1
-end
-
-define :synths do
+live_loop :synths, delay: 6 do
   puts "how does it feel?"
   use_synth :mod_saw
   use_synth_defaults amp: 0.5, attack: 0, sustain: 1, release: 0.25, cutoff: 90, mod_range: 12, mod_phase: 0.5, mod_invert_wave: 1
@@ -35,16 +30,7 @@ define :synths do
   end
 end
 
-in_thread(name: :synths) do
-  sleep 6
-  loop{synths}
-end
-
-in_thread(name: :drums) do
-  loop{drums}
-end
-
-in_thread(name: :snare) do
-  sleep 12.5
-  loop{snare}
+live_loop :snare, delay: 12.5 do
+  sample :drum_snare_soft
+  sleep 1
 end
