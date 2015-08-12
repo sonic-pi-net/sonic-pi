f35aa5ad7487e965c5ff0c28b9cb6ce4451794c0
Ivan Zarea <zarea.ion@gmail.com>
2015-08-13 01:00:04 +0300

    Can save code now, yay

--- app/gui/electron/src/frontend/scripts/app.js
+++ app/gui/electron/src/frontend/scripts/app.js
@@ -1,1 +1,24 @@
+import Sender from './backend/sender';
 
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/tomorrow_night_eighties");
+editor.getSession().setMode("ace/mode/ruby");
+editor.setKeyboardHandler("ace/keyboard/vim");
+editor.focus();
+
+var runCurrentBuffer = function() {
+  var sender = new Sender();
+  sender.runCode(editor.getValue());
+}
+
+editor.commands.addCommand({
+  name: 'saveFile',
+  bindKey: {
+    win: 'Ctrl-S',
+    mac: 'Command-S',
+    sender: 'editor|cli'
+  },
+  exec: function(env, args, request) {
+    runCurrentBuffer();
+  }
+});
