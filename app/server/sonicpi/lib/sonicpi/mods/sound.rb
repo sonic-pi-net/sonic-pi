0ddc74bc89de0fbe2c171e2213b2589eacace1b5
Sam Aaron <samaaron@gmail.com>
2015-08-17 21:31:07 +0100

    Sound - fix how note_info handles octaves
    Fixes: #616

--- app/server/sonicpi/lib/sonicpi/mods/sound.rb
+++ app/server/sonicpi/lib/sonicpi/mods/sound.rb
@@ -2381,7 +2381,7 @@ end"
         raise Exception.new("note_info argument must be a valid note. Got nil.") if(n.nil?)
         args_h = resolve_synth_opts_hash_or_array(args)
         octave = args_h[:octave]
-        SonicPi::Note.new(SonicPi::Note.resolve_note_name(n, octave), octave)
+        SonicPi::Note.new(n, octave)
       end
       doc name:          :note_info,
           introduced:    Version.new(2,0,0),
