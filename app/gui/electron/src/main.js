9275d8b8af4dd877b1d21ea52e230d06ee352da2
Ivan Zarea <zarea.ion@gmail.com>
2015-08-18 11:43:45 +0300

    Properly capturing the stdout of the child process

--- app/gui/electron/src/main.js
+++ app/gui/electron/src/main.js
@@ -1,23 +1,34 @@
-require('electron-compile').init();
-var app = require('app');
-var BrowserWindow = require('browser-window');
-var exec = require('child_process').exec;
-var mainWindow = null;
+let app = require('app');
+let BrowserWindow = require('browser-window');
+let spawn = require('child_process').spawn;
+let server = spawn('./../../../app/server/native/osx/ruby/bin/ruby', ['./../../../app/server/bin/sonic-pi-server.rb']);
 
-app.on('ready', function() {
-  exec('./server.sh', function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
-  });
+import Receiver from './backend/receiver';
+import Sender   from './backend/sender';
+
+let mainWindow = null;
+
+server.stdout.on('data', (data) => {
+  console.log(data.toString());
+});
+
+server.stderr.on('data', (data) => {
+  console.log(data.toString());
+});
+
+app.on("ready", () => {
+  let receiver = new Receiver();
 
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadUrl("file://" + __dirname + "/index.html");
   mainWindow.openDevTools();
 
-  mainWindow.on('closed', function() {
+  mainWindow.on("closed", () => {
     mainWindow = null;
   });
+});
+
+app.on("quit", () => {
+  console.log("Killing the child process");
+  server.kill();
 });
