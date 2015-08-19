e91b8a7dfbd925031c53e8d1d61edc02063ae057
Ivan Zarea <zarea.ion@gmail.com>
2015-08-19 10:58:23 +0300

    Extracted the server code into a separate file

---
+++ app/gui/electron/src/backend/server.js
@@ -0,0 +1,35 @@
+let spawn = require('child_process').spawn;
+
+class Server {
+  constructor(options = {}) {
+    this.stdoutHandler = options.stdoutHandler || ((data) => console.log(data.toString()));
+    this.stderrHandler = options.stderrHandler || ((data) => console.log(data.toString()));
+    return this;
+  }
+
+  spawn() {
+    let platform = process.platform;
+    let ruby_path = null;
+    let prefix = "../../..";
+    let server_path = prefix + "/app/server/bin/sonic-pi-server.rb";
+
+    if(platform == "darwin") {
+      ruby_path = prefix + "/app/server/native/osx/ruby/bin/ruby";
+    } else if (platform == "win32") {
+      ruby_path = prefix + "/app/server/native/windows/bin/ruby.exe";
+    } else {
+      ruby_path = prefix + "/app/server/native/raspberry/ruby/bin/ruby";
+    }
+
+    this.server = spawn(ruby_path, [server_path]);
+    this.server.stdout.on("data", this.stdoutHandler);
+    this.server.stderr.on("data", this.stderrHandler);
+    return this;
+  }
+
+  kill() {
+    this.server.kill();
+  }
+}
+
+export default Server;
--- app/gui/electron/src/main.js
+++ app/gui/electron/src/main.js
@@ -1,23 +1,16 @@
 let app = require('app');
 let BrowserWindow = require('browser-window');
-let spawn = require('child_process').spawn;
-let server = spawn('./../../../app/server/native/osx/ruby/bin/ruby', ['./../../../app/server/bin/sonic-pi-server.rb']);
 
 import Receiver from './backend/receiver';
 import Sender   from './backend/sender';
+import Server   from './backend/server';
 
 let mainWindow = null;
-
-server.stdout.on('data', (data) => {
-  console.log(data.toString());
-});
-
-server.stderr.on('data', (data) => {
-  console.log(data.toString());
-});
+let server = new Server();
 
 app.on("ready", () => {
   let receiver = new Receiver();
+  server.spawn();
 
   mainWindow = new BrowserWindow({width: 800, height: 600});
   mainWindow.loadUrl("file://" + __dirname + "/index.html");
