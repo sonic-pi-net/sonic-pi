48a74490ffae5dbd45d83f56c47c06e0c20826b6
Sam Aaron <samaaron@gmail.com>
2015-11-18 16:38:12 +0000

    scsynth - further reduce resources (and CPU) for RP1

--- app/server/sonicpi/lib/sonicpi/util.rb
+++ app/server/sonicpi/lib/sonicpi/util.rb
@@ -47,7 +47,24 @@ module SonicPi
 
     def num_audio_busses_for_current_os
       if os == :raspberry
-        64
+        if raspberry_pi_1?
+          16
+        else
+          64
+        end
+      else
+        1024
+      end
+
+    end
+
+    def num_control_busses_for_current_os
+      if os == :raspberry
+        if raspberry_pi_1?
+          16
+        else
+          64
+        end
       else
         1024
       end
