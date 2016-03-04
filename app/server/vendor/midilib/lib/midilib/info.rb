4f1531887973b75b92c6bf22f22ea510fe3b8a7e
Sam Aaron <samaaron@gmail.com>
2016-03-04 22:08:48 +0000

    vendor - remove unused MIDI libs

--- app/server/vendor/midilib/lib/midilib/info.rb
+++
@@ -1,9 +0,0 @@
-module MIDI
-
-  VERSION_MAJOR = 2
-  VERSION_MINOR = 0
-  VERSION_TWEAK = 4
-  Version = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_TWEAK}"
-  Copyright = 'Copyright (c) 2003-2013 by Jim Menard <jim@jimmenard.com>'
-
-end
