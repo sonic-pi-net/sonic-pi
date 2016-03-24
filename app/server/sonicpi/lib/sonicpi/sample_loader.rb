87dbcaa25e6ff2de07a03e8c079ea05acf994cf7
Sam Aaron <samaaron@gmail.com>
2016-03-24 17:29:58 +0000

    Sample loader - tidy and improve logic
    * string matches are now case-insensitive
    * strings can match *entire filename* with extension
      provided case also matches
    * globbing is now performed safely (and accidental
      glob chars in paths will not be globbed)
    * added basic tests

--- app/server/sonicpi/lib/sonicpi/sample_loader.rb
+++ app/server/sonicpi/lib/sonicpi/sample_loader.rb
@@ -29,67 +29,75 @@ module SonicPi
       return [] if filts_and_sources.empty?
       candidates = @cached_candidates[filts_and_sources]
       return candidates if candidates
-      res = []
-      @mutex.synchronize do
 
+      @mutex.synchronize do
         candidates = @cached_candidates[filts_and_sources]
         return candidates if candidates
 
-        idx = nil
-        dirs = []
-        filters = []
         candidates = []
         procs = []
+        filters = []
+        idx = nil
+
+
         filts_and_sources.each do |arg|
-          idx = consume_filt_or_source!(arg, idx, filters, dirs, candidates, procs, res)
+          idx = consume_filt_or_source!(arg, candidates, procs, filters, idx)
         end
 
-        if dirs.empty? && !filters.empty?
-          dirs = default_samples_paths
-        end
+        if candidates.empty? && procs.empty? && filters.size == 1 && filters[0].is_a?(Symbol)
 
-        dirs.each do |dir|
-          path = File.expand_path(dir)
-          candidates += ls_samples(path)
+          default_samples_paths.each do |p|
+            p = File.expand_path(p)
+            candidates.concat(ls_samples(p)) if File.directory?(p)
+          end
         end
 
         procs.each do |p|
           candidates = p.call(candidates)
-          raise "Sample Pack Filter Proc needs to return an array or ring. Got #{res.class}: #{res.inspect}" unless candidates.is_a?(Array) || candidates.is_a?(SonicPi::Core::RingVector)
+          raise "Sample Pack Filter Proc needs to return an array or ring. Got #{candidates.class}: #{candidates.inspect}" unless candidates.is_a?(Array) || candidates.is_a?(SonicPi::Core::RingVector)
         end
 
         filters.each do |f|
           candidates.keep_if do |v|
-            v = File.basename(v)
+            bn = File.basename(v, ".*")
+            bne = File.basename(v)
             case f
             when String
-              v.include? f
+              bn.downcase.include?(f.downcase) || bne == f
             when Symbol
-              v == f.to_s
+              # match only sample extensions
+              regexp = /#{f}\.(wav|aif|wave|aiff|flac)/
+              bne.match(regexp)
             when Regexp
-              v.match f
+              bn.match f
             end
           end
         end
 
-        candidates = [candidates[idx % candidates.size]] if(idx && !candidates.empty?)
 
-        res = (res + candidates).uniq
-        @cached_candidates[filts_and_sources] = res
+
+        # Grab the nth element from the list using idx
+        # mod idx by candidates size if idx is specified
+        candidates = [candidates[idx % candidates.size]] if(idx && !candidates.empty?)
+        @cached_candidates[filts_and_sources] = candidates
         #end mutex
       end
 
-      return res
+      return candidates
     end
 
-    def ls_samples(path)
+    def ls_samples(path, recursive=false)
       res = @cached_folder_contents[path]
       return res if res
+
       @folder_contents_mutex.synchronize do
         res = @cached_folder_contents[path]
         return res if res
-
-        res = Dir.glob(path + "/*.{wav,wave,aif,aiff,flac}").sort
+        if recursive
+          res = Dir.chdir(path) { Dir.glob("**/*.{wav,wave,aif,aiff,flac}").map {|path| File.expand_path(path) } }.sort
+        else
+          res = Dir.chdir(path) { Dir.glob("*.{wav,wave,aif,aiff,flac}").map {|path| File.expand_path(path) } }.sort
+        end
         @cached_folder_contents[path] = res
       end
       res
@@ -101,50 +109,43 @@ module SonicPi
       [Thread.current.thread_variable_get(:sonic_pi_mod_sound_sample_path) || @samples_path]
     end
 
-    def consume_filt_or_source!(filt_or_source, idx, filters, dirs, candidates, procs, res)
+    def consume_filt_or_source!(filt_or_source, candidates, procs, filters, idx)
       case filt_or_source
-      when Symbol
-        regexp = /#{filt_or_source}\.(wav|aif|wave|aiff|flac)/
-        (dirs + default_samples_paths).each do |d|
-          s = ls_samples(d).find {|el| el.match(regexp)}
-          if s
-            res << s
-            break
-          end
-        end
-      when Integer
-        idx = filt_or_source
       when String
-
-        if filt_or_source.end_with?("**") && File.directory?(File.expand_path(filt_or_source[0...-2]))
-          # allow /foo/bar/baz** recursive dir matches
-          dirs << filt_or_source
-        elsif File.directory?(File.expand_path(filt_or_source))
-          dirs << filt_or_source
-        elsif File.exists?(File.expand_path(filt_or_source))
-          candidates << filt_or_source
+        expanded = File.expand_path(filt_or_source)
+        if expanded.end_with?("**") && File.directory?(expanded[0...-2])
+          candidates.concat(ls_samples(expanded[0...-2], true))
+        elsif File.directory?(expanded)
+          candidates.concat(ls_samples(expanded))
+        elsif File.exists?(expanded)
+          candidates << expanded
         else
+          # Treat as a regular filter
           filters << filt_or_source
         end
+      when Symbol
+        filters << filt_or_source
+      when Integer
+        idx = filt_or_source
+      when Regexp
+        filters << filt_or_source
       when Proc
         if filt_or_source.arity == 0
-          consume_filt_or_source!(filt_or_source.call, idx, filters, dirs, candidates, procs, res)
+          idx = consume_filt_or_source!(filt_or_source.call, candidates, procs, filters, idx)
         elsif filt_or_source.arity == 1
           procs << filt_or_source
         else
           raise "Sample Pack Proc needs to accept either 0 or 1 arguments. Found #{block.arity}"
         end
-      when Regexp
-        filters << filt_or_source
       when Array, SonicPi::Core::RingVector
         filt_or_source.each do |fos|
-          idx = consume_filt_or_source!(fos, idx, filters, dirs, candidates, procs, res)
+          idx = consume_filt_or_source!(fos, candidates, procs, filters, idx)
         end
       else
         raise "Unknown sample filter or source type: #{filt_or_source.inspect}"
       end
 
-      return idx
+      idx
     end
   end
 end
---
+++ app/server/sonicpi/test/lang/sound/test_sample_loader.rb
@@ -0,0 +1,110 @@
+#--
+# This file is part of Sonic Pi: http://sonic-pi.net
+# Full project source: https://github.com/samaaron/sonic-pi
+# License: https://github.com/samaaron/sonic-pi/blob/master/LICENSE.md
+#
+# Copyright 2013, 2014, 2015, 2016 by Sam Aaron (http://sam.aaron.name).
+# All rights reserved.
+#
+# Permission is granted for use, copying, modification, and
+# distribution of modified versions of this work as long as this
+# notice is included.
+#++
+
+require_relative "../../setup_test"
+require_relative "../../../lib/sonicpi/sample_loader"
+
+module SonicPi
+  class SampleLoaderTester < Minitest::Test
+    def setup
+      @fake_sample_dir = File.expand_path(File.dirname(__FILE__) + '../../../fake_sample_dir')
+      # Contains:
+      # -  bar_baz.aiff
+      # -  buzz_100.flac
+      # -  eggs.wav
+      # -  foo.wav
+      # -  woo_100.aiff
+      # -  subdir
+      #    |
+      #    - quux.wav
+      @loader = SampleLoader.new(@fake_sample_dir)
+    end
+
+    def test_single_builtin_symbol
+      res = @loader.find_candidates([:foo])
+      assert_equal(res, [@fake_sample_dir + '/foo.wav'])
+    end
+
+    def test_globs_count
+      res = @loader.find_candidates([@fake_sample_dir + "/**"])
+      assert_equal(res.count, 6)
+    end
+
+    def test_non_globs_count
+      res = @loader.find_candidates([@fake_sample_dir])
+      assert_equal(res.count, 5)
+    end
+
+    def test_globs_filt
+      res = @loader.find_candidates([@fake_sample_dir + "/**", :quux])
+      assert_equal([@fake_sample_dir + "/subdir/quux.wav"] , res)
+    end
+
+    def test_non_globs_filt_does_not_include_subdir
+      res = @loader.find_candidates([@fake_sample_dir, :quux])
+      assert_equal([] , res)
+    end
+
+    def test_idx
+      res = @loader.find_candidates([@fake_sample_dir, 1])
+      assert_equal([@fake_sample_dir + "/buzz_100.flac"] , res)
+    end
+
+    def test_mult_idx_overrides
+      res = @loader.find_candidates([@fake_sample_dir, 3, 5, 1])
+      assert_equal([@fake_sample_dir + "/buzz_100.flac"] , res)
+    end
+
+    def test_idx_wrap
+      res = @loader.find_candidates([@fake_sample_dir, 5])
+      assert_equal([@fake_sample_dir + "/bar_baz.aiff"] , res)
+    end
+
+    def test_glob_idx_wrap
+      res = @loader.find_candidates([@fake_sample_dir + "/**", 5])
+      assert_equal([@fake_sample_dir + "/woo_100.aiff"] , res)
+    end
+
+    def test_str_filter
+      res = @loader.find_candidates([@fake_sample_dir, "100"])
+      assert_equal([@fake_sample_dir + "/buzz_100.flac",
+                    @fake_sample_dir + "/woo_100.aiff"] , res)
+    end
+
+    def test_regex_filter
+      res = @loader.find_candidates([@fake_sample_dir, /b[uU]zz/])
+      assert_equal([@fake_sample_dir + "/buzz_100.flac"] , res)
+    end
+
+    def test_0_arity_proc_filter
+      proc = lambda {/b[uU]zz/}
+      res = @loader.find_candidates([@fake_sample_dir, proc])
+      assert_equal([@fake_sample_dir + "/buzz_100.flac"] , res)
+    end
+
+    def test_1_arity_proc_filter
+      proc = lambda {|c| c.keep_if { |v| v.include? "foo"}}
+      res = @loader.find_candidates([@fake_sample_dir, proc])
+      assert_equal([@fake_sample_dir + "/foo.wav"] , res)
+    end
+
+    def test_multi_filters
+      identity_proc = lambda {|c| c }
+      res = @loader.find_candidates([@fake_sample_dir, 1, identity_proc, "100", /100/])
+      assert_equal([@fake_sample_dir + "/woo_100.aiff"] , res)
+    end
+
+
+
+  end
+end
