89e3cef351e940f378a52cde654df1bf89f292f1
Sam Aaron <samaaron@gmail.com>
2016-06-29 20:06:57 +0100

    Thread Locals - add missing files
    Fixes #1308

---
+++ app/server/sonicpi/test/test_threadlocal.rb
@@ -0,0 +1,59 @@
+#--
+# This file is part of Sonic Pi: http://sonic-pi.net
+# Full project source: https://github.com/samaaron/sonic-pi
+# License: https://github.com/samaaron/sonic-pi/blob/master/LICENSE.md
+#
+# Copyright 2013, 2014, 2015, 2016 by Sam Aaron (http://sam.aaron.name).
+# All rights reserved.
+#
+# Permission is granted for use, copying, modification, and
+# distribution of modified versions of this work as long as this
+# notice is included.
+#++
+
+require_relative "./setup_test"
+require_relative "../lib/sonicpi/threadlocal"
+
+module SonicPi
+  class VersionTester < Minitest::Test
+
+    def test_basic_setting_and_getting
+      t = ThreadLocal.new
+      assert_equal(t.get(:foo), nil)
+      t.set(:foo, 1)
+      assert_equal(t.get(:foo), 1)
+      t.set(:foo, 3)
+      assert_equal(t.get(:foo), 3)
+    end
+
+    def test_local_setting
+      t = ThreadLocal.new
+      assert_equal(t.get(:foo), nil)
+      t.set_local(:foo, 1)
+      assert_equal(t.get(:foo), 1)
+      t.set(:foo, 3)
+      assert_equal(t.get(:foo), 3)
+    end
+
+    def test_inheritance
+      t = ThreadLocal.new
+      t.set(:foo, 1)
+      assert_equal(t.get(:foo), 1)
+      t2 = ThreadLocal.new(t)
+      assert_equal(t2.get(:foo), 1)
+      t.set_local(:foo, 10)
+      assert_equal(t.get(:foo), 10)
+      assert_equal(t2.get(:foo), 1)
+    end
+
+    def test_local_setting_is_not_inherited
+      t = ThreadLocal.new
+      t.set_local(:foo, 1)
+      assert_equal(t.get(:foo), 1)
+      t2 = ThreadLocal.new(t)
+      t2.set(:bar, 3)
+      assert_equal(t2.get(:bar), 3)
+      assert_equal(t2.get(:foo), nil)
+    end
+  end
+end
