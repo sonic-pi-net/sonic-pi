75ed0a0c4ecfceb0ab1b2a6afeb3f65b36cc49d0
Ethan Crawford <ethan_jc@hotmail.com>
2016-08-05 22:35:21 +0800

    Typos - fix with_timing_warnings exception message

--- app/server/sonicpi/lib/sonicpi/lang/sound.rb
+++ app/server/sonicpi/lib/sonicpi/lang/sound.rb
@@ -466,7 +466,7 @@ end"]
 
 
       def with_timing_warnings(v, &block)
-        raise "with_debug requires a do/end block. Perhaps you meant use_debug" unless block
+        raise "with_timing_warnings requires a do/end block. Perhaps you meant use_timing_warnings" unless block
         current = __thread_locals.get(:sonic_pi_mod_sound_disable_timing_warnings)
         __thread_locals.set(:sonic_pi_mod_sound_disable_timing_warnings, !v)
         res = block.call
