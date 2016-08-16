1dc4e8fd544194e1b8f0c7f9416b343011a02c76
Sam Aaron <samaaron@gmail.com>
2016-08-16 09:42:09 +0100

    Tests - add tests for *_synth_defaults

--- app/server/sonicpi/test/lang/sound/test_play.rb
+++ app/server/sonicpi/test/lang/sound/test_play.rb
@@ -33,6 +33,7 @@ module SonicPi
     end
 
     def test_play_with_various_args
+      @mock_sound.reset
       @mock_sound.expects(:trigger_inst).with(:beep, {note: 60.0}, @beep_info)
       @mock_sound.play :c
 
@@ -49,11 +50,13 @@ module SonicPi
     end
 
     def test_multi_notes
+      @mock_sound.reset
       @mock_sound.expects(:trigger_chord).with(:beep, [62.0], {})
       @mock_sound.play [62]
     end
 
     def test_note_with_tuning
+      @mock_sound.reset
       @mock_sound.expects(:trigger_inst).with(:beep, {note: 62.039100017}, @beep_info)
       @mock_sound.with_tuning :just do
         @mock_sound.play 62
@@ -61,6 +64,7 @@ module SonicPi
     end
 
     def test_chord_with_tuning
+      @mock_sound.reset
       @mock_sound.expects(:trigger_chord).with(:beep, [62.039100017], {})
       @mock_sound.with_tuning :just do
         @mock_sound.play [62]
@@ -68,9 +72,42 @@ module SonicPi
     end
 
     def test_play_chord_with_tuning
+      @mock_sound.reset
       @mock_sound.expects(:trigger_chord).with(:beep, [62.039100017], {})
       @mock_sound.with_tuning :just do
         @mock_sound.play_chord [62]
+      end
+    end
+
+    def test_play_and_use_synth_defaults
+      @mock_sound.reset
+      @mock_sound.use_synth_defaults release: 0.3
+      @mock_sound.expects(:trigger_inst).with(:beep, {note: 60.0, release: 0.3}, @beep_info)
+      @mock_sound.play 60
+
+      @mock_sound.expects(:trigger_inst).with(:beep, {note: 61.0, release: 0.3}, @beep_info)
+      @mock_sound.play 61
+
+      @mock_sound.expects(:trigger_inst).with(:beep, {note: 61.0, release: 0.5}, @beep_info)
+      @mock_sound.play 61, release: 0.5
+    end
+
+    def test_play_and_with_synth_defaults
+      @mock_sound.reset
+
+      @mock_sound.expects(:trigger_inst).with(:beep, {note: 60.0, release: 0.3}, @beep_info)
+      @mock_sound.with_synth_defaults release: 0.3 do
+        @mock_sound.play 60
+      end
+
+      @mock_sound.expects(:trigger_inst).with(:beep, {note: 61.0, release: 0.3}, @beep_info)
+      @mock_sound.with_synth_defaults release: 0.3 do
+        @mock_sound.play 61
+      end
+
+      @mock_sound.expects(:trigger_inst).with(:beep, {note: 61.0, release: 0.5}, @beep_info)
+      @mock_sound.with_synth_defaults release: 0.5 do
+        @mock_sound.play 61, release: 0.5
       end
     end
   end
