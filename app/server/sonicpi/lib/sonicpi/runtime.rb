2ce3a3a10d579338e14c05d49a5786f8cd861e6b
Sam Aaron <samaaron@gmail.com>
2016-09-23 07:51:28 +0100

    Version - bump from dev to BETA

--- app/server/sonicpi/lib/sonicpi/runtime.rb
+++ app/server/sonicpi/lib/sonicpi/runtime.rb
@@ -959,7 +959,7 @@ module SonicPi
       @git_hash = __extract_git_hash
       gh_short = @git_hash ? "-#{@git_hash[0, 5]}" : ""
       @settings = Config::Settings.new(user_settings_path)
-      @version = Version.new(2, 11, 0, "dev#{gh_short}")
+      @version = Version.new(2, 11, 0, "BETA-#{gh_short}")
       @server_version = __server_version
       @life_hooks = LifeCycleHooks.new
       @msg_queue = msg_queue
