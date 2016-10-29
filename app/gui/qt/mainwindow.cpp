e72c2d540a115945f2937209fd67bc4678255d9a
Sam Aaron <samaaron@gmail.com>
2016-10-29 23:30:34 +0100

    GUI - modify wording of auto scroll pref
    Previous version of “Log auto scroll” looked too similar to “log synths” and “log cues” whilst it had a very different meaning for log (the log, not to log). Change wording to “Auto-scroll log” to help tease that difference apart a little.

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -1122,7 +1122,7 @@ void MainWindow::initPrefsWindow() {
   log_cues = new QCheckBox(tr("Log cues"));
   log_cues->setToolTip(tr("Enable or disable logging of cues.\nIf disabled, cues will still trigger.\nHowever, they will not be visible in the logs."));
 
-  log_auto_scroll = new QCheckBox(tr("Log auto scroll"));
+  log_auto_scroll = new QCheckBox(tr("Auto-scroll log"));
   log_auto_scroll->setToolTip(tr("Toggle log auto scrolling.\nIf enabled the log is scrolled to the bottom after every new message is displayed."));
   connect(log_auto_scroll, SIGNAL(clicked()), this, SLOT(updateLogAutoScroll()));
 
