5b9b1e01d89fe9cd4bca02bfac89277e342bc965
Sam Aaron <samaaron@gmail.com>
2016-11-03 22:54:59 +0000

    Version - start working on v2.12

--- app/server/sonicpi/lib/sonicpi/runtime.rb
+++ app/server/sonicpi/lib/sonicpi/runtime.rb
@@ -1006,7 +1006,7 @@ module SonicPi
       @git_hash = __extract_git_hash
       gh_short = @git_hash ? "-#{@git_hash[0, 5]}" : ""
       @settings = Config::Settings.new(user_settings_path)
-      @version = Version.new(2, 11, 0)
+      @version = Version.new(2, 12, 0, "BETA-#{gh_short}")
       @server_version = __server_version
       @life_hooks = LifeCycleHooks.new
       @msg_queue = msg_queue
