87e0b11fcf3e75058678694545ec24b5411378d8
Sam Aaron <samaaron@gmail.com>
2017-01-31 13:10:39 +0100

    Tests - rework test_sample to use the new mock language object

--- app/server/sonicpi/test/lang/sound/test_sample.rb
+++ app/server/sonicpi/test/lang/sound/test_sample.rb
@@ -22,46 +22,40 @@ require 'ostruct'
 
 module SonicPi
   class SampleTester < Minitest::Test
-    class MockStudio
-      def initialize
-        @mod_sound_studio = Object.new
-        @mod_sound_studio.stubs(:sample_loaded?).returns(true)
-      end
-    end
 
     def setup
-      @mock_sound = MockStudio.new
-      @mock_sound.extend(Lang::Sound)
-      @mock_sound.extend(Lang::Core)
-      @mock_sound.stubs(:sleep) # avoid loading Spider class
-      @mock_sound.stubs(:ensure_good_timing!) # avoid loading Spider class
-      @mock_sound.stubs(:sample_loaded?).returns(true)
-      @mock_sound.stubs(:__delayed_user_message).returns(true)
-      @mock_sound.stubs(:sample_find_candidates).returns(["/foo/bar.wav"])
-      @mock_sound.stubs(:load_sample_at_path).returns(OpenStruct.new({id: 42, num_chans: 2}))
-      @mock_sound.stubs(:__delayed_message).returns(true)
-      @info = Synths::SynthInfo.get_info(:stereo_player)
+      @lang = SonicPi::MockLang.new
+      @lang.mod_sound_studio.stubs(:sample_loaded?).returns(true)
+      @lang.stubs(:sample_find_candidates).returns(["/foo/bar.wav"])
     end
 
     def test_sample_with_various_args
-      @mock_sound.expects(:trigger_sampler).with("/foo/bar.wav",  {})
-      @mock_sound.sample :loop_amen
+      @lang.expects(:trigger_sampler).with("/foo/bar.wav",  {})
+      @lang.instance_eval do
+        sample :loop_amen
+      end
 
-      @mock_sound.expects(:trigger_sampler).with("/foo/bar.wav", {rate: 2})
-      @mock_sound.sample :loop_amen, rate: 2
+      @lang.expects(:trigger_sampler).with("/foo/bar.wav", {rate: 2})
+      @lang.instance_eval do
+        sample :loop_amen, rate: 2
+      end
 
-      # # This works in the codebase, but need to figure out a nice way of
-      # # testing it...
-      # # @mock_sound.expects(:trigger_sampler).with(:loop_amen, 42, 2, {rate: 2})
-      # # @mock_sound.sample lambda { :loop_amen }, rate: 2
+      @lang.expects(:trigger_sampler).with("/foo/bar.wav", {rate: 2})
+      @lang.instance_eval do
+        sample lambda { :loop_amen }, rate: 2
+      end
 
       #Single hash
-      @mock_sound.expects(:trigger_sampler).with("/foo/bar.wav", {rate: 2})
-      @mock_sound.sample path: :loop_amen, rate: 2
+      @lang.expects(:trigger_sampler).with("/foo/bar.wav", {rate: 2})
+      @lang.instance_eval do
+        sample path: :loop_amen, rate: 2
+      end
 
       # Hash and args
-      @mock_sound.expects(:trigger_sampler).with("/foo/bar.wav", {rate: 2})
-      @mock_sound.sample({path: :loop_amen}, {rate: 2})
+      @lang.expects(:trigger_sampler).with("/foo/bar.wav", {rate: 2})
+      @lang.instance_eval do
+        sample({path: :loop_amen}, {rate: 2})
+      end
     end
 
   end
