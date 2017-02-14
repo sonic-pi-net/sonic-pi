121df0d7eb0d4733356f150222c408c7f0343ada
Sam Aaron <samaaron@gmail.com>
2017-02-14 22:01:20 +0000

    Tidy - reduce Ruby interpreter warnings

--- app/server/core.rb
+++ app/server/core.rb
@@ -837,7 +837,7 @@ class Object
   # The hidden singleton lurks behind everyone
   def metaclass; class << self; self; end; end
 
-  def meta_eval &blk; metaclass.instance_eval &blk; end
+  def meta_eval(&blk); metaclass.instance_eval &blk; end
   # Adds methods to a metaclass
   def meta_def name, &blk
     meta_eval { define_method name, &blk }
