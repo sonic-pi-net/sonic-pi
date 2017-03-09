022ee7d9fdc2006b1d862db8189df14f353e387e
Sam Aaron <samaaron@gmail.com>
2017-03-09 22:55:13 +0000

    Tests - update test_sleep to use MockLang

--- app/server/sonicpi/test/lang/core/test_sleep.rb
+++ app/server/sonicpi/test/lang/core/test_sleep.rb
@@ -3,7 +3,7 @@
 # Full project source: https://github.com/samaaron/sonic-pi
 # License: https://github.com/samaaron/sonic-pi/blob/master/LICENSE.md
 #
-# Copyright 2013, 2014, 2015 by Sam Aaron (http://sam.aaron.name).
+# Copyright 2013, 2014, 2015, 2017 by Sam Aaron (http://sam.aaron.name).
 # All rights reserved.
 #
 # Permission is granted for use, copying, modification, and
@@ -13,47 +13,33 @@
 
 require_relative "../../setup_test"
 require_relative "../../../lib/sonicpi/lang/core"
-require_relative "../../../lib/sonicpi/runtime"
-require_relative "../../../lib/sonicpi/util"
 require 'mocha/setup'
 
 module SonicPi
 
   class SleepTester < Minitest::Test
-    include SonicPi::Lang::Core
-    include SonicPi::RuntimeMethods
-    include SonicPi::Util
-
     def setup
-      Kernel.stubs(:sleep)
-
-      @mod_sound_studio = mock()
-      @mod_sound_studio.expects(:sched_ahead_time).at_least_once.returns(0.5)
-
-      @start_time = Time.now.freeze
-
-      __system_thread_locals.set(:sonic_pi_spider_time, @start_time)
-      __system_thread_locals.set(:sonic_pi_spider_start_time, @start_time)
-
-      SleepTester.any_instance.stubs(:__schedule_delayed_blocks_and_messages!).returns(true)
-
-      use_bpm 60
+      @lang = SonicPi::MockLang.new
     end
 
     def test_basic_sleep
-      sleep 2
-      assert_equal(__current_local_run_time, 2.0)
-      sleep 1
-      assert_equal(__current_local_run_time, 3.0)
+      @lang.instance_eval do
+        sleep 2
+        assert_equal(__current_local_run_time, 2.0)
+        sleep 1
+        assert_equal(__current_local_run_time, 3.0)
+      end
     end
 
     def test_bpm_scaled_sleep
-      use_bpm 120
+      @lang.instance_eval do
+        use_bpm 120
 
-      sleep 2
-      assert_equal(__current_local_run_time, 1.0)
-      sleep 1
-      assert_equal(__current_local_run_time, 1.5)
+        sleep 2
+        assert_equal(__current_local_run_time, 1.0)
+        sleep 1
+        assert_equal(__current_local_run_time, 1.5)
+      end
     end
 
   end
