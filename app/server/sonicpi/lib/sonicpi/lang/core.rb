14bbd2f9ac5aa6cfcbc03263c01bf449f546ec75
Sam Aaron <samaaron@gmail.com>
2017-03-24 21:59:34 +0000

    Core - teach time_warp to work with control deltas
    Commit 3474cda fixed a bug between the interaction between `sync` and the control delta mechanism.
    
    This commit extends this fix to handle the interaction between the control deltas and `time_warp`. We do this in the following way:
    
    1. Remember the control deltas prior to the `time_warp` and then restore them after the time warp has concluded
    2. Reset the control deltas for every time_warp block execution.
    
    Note that reseting the control deltas for every block execution assumes that the same time isn’t visited twice within the same time_warp and that within those repeated executions on the same time a specific synth node isn’t controlled more than once.

--- app/server/sonicpi/lib/sonicpi/lang/core.rb
+++ app/server/sonicpi/lib/sonicpi/lang/core.rb
@@ -496,7 +496,10 @@ end"
 
       def time_warp(times=0, params=nil, &block)
         __schedule_delayed_blocks_and_messages!
+
+
         raise "time_warp requires a do/end block" unless block
+        prev_ctl_deltas = __system_thread_locals.get(:sonic_pi_local_control_deltas)
 
         had_params = params
         times = [times] if times.is_a? Numeric
@@ -526,6 +529,7 @@ end"
 
           __system_thread_locals.set :sonic_pi_spider_time, new_time.freeze
           __system_thread_locals.set :sonic_pi_spider_beat, orig_beat + delta
+          __system_thread_locals.set_local :sonic_pi_local_control_deltas, {}
 
           case block.arity
           when 0
@@ -549,6 +553,7 @@ end"
         __system_thread_locals.set :sonic_pi_spider_time, vt_orig
         __system_thread_locals.set :sonic_pi_spider_beat, orig_beat
         __system_thread_locals.set_local :sonic_pi_spider_in_time_warp, already_in_time_warp
+        __system_thread_locals.set_local :sonic_pi_local_control_deltas, prev_ctl_deltas
       end
       doc name:           :time_warp,
           introduced:     Version.new(2,11,0),
