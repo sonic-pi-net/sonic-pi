d855a7a8e33064b2708dfdb6b1021b2c6eafa3df
Sam Aaron <samaaron@gmail.com>
2017-06-28 23:14:15 +0100

    Sound - teach live_audio the :stop command
    live_audio :foo #=> start listening on input 1
    sleep 2
    live_audio :foo, :stop #=> stop listening to input

--- app/server/sonicpi/lib/sonicpi/lang/sound.rb
+++ app/server/sonicpi/lib/sonicpi/lang/sound.rb
@@ -204,7 +204,7 @@ module SonicPi
         info = Synths::SynthInfo.get_info(sn_sym)
         synth_name = info ? info.scsynth_name : synth_name
 
-        if args.size > 1 && args[1] == nil
+        if args.size > 1 && ((args[1] == nil) || (args[1] == :stop))
           # kill synth
           return @mod_sound_studio.kill_live_synth(id)
         end
@@ -233,6 +233,8 @@ However, unlike `play`, `sample` and `synth`, which allow multiple similar synth
 An additional difference is that `live_audio` will create an infinitely long synth rather than be timed to an envelope like the standard `synth` and `sample` synths. This is particularly suitable for working with continuous incoming audio streams where the source of the audio is unknown (for example, it may be a guitar, an analog synth or an electronic violin). If the source is continuous, then it may not be suited to being stitched together by successive enveloped calls to something like: `synth :sound_in, attack: 0, sustain: 4, release: 0`. If we were to `live_loop` this with a `sleep 4` to match the sustain duration, we would get something that emulated a continuous stream, but for certain inputs you'll hear clicking at the seams between each successive call to `synth` where the final part of the audio signal from the previous synth doesn't precisely match up with the start of the signal in the next synth due to very minor timing differences.
 
 Another important feature of `live_audio` is that it will automatically move an existing `live_audio` synth into the current FX context. This means you can live code the FX chain around the live stream and it will update automatically. See examples.
+
+To stop a `live_audio` synth, use the `:stop` arg: `live_audio :foo, :stop`.
 .
 ",
       examples:       [
