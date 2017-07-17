5e2052df87635afb372930960bc76f27364f121e
Sam Aaron <samaaron@gmail.com>
2017-07-17 19:54:07 +0100

    Erlang Scheduler - don’t crash when sending to an invalid address/port

--- app/server/erlang/pi_server.erl
+++ app/server/erlang/pi_server.erl
@@ -182,7 +182,7 @@ send_later(Tag, BundleTime, {_Tremote,_Tlocal}, Socket, Host, Port, Cmd) ->
         true ->
             io:format("Ignoring negative sleep: ~p~n", [MsDelay])
     end,
-    ok = gen_udp:send(Socket, Host, Port, Bin).
+    spawn(fun() -> gen_udp:send(Socket, Host, Port, Bin) end).
     %% io:format("Group (~p) Sending to ~p:~p => ~p~n",[Tag,Host, Port, Cmd]).
 
 %% send_later(BundleTime, {Tremote,Tlocal}, Socket, Host, Port, Cmd) ->
