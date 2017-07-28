9016bd4786d6d936931a1f13c8df690bbecb102a
Sam Aaron <samaaron@gmail.com>
2017-07-28 22:05:23 +0100

    GUI - update reported version

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -175,7 +175,7 @@ MainWindow::MainWindow(QApplication &app, bool i18n, QSplashScreen* splash)
   show_rec_icon_a = false;
   restoreDocPane = false;
   focusMode = false;
-  version = "2.12.0-dev";
+  version = "3.0.1";
   latest_version = "";
   version_num = 0;
   latest_version_num = 0;
