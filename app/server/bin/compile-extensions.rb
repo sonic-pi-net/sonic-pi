e1bb1e81fb9dff9e05db0c717908131d1355383d
Sam Aaron <samaaron@gmail.com>
2017-10-30 12:46:41 +0000

    Vendor - upgrade rugged lib to 0.26

--- app/server/bin/compile-extensions.rb
+++ app/server/bin/compile-extensions.rb
@@ -38,7 +38,7 @@ FileUtils.mkdir_p native_dir
 # Rugged is used for storing the user's ruby music scripts in Git
 # FFI is used for MIDI lib support
 native_ext_dirs = [
-  File.expand_path(File.dirname(__FILE__) + '/../vendor/rugged-0.25.1/ext/rugged'),
+  File.expand_path(File.dirname(__FILE__) + '/../vendor/rugged-0.26.0/ext/rugged'),
   File.expand_path(File.dirname(__FILE__) + '/../vendor/ffi-1.9.17/ext/ffi_c'),
   File.expand_path(File.dirname(__FILE__) + '/../vendor/atomic/ext'),
   File.expand_path(File.dirname(__FILE__) + '/../vendor/ruby-prof-0.15.8/ext/ruby_prof/'),
