5bee9a728690e5c7297afeb690e52f14bb86320b
Xavier Riley <xavriley@users.noreply.github.com>
2017-12-20 11:38:38 +0000

    Revamp nested_pattern as pattern
    Nested patterns were implemented in an unloaded file since version 2.8.0 before rings were available. This commit brings them up to date to use the ring API, with the addition of a special method called `.tatum` to access the time to sleep for. The choice of `tatum` as the method name builds on a convention of using this to refer to the smallest rhythmic subdivision that a listener perceives: https://en.wikipedia.org/wiki/Tatum_(music)

--- app/server/ruby/lib/sonicpi/lang/pattern.rb
+++ app/server/ruby/lib/sonicpi/lang/pattern.rb
@@ -4,7 +4,7 @@
 # Full project source: https://github.com/samaaron/sonic-pi
 # License: https://github.com/samaaron/sonic-pi/blob/master/LICENSE.md
 #
-# Copyright 2013, 2014, 2015, 2016 by Sam Aaron (http://sam.aaron.name).
+# Copyright 2013, 2014, 2015, 2016, 2017 by Sam Aaron (http://sam.aaron.name).
 # All rights reserved.
 #
 # Permission is granted for use, copying, modification, and
@@ -12,17 +12,157 @@
 # notice is included.
 #++
 require_relative 'support/docsystem'
+require_relative 'core'
 require_relative "../version"
 require_relative "../util"
+
+
+module SonicPi
+  module Core
+    class PatternVector < RingVector
+      def tick(*args)
+        super(*args)[:val]
+      end
+
+      def look(*args)
+        super(*args)[:val]
+      end
+
+      def tatum(*args)
+        idx = SonicPi::Core::ThreadLocalCounter.look(*args)
+        self[idx][:beats_to_sleep_for]
+      end
+
+      # TODO: implement .play_each, .sample_each and .call_each
+      # The three obvious use cases are patterns made of notes,
+      # samples and lambdas. It would be nice to define a shorthand
+      # for playing each event in a loop like so:
+      #
+      #    def play_each(*args)
+      #      self.each do |step|
+      #        SonicPiLang.play step[:val], *args
+      #        SonicPiLang.sleep step[:beats_to_sleep_for]
+      #      end
+      #    end
+      #
+      # however this is difficult as play, sample and sleep aren't
+      # in scope here.
+
+      def ___sp_vector_name
+        "pattern"
+      end
+    end
+  end
+end
 
 module SonicPi
   module Lang
     module Pattern
 
-
       include SonicPi::Util
       include SonicPi::Lang::Support::DocSystem
 
+      def pattern(pattern, *args)
+        args_h = resolve_synth_opts_hash_or_array(args)
+        args_h[:beat_length] = 1 unless args_h[:beat_length]
+
+        SonicPi::Core::PatternVector.new(nested_beat(pattern,
+                                                     calculate_number_of_beats(pattern)).flatten)
+      end
+      doc name:          :pattern,
+      introduced:    Version.new(3,1,0),
+      summary:       "Create patterns of values",
+      doc:           "Using a nested array to represent rhythm, you can use this method to structure melodies, beats and other rhythmic patterns.
+
+  Patterns are a special kind of ring - values are accessed like a normal ring with tick and look. The sleep timings are accessed with a special method called tatum.
+
+  A nested array is just an array that contains other arrays inside it e.g. `[1, [2, 2], 1, 1]`. Here we can see three elements at the top level of the array (the ones) and two elements which are nested two arrays deep (the twos).
+
+  We can use this nesting to play through the contents of the array at faster and faster rates. Things nested at the second level will play twice as fast, things nested at the third level will play four times as fast and so on.
+
+  It might help to think about music notation - if the level 1 is a crotchet/quarter note, then level two is like a quaver/eighth note and so on, all the way down to hemidemisemiquavers and beyond.
+
+  If you want to use a triplet rhythm you can use a special notation to spread across multiple beats e.g. `[:d5, :cs5, {over: 2, val: [:c5,:c5,:c5]}, :b4, :bb4, :a4]`. This spaces the three `:c5` notes over the space of two normal notes which gives you a quaver triplet rhythm. You might recognize this from Bizet's opera Carmen.
+
+  For advanced usage, you can iterate over the events in a pattern. Each event yields a hash containing a `:val` key and a `:beats_to_sleep_for` key. See the advanced example below for more information on how to use this.",
+      args:          [[:pattern]],
+      opts:          {num_beats: "Number of beats in the pattern - defaults to 4"},
+      accepts_block: false,
+      examples:      ["
+      pat = pattern([:c, [:d, :f], :e, :c])
+      loop do
+        play pat.tick
+        sleep pat.tatum
+      end
+                                # Same as:
+                                #   play :c
+                                #   sleep 1
+                                #   play :d
+                                #   sleep 0.5
+                                #   play :f
+                                #   sleep 0.5
+                                #   play :e
+                                #   sleep 1
+                                #   play :c
+                                #   sleep 1
+      ","
+      # We can also use sample names with `:samples` as the mode option
+      rock_you_pattern =
+        pattern([:bd_haus, :elec_snare, [:bd_haus, :bd_haus], :elec_snare])
+
+      loop do
+        sample rock_you_pattern.tick
+        sleep rock_you_pattern.tatum
+      end # Same as:
+                                #   sample :bd_haus
+                                #   sleep 1
+                                #   sample :elec_snare
+                                #   sleep 1
+                                #   sample :bd_haus
+                                #   sleep 0.5
+                                #   sample :bd_haus
+                                #   sleep 0.5
+                                #   sample :elec_snare
+                                #   sleep 1
+      ","
+      # Triplets and cross rhythms
+      # By using a hash with :over and :val keys, we can spread, for example, three notes over the space of two
+      # This creates the equivalent of triplets in music
+      carmen_pattern = pattern([:d5, :cs5, {over: 2, val: [:c5,:c5,:c5]}, :b4, :bb4, :a4, nil])
+      # This spaces the three `:c5` notes (our `:val`) over the space of two (our `:over`) normal notes which gives you a quaver triplet rhythm.
+      # You might recognize this from Bizet's opera Carmen.
+      loop do
+        play carmen_pattern.tick
+        sleep carmen_pattern.tatum
+      end
+      ",
+        "
+      # Advanced version - a randomised drum beat
+      live_loop :beatz do
+        use_bpm 120
+
+        bd = lambda { sample :bd_haus, rate: 2}
+        sn = lambda { sample :drum_snare_hard, rate: 4 }
+        hh = lambda { sample :drum_cymbal_closed, rate: rrand(3,4)}
+        rest = lambda { nil }
+
+        drumbeat = pattern([bd, sn, [bd, [bd,bd]], [sn, hh]])
+        drumbreak = pattern([[bd,bd,bd,bd], {over: 2, val: [sn,sn,sn]}, [rest,hh,rest,hh]])
+
+        3.times do
+          drumbeat.each do |step|
+            step[:val].call
+            sleep step[:beats_to_sleep_for]
+          end
+        end
+        # We use .shuffle to get a different break each time
+        drumbreak.shuffle.each do |step|
+          step[:val].call
+          sleep step[:beats_to_sleep_for]
+        end
+      end"]
+
+      private
       def nested_beat(el, beat_length=nil)
         if el.kind_of? Hash and el.keys == [:over, :val]
           # We have a triple syntax hash
@@ -47,7 +187,7 @@ module SonicPi
 
           el.map {|x| nested_beat(x, beat_length) }
         else
-          {level: beat_length, step: el}
+          {beats_to_sleep_for: beat_length, val: el}
         end
       end
 
@@ -63,121 +203,6 @@ module SonicPi
           end
         }
       end
-
-      def get_nested_beat(pattern)
-        nested_beat(pattern).flatten.map do |step|
-          [(1.0/step[:level]), step[:step]]
-        end
-      end
-
-      def play_nested_pattern(pattern, *args)
-        args_h = resolve_synth_opts_hash_or_array(args)
-        args_h[:mode] = :notes unless args_h[:mode]
-        args_h[:beat_length] = 1 unless args_h[:beat_length]
-
-        if [:notes, :samples, :lambdas].none? {|x| x == args_h[:mode] }
-          raise "Unrecognized :mode for play_nested_pattern. Must be one of [:notes, :samples, :lambdas]"
-        end
-
-        get_nested_beat(pattern).each do |mul, val|
-          with_bpm_mul mul do
-            case args_h[:mode]
-            when :notes
-              play val
-            when :samples
-              sample val
-            when :lambdas
-              val.call
-            end
-            sleep args_h[:beat_length] # this is scaled by the bpm mul
-          end
-        end
-      end
-      doc name:          :play_nested_pattern,
-      introduced:    Version.new(2,8,0),
-      summary:       "Play a nested pattern of notes, samples or lambdas",
-      doc:           "Using a nested array to represent rhythm, you can use this method to structure melodies, beats and other rhythmic patterns.
-
-  A nested array is just an array that contains other arrays inside it e.g. `[1, [2, 2], 1, 1]`. Here we can see three elements at the top level of the array (the ones) and two elements which are nested two arrays deep (the twos).
-
-  We can use this nesting to play through the contents of the array at faster and faster rates. Things nested at the second level will play twice as fast, things nested at the third level will play four times as fast and so on.
-
-  It might help to think about music notation - if the level 1 is a crotchet/quarter note, then level two is like a quaver/eighth note and so on, all the way down to hemidemisemiquavers and beyond.
-
-  If you want to use a triplet rhythm you can use a special notation to spread across multiple beats e.g. `[:d5, :cs5, {over: 2, val: [:c5,:c5,:c5]}, :b4, :bb4, :a4]`. This spaces the three `:c5` notes over the space of two normal notes which gives you a quaver triplet rhythm. You might recognize this from Bizet's opera Carmen.",
-      args:          [[:pattern]],
-      opts:          {beat_length: "Length of a single (top level) beat - defaults to 1",
-        mode: "One of `:notes`, `:samples` or `:lambdas` depending on what is in your nested pattern. See examples below."},
-      accepts_block: false,
-      examples:      ["
-      play_nested_pattern [:c, [:d, :f], :e, :c]
-                                # Same as:
-                                #   play :c
-                                #   sleep 1
-                                #   play :d
-                                #   sleep 0.5
-                                #   play :f
-                                #   sleep 0.5
-                                #   play :e
-                                #   sleep 1
-                                #   play :c
-                                #   sleep 1
-      ","
-      # We can also use sample names with `:samples` as the mode option
-      rock_you_pattern =
-        [:bd_haus, :elec_snare, [:bd_haus, :bd_haus], :elec_snare]
-
-      play_nested_pattern(rock_you_pattern, mode: :samples) # Same as:
-                                #   sample :bd_haus
-                                #   sleep 1
-                                #   sample :elec_snare
-                                #   sleep 1
-                                #   sample :bd_haus
-                                #   sleep 0.5
-                                #   sample :bd_haus
-                                #   sleep 0.5
-                                #   sample :elec_snare
-                                #   sleep 1
-      ","
-      # We can also use lambdas with `:lambdas` as the mode option
-      rand_notes = lambda {
-        notes = scale(:c3, :minor_pentatonic, num_octaves: 3)
-        play_pattern_timed(notes.shuffle.take(3), 0.33)
-      }
-      random_pattern = [rand_notes, rand_notes, [rand_notes, rand_notes], rand_notes]
-      loop do
-        # because we already sleep inside play_pattern_timed,
-        # we set the :beat_length to zero
-        play_nested_pattern(random_pattern, mode: :lambdas, beat_length: 0)
-      end
-      ","
-      # Triplets and cross rhythms
-      # By using a hash with :over and :val keys, we can spread, for example, three notes over the space of two
-      # This creates the equivalent of triplets in music
-      carmen_pattern = [:d5, :cs5, {over: 2, val: [:c5,:c5,:c5]}, :b4, :bb4, :a4]
-      # This spaces the three `:c5` notes (our `:val`) over the space of two (our `:over`) normal notes which gives you a quaver triplet rhythm.
-      # You might recognize this from Bizet's opera Carmen.
-      play_nested_pattern(carmen_pattern, beat_length: 0.5)
-      ",
-        "
-      # Advanced version - a randomised drum beat
-      live_loop :beatz do
-        use_bpm 120
-
-        bd = lambda { sample :bd_haus, rate: 2}
-        sn = lambda { sample :drum_snare_hard, rate: 4 }
-        hh = lambda { sample :drum_cymbal_closed, rate: rrand(3,4)}
-        rest = lambda { nil }
-
-        drumbeat = [bd, sn, [bd, [bd,bd]], [sn, hh]]
-        drumbreak = [[bd,bd,bd,bd], {over: 2, val: [sn,sn,sn]}, [rest,hh,rest,hh]]
-
-        3.times do
-          play_nested_pattern(drumbeat, mode: :lambdas)
-        end
-        # We use .shuffle to get a different break each time
-        play_nested_pattern(drumbreak.shuffle, mode: :lambdas)
-      end"]
 
     end
   end
