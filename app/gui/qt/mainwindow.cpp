8a9a26e0da1a0f954926780c3df2a43d10cdf945
Sam Aaron <samaaron@gmail.com>
2018-01-22 10:59:40 +0000

    GUI - update version to 3.1.0

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -175,7 +175,7 @@ MainWindow::MainWindow(QApplication &app, bool i18n, QSplashScreen* splash)
   show_rec_icon_a = false;
   restoreDocPane = false;
   focusMode = false;
-  version = "3.0.1";
+  version = "3.1.0";
   latest_version = "";
   version_num = 0;
   latest_version_num = 0;
