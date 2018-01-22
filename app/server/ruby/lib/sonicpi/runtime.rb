17fe8e4243e39a59edc7ac2cdb6bc8f34ce0fc57
Sam Aaron <samaaron@gmail.com>
2018-01-22 12:44:18 +0000

    Version - we’re pretty much at 3.1

--- app/server/ruby/lib/sonicpi/runtime.rb
+++ app/server/ruby/lib/sonicpi/runtime.rb
@@ -1263,7 +1263,7 @@ module SonicPi
       gh_short = @git_hash ? "-#{@git_hash[0, 5]}" : ""
       @settings = Config::Settings.new(user_settings_path)
 
-      @version = Version.new(3, 1, 0, "dev#{gh_short}")
+      @version = Version.new(3, 1, 0)
       @server_version = __server_version
       @life_hooks = LifeCycleHooks.new
       @msg_queue = msg_queue
