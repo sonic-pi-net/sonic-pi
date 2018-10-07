655e58db5d7f1c107f8cac608a808a975c951f3d
Sam Aaron <samaaron@gmail.com>
2018-10-07 21:31:32 +0100

    Tidy - fix whitespace

--- app/server/ruby/test/lang/sound/test_sound.rb
+++ app/server/ruby/test/lang/sound/test_sound.rb
@@ -16,53 +16,53 @@ require_relative "../../../lib/sonicpi/lang/sound"
 
 module SonicPi
   class SoundTester < Minitest::Test
-    
+
     def setup
       @mock_sound = Object.new
       @mock_sound.extend(Lang::Sound)
     end
-    
+
     def test_rest
       assert_equal(false, @mock_sound.rest?(1))
-      
+
       assert_equal(true, @mock_sound.rest?(:rest))
       assert_equal(true, @mock_sound.rest?(:r))
       assert_equal(false, @mock_sound.rest?(:norest))
-      
+
       assert_equal(true, @mock_sound.rest?(nil))
-      
+
       assert_equal(false, @mock_sound.rest?(Hash.new))
-      
+
       assert_equal(false, @mock_sound.rest?("a string"))
     end
-    
+
     def test_truthy
       assert_equal(false, @mock_sound.truthy?(0))
       assert_equal(true, @mock_sound.truthy?(1))
-      assert_equal(true, @mock_sound.truthy?(-1))    
+      assert_equal(true, @mock_sound.truthy?(-1))
       assert_equal(true, @mock_sound.truthy?(0.01))
-      
+
       assert_equal(false, @mock_sound.truthy?(nil))
-      
+
       assert_equal(true, @mock_sound.truthy?(true))
       assert_equal(false, @mock_sound.truthy?(false))
-      
+
       proc = Proc.new {true}
       assert_equal(true, @mock_sound.truthy?(proc))
     end
-    
+
     def test_should_trigger
       h = {on: true, a: 123, c: "d"}
       assert_equal(true, @mock_sound.should_trigger?(h))
       assert_equal(false, h.has_key?(:on))
-      
+
       h = {on: false, a: 123, c: "d"}
       assert_equal(false, @mock_sound.should_trigger?(h))
       assert_equal(false, h.has_key?(:on))
-      
+
       h = {a: 123, c: "d"}
       assert_equal(true, @mock_sound.should_trigger?(h))
     end
-  
+
   end
 end
