9c5fc65ba471412f6f0e4aa72878fe3a44b15463
Sam Aaron <samaaron@gmail.com>
2019-07-18 12:01:41 +0100

    Logs - store the last 10 log sessions
    Create a history directory within the logs directory which stores the last 10 sessions labelled by timestamp.

--- app/server/ruby/bin/clear-logs.rb
+++ app/server/ruby/bin/clear-logs.rb
@@ -12,13 +12,44 @@
 #++
 require 'tmpdir'
 require 'fileutils'
+require 'time'
 
 require_relative "../core.rb"
 require_relative "../lib/sonicpi/util"
 
 include SonicPi::Util
 
-# Clear out all logs (don't remove the files, just empty them)
-Dir["#{log_path}/*"].each do |p|
+history_dir = "#{log_path}/history/#{Time.now.to_s}"
+FileUtils.mkdir_p(history_dir)
+
+Dir["#{log_path}/*.log"].each do |p|
+  # Copy log to history directory
+  FileUtils.cp(p, "#{history_dir}/")
+  # Clear out all logs (don't remove the files, just empty them)
   File.open(p, 'w') {|file| file.truncate(0) }
+end
+
+# clean up old history logs
+# only store the last 10 sessions
+num_sessions_to_store = 10
+
+history_dirs = Dir.glob("#{log_path}/history/*")
+
+timestamps = history_dirs.map do |d|
+  begin
+    Time.parse File.basename(d)
+  rescue
+    nil
+  end
+end
+
+timestamps.compact!
+num_timestamps = timestamps.size
+num_to_drop = num_timestamps - num_sessions_to_store
+
+if num_to_drop > 0
+  timestamps.sort.take(num_to_drop).each do |ts|
+    dir = ts.to_s
+    FileUtils.rm_rf("#{log_path}/history/#{dir}")
+  end
 end
