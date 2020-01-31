2e1f6d88b5b715ccff0e2a5afb7a6b9bace7ea54
SunderB <20426079+SunderB@users.noreply.github.com>
2020-01-31 15:57:44 +0000

    Improve cmake build Linux compatibility
    * Add Threads to the link libraries, and librt to the link libs on UNIX only
    * Make config.sh and prebuild.sh
    * Update config.bat and prebuild.bat with more info messages
    * Fix app/gui/qt/external/QScintilla-2.11.4/CMakeLists.txt so it detects Linux properly

--- app/gui/qt/prebuild.bat
+++ app/gui/qt/prebuild.bat
@@ -2,9 +2,12 @@ cd %~dp0
 
 rmdir /S /Q ..\..\server\ruby\vendor\ruby-aubio-prerelease
 
+echo "Translating tutorial..."
 ..\..\server\native\ruby\bin\ruby ../../server/ruby/bin/i18n-tool.rb -t
+
+echo "Generating docs for the Qt GUI..."
 copy /Y utils\ruby_help.tmpl utils\ruby_help.h
 ..\..\server\native\ruby\bin\ruby ../../server/ruby/bin/qt-doc.rb -o utils/ruby_help.h
 
-REM Do the language translation
+echo "Updating GUI translation files..."
 forfiles /p lang /s /m *.ts /c "cmd /c %QT_INSTALL_LOCATION%/bin/lrelease @file"
