5fb52e254b032a4fb4ab6987e279ca514f35b69c
Chris Maughan <cmaughan@snarlsoftware.com>
2020-02-03 19:41:09 +0000

    Fixes for the Installer
    Path to ruby was incorrect, resulting in no ruby server code
    Path to release binary was incorrect, meaning installer put it in the
    wrong place
    Add missing server plugins
    Fix prebuild to copy them
    Prune contents
    Add plugins
    Filter gui.wix to remove extra copy of the exe

--- install/windows/wix/gen_wix.bat
+++ install/windows/wix/gen_wix.bat
@@ -1,9 +1,8 @@
 cd %~dp0
 cd ..
-heat dir etc -gg -g1 -sfrag -dr SonicPi -cg ETC -out etc.wxs
-heat dir app\gui -gg -g1 -sfrag -dr AppDir -cg GUI -out gui.wxs
-heat dir app\server -gg -g1 -sfrag -dr AppDir -cg SERVER -out server.wxs
 
-echo Update version number in wix\sonic-pi.wxs
-echo Remove duplicate sonic-pi.exe info from wix\gui.wxs
-echo Then run gen_msi.bat
+@echo "Generating gui, server, etc wix files"
+heat dir etc -gg -g1 -f -sfrag -dr SonicPi -cg ETC -out etc.wxs
+heat dir app\gui -gg -g1 -v -t "wix\filter.xslt" -sreg -sfrag -dr AppDir -cg GUI -out gui.wxs
+heat dir app\server -gg -g1 -sreg -sfrag -dr AppDir -cg SERVER -out server.wxs
+
