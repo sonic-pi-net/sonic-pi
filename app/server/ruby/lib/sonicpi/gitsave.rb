6d9acf06aee40f4cc9b768070f15aea0964a83a7
Sam Aaron <samaaron@gmail.com>
2020-02-16 20:19:15 +0000

    Fix Warning - mismatched indentations at 'end' with 'module'

--- app/server/ruby/lib/sonicpi/gitsave.rb
+++ app/server/ruby/lib/sonicpi/gitsave.rb
@@ -15,56 +15,55 @@ begin
   require 'rugged'
 
   module SonicPi
-  class GitSave
+    class GitSave
 
-    def initialize(path)
-      path = path.encode('utf-8')
-      @path = path
-      begin
-        @repo = Rugged::Repository.new(path + '.git')
-      rescue
+      def initialize(path)
+        path = path.encode('utf-8')
+        @path = path
         begin
-          @repo = Rugged::Repository.init_at path, false
+          @repo = Rugged::Repository.new(path + '.git')
         rescue
-          # Repo is malformed - nuke it for now!
-          FileUtils.rm_rf path + '.git'
-          @repo = Rugged::Repository.init_at path, false
+          begin
+            @repo = Rugged::Repository.init_at path, false
+          rescue
+            # Repo is malformed - nuke it for now!
+            FileUtils.rm_rf path + '.git'
+            @repo = Rugged::Repository.init_at path, false
+          end
         end
       end
+
+      def save!(filename, content, msgpre="")
+        oid = @repo.write(content, :blob)
+        index = @repo.index
+        index.reload
+        index.add(:path => filename, :oid => oid, :mode => 0100644)
+
+        options = {}
+        options[:tree] = index.write_tree(@repo)
+
+        options[:author] = { :email => "autosave@sonic-pi.net", :name => 'Sonic Pi Autosave', :time => Time.now }
+        options[:committer] = { :email => "autosave@sonic-pi.net", :name => 'Sonic Pi Autosave', :time => Time.now }
+        options[:message] ||= "#{msgpre} :~: Autosave Workspace #{filename}"
+        options[:parents] = @repo.empty? ? [] : [ @repo.head.target ].compact
+        options[:update_ref] = 'HEAD'
+
+        Rugged::Commit.create(@repo, options)
+      end
+
+
     end
-
-    def save!(filename, content, msgpre="")
-      oid = @repo.write(content, :blob)
-      index = @repo.index
-      index.reload
-      index.add(:path => filename, :oid => oid, :mode => 0100644)
-
-      options = {}
-      options[:tree] = index.write_tree(@repo)
-
-      options[:author] = { :email => "autosave@sonic-pi.net", :name => 'Sonic Pi Autosave', :time => Time.now }
-      options[:committer] = { :email => "autosave@sonic-pi.net", :name => 'Sonic Pi Autosave', :time => Time.now }
-      options[:message] ||= "#{msgpre} :~: Autosave Workspace #{filename}"
-      options[:parents] = @repo.empty? ? [] : [ @repo.head.target ].compact
-      options[:update_ref] = 'HEAD'
-
-      Rugged::Commit.create(@repo, options)
-    end
-
-
   end
-end
 
 rescue LoadError
- # Rugged isn't available - don't attempt to save buffers into a local git repo
-module SonicPi
-  class GitSave
-    def initialize(*args)
-    end
+  # Rugged isn't available - don't attempt to save buffers into a local git repo
+  module SonicPi
+    class GitSave
+      def initialize(*args)
+      end
 
-    def save!(*args)
+      def save!(*args)
+      end
     end
   end
-end
-
 end
