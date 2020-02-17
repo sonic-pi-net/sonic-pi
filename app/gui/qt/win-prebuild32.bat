73a90de429b16ec80cacb03186a48eff6f962075
SunderB <20426079+SunderB@users.noreply.github.com>
2020-02-17 13:02:08 +0000

    Attempt to fix win-prebuild.bat & win-prebuild32.bat so it copies prebuilt binaries to app/server/native folder successfully

--- app/gui/qt/win-prebuild32.bat
+++ app/gui/qt/win-prebuild32.bat
@@ -1,15 +1,15 @@
 cd %~dp0
 call ..\..\server\external\win_x86_build_externals.bat
 
-@echo "Copying scsynth to server..."
-xcopy /Y /I /R /E ..\..\..\prebuilt\windows\x86\*.* ..\..\server\native
+@echo Copying scsynth to server...
+xcopy /Y /I /R /E ..\..\..\prebuilt\windows\x86\* ..\..\server\native\
 
-@echo "Translating tutorial..."
+@echo Translating tutorial...
 ..\..\server\native\ruby\bin\ruby ../../server/ruby/bin/i18n-tool.rb -t
 
-@echo "Generating docs for the Qt GUI..."
+@echo Generating docs for the Qt GUI...
 copy /Y utils\ruby_help.tmpl utils\ruby_help.h
 ..\..\server\native\ruby\bin\ruby ../../server/ruby/bin/qt-doc.rb -o utils/ruby_help.h
 
-@echo "Updating GUI translation files..."
+@echo Updating GUI translation files...
 forfiles /p lang /s /m *.ts /c "cmd /c %QT_INSTALL_LOCATION32%/bin/lrelease @file"
