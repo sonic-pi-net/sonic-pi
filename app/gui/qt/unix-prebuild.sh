5a7bb8dac061bf380dd694c0fc927d59c2b6e86a
SunderB <20426079+SunderB@users.noreply.github.com>
2020-02-24 19:01:37 +0000

    Build Aubio and Osmid during prebuild on Unix
    Build Aubio dependencies with position independent code to allow for
    more successful compilation on Unix-like systems, and account for extra
    'lib' prefix on Windows only.

--- app/gui/qt/unix-prebuild.sh
+++ app/gui/qt/unix-prebuild.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 set -e # Quit script on error
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#dont remove ruby-aubio-prerelease  as needed in linux build
+echo "Warning: Unix build scripts are still a work in progress!"
+
+# Build external dependencies
+"${SCRIPT_DIR}/external/unix_build_externals.sh"
+
+# Install dependencies to server
+echo "Copying external dependencies to the server..."
+mkdir -p "${SCRIPT_DIR}/../../server/native/lib"
+cp "${SCRIPT_DIR}/external/build/aubio-prefix/src/Aubio-build/libaubio-5.so" "${SCRIPT_DIR}/../../server/native/lib/"
+
+mkdir -p "${SCRIPT_DIR}/../../server/native/osmid"
+cp "${SCRIPT_DIR}/external/build/osmid-prefix/src/osmid-build/o2m" "${SCRIPT_DIR}/../../server/native/osmid/"
+cp "${SCRIPT_DIR}/external/build/osmid-prefix/src/osmid-build/m2o" "${SCRIPT_DIR}/../../server/native/osmid/"
+
+#dont remove ruby-aubio-prerelease, as needed in linux build
 #it is removed in the windows-prebuild
+
 echo "Translating tutorial..."
 #assumes linux uses system ruby
 #so dont use prefix ../../server/native/ruby/bin/ruby, as unnecessary to set this up
