2c1199a47853981ea01a171ae253f313dbc83f01
Sam Aaron <samaaron@gmail.com>
2020-03-10 01:08:47 +0000

    Vendor - remove hamster as a dependency

--- app/server/ruby/vendor/hamster-3.0.0/lib/hamster/undefined.rb
+++
@@ -1,5 +0,0 @@
-module Hamster
-  # @private
-  module Undefined
-  end
-end
--- app/server/ruby/vendor/hamster-3.0.0/lib/hamster/version.rb
+++
@@ -1,5 +0,0 @@
-module Hamster
-  # Current released gem version. Note that master will often have the same
-  # value as a release gem but with different code.
-  VERSION = "3.0.0"
-end
