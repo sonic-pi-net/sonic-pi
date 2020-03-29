67eb5916dbcef1cea02f7223b13828f188637b42
Sam Aaron <samaaron@gmail.com>
2020-03-29 13:10:23 +0100

    Test - fix tests now that we've reverted to the pure Ruby OSC

--- app/server/ruby/test/test_osc.rb
+++ app/server/ruby/test/test_osc.rb
@@ -18,8 +18,8 @@ module SonicPi
   class OSCTester < Minitest::Test
 
     def test_basic_address_encoding
-      encoder = FastOsc
-      decoder = FastOsc
+      decoder = OscDecode.new(true)
+      encoder = OscEncode.new(true)
 
       address = "/foo"
 
@@ -31,8 +31,8 @@ module SonicPi
 
 
     def test_args_encoding_multiple
-      encoder = FastOsc
-      decoder = FastOsc
+      decoder = OscDecode.new(true)
+      encoder = OscEncode.new(true)
 
       address = "/feooblah"
 
