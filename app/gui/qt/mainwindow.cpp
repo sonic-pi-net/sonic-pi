b511aad4f04f5635269de25059bd21cecc2ff979
Sam Aaron <samaaron@gmail.com>
2020-03-30 10:47:45 +0100

    Version - update to v3.2.1 ready for new release

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -123,7 +123,7 @@ MainWindow::MainWindow(QApplication &app, bool i18n, QSplashScreen* splash)
     show_rec_icon_a = false;
     restoreDocPane = false;
     focusMode = false;
-    version = "3.2.0";
+    version = "3.2.1";
     latest_version = "";
     version_num = 0;
     latest_version_num = 0;
