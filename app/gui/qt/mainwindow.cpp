701c00fac9c9cf4495ad0f5359cfdba9beb4c9bf
Sam Aaron <samaaron@gmail.com>
2020-04-05 10:42:35 +0100

    Version - update to v3.2.2

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -123,7 +123,7 @@ MainWindow::MainWindow(QApplication &app, bool i18n, QSplashScreen* splash)
     show_rec_icon_a = false;
     restoreDocPane = false;
     focusMode = false;
-    version = "3.2.1";
+    version = "3.2.2";
     latest_version = "";
     version_num = 0;
     latest_version_num = 0;
--- app/server/ruby/lib/sonicpi/runtime.rb
+++ app/server/ruby/lib/sonicpi/runtime.rb
@@ -1260,7 +1260,7 @@ module SonicPi
       @git_hash = __extract_git_hash
       gh_short = @git_hash ? "-#{@git_hash[0, 5]}" : ""
       @settings = Config::Settings.new(user_settings_path)
-      @version = Version.new(3, 2, 1)
+      @version = Version.new(3, 2, 2)
       @server_version = __server_version
       @life_hooks = LifeCycleHooks.new
       @msg_queue = msg_queue
