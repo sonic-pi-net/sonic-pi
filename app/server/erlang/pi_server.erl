6ad9c0b11567b98b61c052a4d409eeb1faed452f
Richard Carlsson <carlsson.richard@gmail.com>
2020-04-14 19:04:34 +0200

    Break out logging and debugging to a util module

--- app/server/erlang/pi_server.erl
+++ app/server/erlang/pi_server.erl
@@ -55,49 +55,11 @@
 %% are forwarded directly without starting a timer.
 -define(NODELAY_LIMIT, 1).
 
-%% set to 0 for no debugging messages
--define(DEBUG_LEVEL, 0).
-
-%% set to 0 for no log messages
--define(LOG_LEVEL, 1).
-
 %% time between idling messages
 -define(IDLE_TIME, 60000).
 
-
-debug(Msg, Vals) ->
-    debug(1, Msg, Vals).
-
-debug(Level, Msg, Vals) ->
-    debug(Level, osc:now(), Msg, Vals).
-
-debug(Level, Now, Msg, Vals) when Level =< ?DEBUG_LEVEL ->
-    try io:format("~f: " ++ Msg, [Now|Vals])
-    catch
-        _Class:_Term ->
-            io:format(standard_io,
-                      "** debug format error: string=~p, args=~p~n",
-                      [Msg, Vals])
-    end;
-debug(_Level, _Msg, _Vals, _Now) ->
-    ok.
-
-
-log(Msg) ->
-    log(Msg, []).
-
-log(Msg, Vals) ->
-    log(1, Msg, Vals).
-
-log(Level, Msg, Vals) when Level =< ?LOG_LEVEL ->
-    try io:format(Msg, Vals)
-    catch
-        _Class:_Term ->
-            io:format("** log format error: string=~p, args=~p~n",
-                      [Msg, Vals])
-    end;
-log(_Level, _Msg, _Vals) ->
-    ok.
+-import(pi_server_util,
+        [log/1, log/2, log/3, debug/2, debug/3, debug/4]).
 
 
 cue_server_host() ->
