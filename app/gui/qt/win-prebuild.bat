7867c0a556d43823c2a39330de0f950bc742d8c4
Sam Aaron <samaaron@gmail.com>
2020-05-15 13:04:11 +0000

    Build - improve windows prebuild script
    Now cleans out native dir and also compiles all Erlang files

--- app/gui/qt/win-prebuild.bat
+++ app/gui/qt/win-prebuild.bat
@@ -2,13 +2,21 @@ cd %~dp0
 call external/win_x64_build_externals.bat
 
 cd %~dp0
+
+@echo Cleaning out native dir....
+del ..\..\server\native\*.* /s /q
+rmdir ..\..\server\native\erlang /s /q
+rmdir ..\..\server\native\osmid /s /q
+rmdir ..\..\server\native\plugins /s /q
+
 @echo Copying aubio to the server...
 copy external\build\aubio-prefix\src\aubio-build\Release\libaubio-5.dll ..\..\server\native\ruby\bin
 
 @echo Copying osmid to the server...
+mkdir ..\..\server\native\osmid
 xcopy /Y /I /R /E external\build\osmid-prefix\src\osmid-build\Release\*.exe ..\..\server\native\osmid
 
-@echo Copying scsynth to server...
+@echo Copying all other native files to server...
 xcopy /Y /I /R /E ..\..\..\prebuilt\windows\x64\*.* ..\..\server\native
 
 @echo Translating tutorial...
@@ -20,3 +28,9 @@ copy /Y utils\ruby_help.tmpl utils\ruby_help.h
 
 @echo Updating GUI translation files...
 forfiles /p lang /s /m *.ts /c "cmd /c %QT_INSTALL_LOCATION%\bin\lrelease.exe @file"
+
+@echo Compiling Erlang BEAM files...
+cd %~dp0
+cd ..\..\server\erlang
+forfiles /m *.erl /c "cmd /c ..\native\erlang\bin\erl.exe -compile @fname"
+cd %~dp0
