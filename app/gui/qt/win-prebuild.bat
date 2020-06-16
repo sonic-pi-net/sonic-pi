a482966512123badcb38e9be48cf772dd3d177b2
cmaughan <mornymorny@gmail.com>
2020-06-16 13:43:45 +0100

    Issue #2364
    Fix Lissajous scope: don't start in the center
    Fix win-prebuild to properly copy the erlang file

--- app/gui/qt/win-prebuild.bat
+++ app/gui/qt/win-prebuild.bat
@@ -34,5 +34,5 @@ cd ..\..\server\erlang\sonic_pi_server
 ..\..\native\erlang\bin\erl.exe -make
 cd %~dp0
 cd ..\..\server\erlang\sonic_pi_server
-copy /Y /R src\sonic_pi_server.app.src ebin\sonic_pi_server.app
+xcopy /Y /R src\sonic_pi_server.app.src .\ebin\sonic_pi_server.app
 cd %~dp0
