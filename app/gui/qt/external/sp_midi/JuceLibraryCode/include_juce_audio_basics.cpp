300104a1c14c4a007d736813f8010a5815ee6f79
Sam Aaron <samaaron@gmail.com>
2020-10-05 19:46:07 +0000

    externals - remove old sp_midi

--- app/gui/qt/external/sp_midi/JuceLibraryCode/include_juce_audio_basics.cpp
+++
@@ -1,9 +0,0 @@
-/*
-
-    IMPORTANT! This file is auto-generated each time you save your
-    project - if you alter its contents, your changes may be overwritten!
-
-*/
-
-#include "AppConfig.h"
-#include <juce_audio_basics/juce_audio_basics.cpp>
