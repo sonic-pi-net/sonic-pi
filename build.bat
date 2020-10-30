4e782d2b62b26f38ecdf5a97ae8782c05e752bf0
Marmaduke Woodman <mmwoodman@gmail.com>
2020-10-30 23:21:07 +0100

    comments

--- build.bat
+++ build.bat
@@ -1,16 +1,22 @@
-set QT_INSTALL_LOCATION c:\Qt\5.12.9\msvc2017_64
+@echo on
 
+REM set QT_INSTALL_LOCATION c:\Qt\5.12.9\msvc2017_64
+
+REM install ruby gems
 gem install win32-process
 gem install rugged --version 0.27.1
 gem install ffi
 
+REM make ruby link
 cd app\server\native
 new-item -itemtype symboliclink -path . -name ruby -value D:\rubyinstaller-2.7.1-1-x64
 cd ..\..\..
 
+REM config
 cd app\gui\qt
 win-prebuild.bat
 win-config.bat
 
+REM build
 cd build
 cmake --build . --config Release
