86370c10314bae2b0b7385ea4773bb239b5de12a
Sam Aaron <samaaron@gmail.com>
2021-01-06 01:54:01 +0000

    Build - handy script for turning a standard cmake build into a release-ready app structure on macOS

---
+++ app/gui/qt/mac-release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eux # Quit script on error
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${SCRIPT_DIR}/build/Sonic Pi.app/Contents/Resources"
+
+rm app etc server
+mkdir app
+cp -R ../../../../../../../app/server app/server
+cp -R ../../../../../../../etc .
+ln -s app/server .
+
+mkdir -p app/gui/qt
+cp -R ../../../../../../../app/gui/qt/theme app/gui/qt/
+
+../../../../../../../app/gui/qt/prune.rb app/server/ruby/vendor
+
+echo "build/Sonic Pi.app is now ready for signing, notarising and releasing..."
+
+cd "${SCRIPT_DIR}"
