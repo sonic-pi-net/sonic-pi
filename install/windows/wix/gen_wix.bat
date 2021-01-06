32e17573c253cc6e4f30f70746f66236d5fae4ee
Sam Aaron <samaaron@gmail.com>
2021-01-06 20:27:13 +0000

    Build - handle new config dir when building Windows installer

--- install/windows/wix/gen_wix.bat
+++ install/windows/wix/gen_wix.bat
@@ -5,4 +5,4 @@ cd ..
 heat dir etc -gg -g1 -f -sfrag -dr SonicPi -cg ETC -out etc.wxs
 heat dir app\gui -gg -g1 -v -t "wix\filter.xslt" -sreg -sfrag -dr AppDir -cg GUI -out gui.wxs
 heat dir app\server -gg -g1 -sreg -sfrag -dr AppDir -cg SERVER -out server.wxs
-
+heat dir app\config -gg -g1 -sreg -sfrag -dr AppDir -cg CONFIG -out config.wxs
