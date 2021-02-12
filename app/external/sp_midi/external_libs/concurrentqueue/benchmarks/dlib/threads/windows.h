5d52bd1313842cd2a76e66086c6ae0ed44b8e222
Luis Lloret <luislloret@gmail.com>
2021-02-12 20:27:52 +0000

    Removed old sp_midi manually synced dir

--- app/external/sp_midi/external_libs/concurrentqueue/benchmarks/dlib/threads/windows.h
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2003  Davis E. King (davis@dlib.net)
-// License: Boost Software License   See LICENSE.txt for the full license.
-#ifndef DLIB_THREADS_KERNEl_2_
-#include "threads_kernel_1.h"
-#endif
-
--- app/external/sp_midi/external_libs/concurrentqueue/tests/relacy/relacy/example/ws_deque2/stdafx.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#include "../../relacy/pch.hpp"
-#include "../../relacy/relacy_std.hpp"
-
