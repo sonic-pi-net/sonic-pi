b22644535f3a10efd87a7a81993052265b3fa9c9
Ethan Crawford <ethan_jc@hotmail.com>
2021-03-25 20:06:51 +0800

    Docs - add skeleton liveview app for documentation
    TODO: Add parsing code for TOML files, add live views etc

---
+++ etc/docs/test/docs_web/live/page_live_test.exs
@@ -0,0 +1,11 @@
+defmodule DocsWeb.PageLiveTest do
+  use DocsWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  test "disconnected and connected render", %{conn: conn} do
+    {:ok, page_live, disconnected_html} = live(conn, "/")
+    assert disconnected_html =~ "Welcome to Phoenix!"
+    assert render(page_live) =~ "Welcome to Phoenix!"
+  end
+end
---
+++ etc/docs/test/docs_web/views/layout_view_test.exs
@@ -0,0 +1,8 @@
+defmodule DocsWeb.LayoutViewTest do
+  use DocsWeb.ConnCase, async: true
+
+  # When testing helpers, you may want to import Phoenix.HTML and
+  # use functions such as safe_to_string() to convert the helper
+  # result into an HTML string.
+  # import Phoenix.HTML
+end
