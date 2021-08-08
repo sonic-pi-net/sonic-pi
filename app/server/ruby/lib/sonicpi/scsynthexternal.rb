6a6ec1c872acc889038d174f8d19f7cb0dbba657
b08x <rwpannick@gmail.com>
2021-08-08 09:35:11 -0400

    set jack to not autoconnect

--- app/server/ruby/lib/sonicpi/scsynthexternal.rb
+++ app/server/ruby/lib/sonicpi/scsynthexternal.rb
@@ -459,10 +459,10 @@ module SonicPi
 
       boot_and_wait(scsynth_path, scsynth_opts)
 
-      `jack_connect SuperCollider:out_1 system:playback_1`
-      `jack_connect SuperCollider:out_2 system:playback_2`
-      `jack_connect SuperCollider:in_1 system:capture_1`
-      `jack_connect SuperCollider:in_2 system:capture_2`
+      #`jack_connect SuperCollider:out_1 system:playback_1`
+      #`jack_connect SuperCollider:out_2 system:playback_2`
+      #`jack_connect SuperCollider:in_1 system:capture_1`
+      #`jack_connect SuperCollider:in_2 system:capture_2`
     end
   end
 end
