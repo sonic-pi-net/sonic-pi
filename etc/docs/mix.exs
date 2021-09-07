5d74273d196a5a194edb618c50ffcb6317b031d5
Ethan Crawford <ethan_jc@hotmail.com>
2021-09-07 11:54:32 +0800

    Upgrade Elixir and Node packages
    The Pheonix web app has now had its Elixir and Node packages upgraded, to allow
    for easier development of some parts, such as HEex view templates.

--- etc/docs/mix.exs
+++ etc/docs/mix.exs
@@ -33,17 +33,17 @@ defmodule Docs.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.8"},
-      {:phoenix_live_view, "~> 0.15.1"},
+      {:phoenix, "~> 1.5.12"},
+      {:phoenix_live_view, "~> 0.16.1"},
       {:floki, ">= 0.27.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.4"},
+      {:phoenix_html, "~> 3.0.2"},
+      {:phoenix_live_reload, "~> 1.3.3", only: :dev},
+      {:phoenix_live_dashboard, "~> 0.5"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
+      {:plug_cowboy, "~> 2.5.1"},
       {:toml, "~> 0.6.1"}
     ]
   end
