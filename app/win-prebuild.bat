8255711eef9774854629fa5aa5328f9fe760da4b
Sam Aaron <samaaron@gmail.com>
2021-09-07 22:36:26 +0100

    build - call mix commands in a cmd.
    Previously the script appeared to halt after calling mix local.hex --force. With this new approach, all the mix commands get executed.

--- app/win-prebuild.bat
+++ app/win-prebuild.bat
@@ -55,9 +55,9 @@ forfiles /p gui\qt\lang /s /m *.ts /c "cmd /c %QT_INSTALL_LOCATION%\bin\lrelease
 
 @echo Compiling Erlang/Elixir files...
 cd %~dp0\server\erlang\tau
-mix local.hex --force
-mix deps.get
-mix release
+cmd /c mix local.hex --force
+cmd /c mix deps.get
+cmd /c mix release
 
 cd %~dp0\server\erlang\tau
 copy /Y src\tau.app.src .\ebin\tau.app
