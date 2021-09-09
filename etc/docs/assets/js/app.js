a4edc16551180ee2022316a02531eabbcb4f6aef
Ethan Crawford <ethan_jc@hotmail.com>
2021-09-09 21:19:32 +0800

    Docs - switch to using bootstrap from npm

--- etc/docs/assets/js/app.js
+++ etc/docs/assets/js/app.js
@@ -1,6 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
+import 'bootstrap';
 import "../css/app.scss"
 
 // webpack automatically bundles all modules in your
