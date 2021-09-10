4389a8e22c8fb68d02e5ad44ac3b2681885407e1
Sam Aaron <samaaron@gmail.com>
2021-09-10 20:48:10 +0100

    Tau - specify lower bound for minimum supported elixir version

--- app/server/erlang/tau/mix.exs
+++ app/server/erlang/tau/mix.exs
@@ -5,7 +5,7 @@ defmodule Tau.MixProject do
     [
       app: :tau,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
