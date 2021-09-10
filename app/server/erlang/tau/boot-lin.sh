4dff168b746bba57b1d65903f94f1323b68dc5f2
Sam Aaron <samaaron@gmail.com>
2021-09-10 20:40:23 +0100

    Boot - tweak tau boot scripts

--- app/server/erlang/tau/boot-lin.sh
+++ app/server/erlang/tau/boot-lin.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e # Quit script on error
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+SCRIPT_DIR="$(dirname "$0")"
 cd ${SCRIPT_DIR}
-
 echo "Booting Sonic Pi on Linux..."
-
 TAU_ENABLED=$1 TAU_INTERNAL=$2 TAU_MIDI_ENABLED=$3 TAU_LINK_ENABLED=$4 TAU_IN_PORT=$5 TAU_API_PORT=$6 TAU_SPIDER_PORT=$7 TAU_DAEMON_PORT=$8 _build/"${MIX_ENV:-dev}"/rel/tau/bin/tau start
