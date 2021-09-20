sonic-pi-net/sonic-pi

commit 0982873c4120bfb2506f65a8b7615307fbef394f

Spider - tweak life cycle hooks
The life cycle hooks as they stand don't particularly sit well with the architecture, but do currently perform an important functionality and work. So, rather than making radical changes, just add a bit more safety, checking and logging. This stuff will likely just be removed after a sufficient amount of movement of functionality over to tau.

app/server/ruby/lib/sonicpi/lifecyclehooks.rb
@@ -11,8 +11,12 @@
 # notice is included.
 #++
 
+require_relative "util"
+
 module SonicPi
   class LifeCycleHooks
+
+    include Util
 
     # Life cycle hooks for jobs
 
@@ -23,65 +27,91 @@ module SonicPi
       @exited_blocks = []
       @all_completed_blocks = []
       @mut = Mutex.new
+      @started = false
+    end
+
+    def ensure_not_started!
+      if @started
+        raise StandardError "Cannot modify life cycle hooks once started"
+      end
     end
 
     def on_init(&blck)
       @mut.synchronize do
+        ensure_not_started!
         @started_blocks << blck
       end
     end
 
     def on_completed(&blck)
       @mut.synchronize do
+        ensure_not_started!
         @completed_blocks << blck
       end
     end
 
     def on_killed(&blck)
       @mut.synchronize do
+        ensure_not_started!
         @killed_blocks << blck
       end
     end
 
     def on_exit(&blck)
       @mut.synchronize do
+        ensure_not_started!
         @exited_blocks << blck
       end
     end
 
     def on_all_completed(&blck)
       @mut.synchronize do
+        ensure_not_started!
         @all_completed_blocks << blck
       end
     end
 
     def init(job_id, arg={})
-      @started_blocks.each do |b|
-        b.call(job_id, arg)
+      log "Lifecycle Hooks Init"
+      @mut.synchronize do
+        @started = true
+        @started_blocks.each do |b|
+          b.call(job_id, arg)
+        end
       end
     end
 
     def completed(job_id, arg={})
-      @completed_blocks.each do |b|
-        b.call(job_id, arg)
+      log "Lifecycle Hooks Completed"
+      @mut.synchronize do
+        @completed_blocks.each do |b|
+          b.call(job_id, arg)
+        end
       end
     end
 
     def all_completed(silent=false)
-      @all_completed_blocks.each do |b|
-        b.call(silent)
+      log "Lifecycle Hooks All Completed"
+      @mut.synchronize do
+        @all_completed_blocks.each do |b|
+          b.call(silent)
+        end
       end
     end
 
     def killed(job_id, arg={})
-      @killed_blocks.each do |b|
-        b.call(job_id, arg)
+      @mut.synchronize do
+        @killed_blocks.each do |b|
+          b.call(job_id, arg)
+        end
       end
     end
 
     def exit(job_id, arg={})
-      @exited_blocks.each do |b|
-        b.call(job_id, arg)
+      @mut.synchronize do
+        @exited_blocks.each do |b|
+          b.call(job_id, arg)
+        end
       end
     end
   end

commit 574ce2114f2f77e5544e5f5c396d60c4983322a4

Spider - fix zombied job bug
Commit 0982873c4120bfb2506f65a8b7615307fbef394f introduced a bug which made it possible to have zombied jobs still running within the GUI despite pressing the stop button. One of the callbacks must block and therefore stop the other callbacks being completed. As this code has worked for a long time, it feels better to simply revert to it rather than to start digging further to attempt to find and resolve the core issue - especially as all this stuff will likely be significantly reworked in the future. Also fix other (related) issues by doing a little more checking.

app/server/ruby/lib/sonicpi/jobs.rb
@@ -37,7 +37,12 @@ module SonicPi
       @mut.synchronize do
         job = @jobs.delete(id)
         if job
-          __system_thread_locals(job[:job]).get(:sonic_pi_local_spider_no_kill_mutex).synchronize do
+          mut =__system_thread_locals(job[:job]).get(:sonic_pi_local_spider_no_kill_mutex)
+          if mut
+            mut.synchronize do
+              job[:job].kill
+            end
+          else
             job[:job].kill
           end
         end

app/server/ruby/lib/sonicpi/lifecyclehooks.rb
@@ -73,45 +73,35 @@ module SonicPi
 
     def init(job_id, arg={})
       log "Lifecycle Hooks Init"
-      @mut.synchronize do
-        @started = true
-        @started_blocks.each do |b|
-          b.call(job_id, arg)
-        end
+      @started = true
+      @started_blocks.each do |b|
+        b.call(job_id, arg)
       end
     end
 
     def completed(job_id, arg={})
       log "Lifecycle Hooks Completed"
-      @mut.synchronize do
-        @completed_blocks.each do |b|
-          b.call(job_id, arg)
-        end
+      @completed_blocks.each do |b|
+        b.call(job_id, arg)
       end
     end
 
     def all_completed(silent=false)
       log "Lifecycle Hooks All Completed"
-      @mut.synchronize do
-        @all_completed_blocks.each do |b|
-          b.call(silent)
-        end
+      @all_completed_blocks.each do |b|
+        b.call(silent)
       end
     end
 
     def killed(job_id, arg={})
-      @mut.synchronize do
-        @killed_blocks.each do |b|
-          b.call(job_id, arg)
-        end
+      @killed_blocks.each do |b|
+        b.call(job_id, arg)
       end
     end
 
     def exit(job_id, arg={})
-      @mut.synchronize do
-        @exited_blocks.each do |b|
-          b.call(job_id, arg)
-        end
+      @exited_blocks.each do |b|
+        b.call(job_id, arg)
       end
     end
   end

app/server/ruby/lib/sonicpi/runtime.rb
@@ -850,6 +850,7 @@ module SonicPi
     end
 
     def __spider_eval(code, info={})
+      now = Time.now.freeze
       # skip __nosave lines for error reporting
       firstline = 1
       firstline -= code.lines.to_a.take_while{|l| l.include? "#__nosave__"}.count
@@ -878,8 +879,7 @@ module SonicPi
           __set_default_user_thread_locals!
           __msg_queue.push({type: :job, jobid: id, action: :start, jobinfo: info})
           @life_hooks.init(id, {:thread => Thread.current})
-          now = Time.now.freeze
-          start_t_prom.deliver! now
+
           ## fix this for link
           __reset_spider_time_and_beat!
           if num_running_jobs == 1
@@ -893,6 +893,8 @@ module SonicPi
           job_in_thread = in_thread seed: 0 do
             eval(code, nil, info[:workspace], firstline)
           end
+
+          start_t_prom.deliver! now
           __schedule_delayed_blocks_and_messages!
         rescue Stop => e
           __no_kill_block do
@@ -937,7 +939,7 @@ module SonicPi
         Thread.current.priority = -10
         __system_thread_locals.set_local(:sonic_pi_local_thread_group, "job-#{id}-GC")
         job.join
-        __system_thread_locals(job_in_thread).get(:sonic_pi_local_spider_subthread_empty).get
+        __system_thread_locals(job_in_thread).get(:sonic_pi_local_spider_subthread_empty).get if job_in_thread
 
         # wait until all synths are dead
         @life_hooks.completed(id)