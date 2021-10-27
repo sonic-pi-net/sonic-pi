33eb5943e33f919f7b9093770679992ce28b58bb
Sam Aaron <samaaron@gmail.com>
2021-10-27 19:06:37 +0100

    Build - clean BEAM distribution too

--- app/win-clean.bat
+++ app/win-clean.bat
@@ -6,4 +6,7 @@ rmdir build /s /q
 @echo Cleaning out external\build dir....
 rmdir external\build /s /q
 
+@echo Cleaning out BEAM distribution....
+rmdir server\beam\tau\_build /s /q
+
 @echo Cleaning completed
