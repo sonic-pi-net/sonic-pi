05df38fc92762349a6f3c6605cbeb2403163df31
Sam Aaron <samaaron@gmail.com>
2021-11-05 20:19:48 +0000

    Build - correctly pass no_imgui flag through from pi-build-all to linux-prebuild script

--- app/pi-prebuild.sh
+++ app/pi-prebuild.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WORKING_DIR="$(pwd)"
 
-# Use generic linux prebuild script
-VCPKG_FORCE_SYSTEM_BINARIES=1 "${SCRIPT_DIR}"/linux-prebuild.sh
+while getopts ":n" opt; do
+  case $opt in
+    n)
+      no_imgui=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ $no_imgui == true ]
+then
+    VCPKG_FORCE_SYSTEM_BINARIES=1 "${SCRIPT_DIR}"/linux-prebuild.sh -n
+else
+    VCPKG_FORCE_SYSTEM_BINARIES=1 "${SCRIPT_DIR}"/linux-prebuild.sh
+fi
+
+# Restore working directory as it was prior to this script running...
+cd "${WORKING_DIR}"
