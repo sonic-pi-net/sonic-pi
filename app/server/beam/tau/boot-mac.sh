99bb397530ed5b21fe97986de47e6a5db102738f
Sam Aaron <samaaron@gmail.com>
2021-11-09 21:16:55 +0000

    Tau - no longer mess with stdin/out/err on boot
    This was an attempt to find a way to make sure the daemon correctly shut things down if tau crashed in a specific way which was seemingly blocked by io pipes with child processes.
    
    I couldn't seem to fix it using this approach, so there's no need for this exec magic stuff any more.

--- app/server/beam/tau/boot-mac.sh
+++ app/server/beam/tau/boot-mac.sh
@@ -3,14 +3,6 @@ set -e # Quit script on error
 SCRIPT_DIR="$(dirname "$0")"
 cd "${SCRIPT_DIR}"
 
-# Close standard output file descriptor
-exec 1<&-
-# Close standard error file descriptor
-exec 2<&-
-
-# Close standard in file descriptor
-exec <&-
-
 echo "Booting Sonic Pi on macOS..."
 
 TAU_CUES_ON=$1 TAU_OSC_IN_UDP_LOOPBACK_RESTRICTED=$2 TAU_MIDI_ON=$3 TAU_LINK_ON=$4 TAU_OSC_IN_UDP_PORT=$5 TAU_API_PORT=$6 TAU_SPIDER_PORT=$7 TAU_DAEMON_PORT=$8 TAU_LOG_PATH=$9 TAU_MIDI_ENABLED=true TAU_LINK_ENABLED=true _build/"${MIX_ENV:-prod}"/rel/tau/bin/tau start > log/tau_stdout.log 2>&1
