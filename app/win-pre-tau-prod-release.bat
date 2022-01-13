bf5823dfb7f80e0c90eb1371d42181524a9e62af
Sam Aaron <samaaron@gmail.com>
2022-01-13 10:19:50 +0000

    Build - separate out win-prebuild scripts
    Likely should do this for lin, mac and pi prebuild scripts too

---
+++ app/win-pre-tau-prod-release.bat
@@ -0,0 +1,18 @@
+cd %~dp0
+
+@echo Compiling Erlang/Elixir files...
+
+cd %~dp0\server\beam\tau
+
+SET MIX_ENV=prod
+
+rem Changes here should be matched in app\server\beam\tau\boot-win.bat
+cmd /c mix local.hex --force
+cmd /c mix local.rebar --force
+cmd /c mix deps.get
+cmd /c mix assets.deploy
+
+cmd /c mix release --overwrite
+
+cd %~dp0\server\beam\tau
+copy /Y src\tau.app.src .\ebin\tau.app
---
+++ app/win-pre-translations.bat
@@ -0,0 +1,11 @@
+cd %~dp0
+
+@echo Translating tutorial...
+server\native\ruby\bin\ruby server/ruby/bin/i18n-tool.rb -t
+
+@echo Generating docs for the Qt GUI...
+copy /Y gui\qt\utils\ruby_help.tmpl gui\qt\utils\ruby_help.h
+server\native\ruby\bin\ruby server/ruby/bin/qt-doc.rb -o gui\qt\utils/ruby_help.h
+
+@echo Updating GUI translation files...
+forfiles /p gui\qt\lang /s /m *.ts /c "cmd /c %QT_INSTALL_LOCATION%\bin\lrelease.exe @file"
