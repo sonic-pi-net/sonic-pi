fc918c39289f9f7064e8511da669710e63192b95
Sam Aaron <samaaron@gmail.com>
2022-02-02 23:20:36 +0000

    Build - pull in Sdl2[x11] when building with linux
    This enables things to link on Ubuntu 21 - need to test on other linux systems.

--- app/linux-prebuild.sh
+++ app/linux-prebuild.sh
@@ -38,7 +38,7 @@ if [ "$no_imgui" == true ]
 then
     ./vcpkg install kissfft crossguid platform-folders reproc catch2 --recurse
 else
-    ./vcpkg install kissfft fmt crossguid sdl2 gl3w reproc gsl-lite concurrentqueue platform-folders catch2 --recurse
+    ./vcpkg install kissfft fmt crossguid sdl2[x11] gl3w reproc gsl-lite concurrentqueue platform-folders catch2 --recurse
 fi
 
 cd "${SCRIPT_DIR}"
