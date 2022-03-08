d4cdbb1ad6eff4136c45a0db4f56c342dbfc2b49
Sam Aaron <samaaron@gmail.com>
2022-03-08 13:57:26 +0000

    Tau - bump esbuild version number to 0.14.25

--- app/server/beam/tau/config/config.exs
+++ app/server/beam/tau/config/config.exs
@@ -20,7 +20,7 @@ config :swoosh, :api_client, false
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.14.0",
+  version: "0.14.25",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
