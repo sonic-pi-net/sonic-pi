c07961cc4c23fcb8db8f5536875c1cafb16db604
Sam Aaron <samaaron@gmail.com>
2022-03-16 22:35:28 +0000

    Tau - remove swoosh routes

--- app/server/beam/tau/lib/tau_web/router.ex
+++ app/server/beam/tau/lib/tau_web/router.ex
@@ -44,16 +44,4 @@ defmodule TauWeb.Router do
           ]
     end
   end
-
-  # Enables the Swoosh mailbox preview in development.
-  #
-  # Note that preview only shows emails that were sent by the same
-  # node running the Phoenix server.
-  if Mix.env() == :dev do
-    scope "/dev" do
-      pipe_through :browser
-
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
-  end
 end
