de8014a54a6efa0250ec09a50e861c93f698ff8e
Lily Foster <lily@lily.flowers>
2022-05-06 18:16:37 -0400

    Build - overhaul linux build scripts to allow offline or vcpkg-less builds

--- app/linux-prebuild.sh
+++ app/linux-prebuild.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
-
 set -e # Quit script on error
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORKING_DIR="$(pwd)"
 
-cd "${SCRIPT_DIR}"
+args=("$@")
+system_libs=false
 
-while getopts ":n" opt; do
-  case $opt in
-    n)
-      no_imgui=true
-      echo "Running prebuild script without support for IMGUI-based GUI"
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-  esac
+# extract options and their arguments into variables.
+while [ -n "$1" ]; do
+    case "$1" in
+        -c|--config)
+            shift 2
+            ;;
+        -n|--no-imgui)
+            shift
+            ;;
+        -s|--system-libs|-o|--offline-build)
+            system_libs=true
+            shift
+            ;;
+        --) shift ; break ;;
+        *) echo "Invalid argument: $1" ; exit 1 ;;
+    esac
 done
 
 cd "${SCRIPT_DIR}"
 
-if [ "$no_imgui" == true ]
-then
-  "${SCRIPT_DIR}"/linux-pre-vcpkg.sh -n
-else
-  "${SCRIPT_DIR}"/linux-pre-vcpkg.sh
+if [ ! "$system_libs" == true ]; then
+  "${SCRIPT_DIR}"/linux-pre-vcpkg.sh "${args[@]}"
 fi
 
 # Build external dependencies and copy to build tree
@@ -35,9 +37,9 @@ echo "Building external binary dependencies..."
 echo "Compiling native ruby extensions..."
 ruby "${SCRIPT_DIR}"/server/ruby/bin/compile-extensions.rb
 
-"${SCRIPT_DIR}"/linux-pre-translations.sh
-"${SCRIPT_DIR}"/linux-pre-copy-binaries.sh
-"${SCRIPT_DIR}"/linux-pre-tau-prod-release.sh
+"${SCRIPT_DIR}"/linux-pre-translations.sh "${args[@]}"
+"${SCRIPT_DIR}"/linux-pre-copy-binaries.sh "${args[@]}"
+"${SCRIPT_DIR}"/linux-pre-tau-prod-release.sh "${args[@]}"
 
 # Restore working directory as it was prior to this script running...
 cd "${WORKING_DIR}"
