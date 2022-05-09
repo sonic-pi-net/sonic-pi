117ec45fb49295f273db234a7df10a25a2ed1c33
SunderB <20426079+SunderB@users.noreply.github.com>
2022-05-09 17:55:12 +0100

    Replace `*_build_externals` and `*-pre-copy-binaries` bash scripts with CMake
    The overall build process is unchanged, apart from that sp_link, sp_midi
    and aubio are now built when running cmake --build.

--- app/win-clean.bat
+++ app/win-clean.bat
@@ -5,9 +5,6 @@ cd %~dp0
 @echo Cleaning out build dir....
 rmdir build /s /q
 
-@echo Cleaning out external\build dir....
-rmdir external\build /s /q
-
 @echo Cleaning out BEAM distribution....
 rmdir server\beam\tau\_build /s /q
 del server\beam\tau\priv\*.so server\beam\tau\priv\*.dylib server\beam\tau\priv\*.dll /s /q
