a584f32b5287b71bc06f16fd7086443a35fe7a9f
SunderB <20426079+SunderB@users.noreply.github.com>
2022-05-09 17:55:12 +0100

    Replace `*_build_externals` and `*-pre-copy-binaries` bash scripts with CMake
    The overall build process is unchanged, apart from that sp_link, sp_midi
    and aubio are now built when running cmake --build.

--- app/external/win_x86_build_externals.bat
+++
@@ -1,6 +0,0 @@
-cd %~dp0
-mkdir build_x86 > nul
-cd build_x86
-cmake -G "Visual Studio 16 2019" -A Win32 -D ERLANG_INCLUDE_PATH=%~dp0\..\..\prebuilt\windows\headers\erlang\ ..\
-cmake --build . --config Release
-cd %CURRENT_DIR%
