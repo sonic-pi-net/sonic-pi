dbb132180fd37ab5057b8da978aff5f713c42e25
Sam Aaron <samaaron@gmail.com>
2022-06-13 15:39:03 +0100

    Build - fix minor syntax error

--- app/win-clean.bat
+++ app/win-clean.bat
@@ -11,7 +11,7 @@ rmdir external\build /s /q
 @echo Cleaning out BEAM distribution....
 rmdir server\beam\tau\_build /s /q
 del server\beam\tau\priv\*.so server\beam\tau\priv\*.dylib server\beam\tau\priv\*.dll /s /q
-if %MIX_ENV%=="dev" (
+if "%MIX_ENV%"=="dev" (
 	rmdir server\beam\tau\priv\static\assets /s /q
 	del server\beam\tau\priv\static\cache_manifest.json /s /q
 	del server\beam\tau\priv\static\*.gz /s /q
