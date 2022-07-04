7906d9e815a7e95d883a2f4108f0188787aee7ff
Sam Aaron <samaaron@gmail.com>
2022-07-04 15:15:51 +0100

    Spider - use :link bpm mode by default
    Given that the default BPM for link is 60, this should therefore not be a breaking change. Rather it enables users to modify the BPM of any of their default threads using the new metronome GUI interface. Previous behaviour can be explicitly obtained via `use_bpm 60` - this will detach the current thread from the link metronome and give it a static bpm of 60.

--- app/server/ruby/lib/sonicpi/runtime.rb
+++ app/server/ruby/lib/sonicpi/runtime.rb
@@ -121,8 +121,8 @@ module SonicPi
 
     def __init_spider_time_and_beat!
       t = Time.now.to_f
-      __change_spider_time_and_beat!(t, 0)
       __system_thread_locals.set :sonic_pi_spider_start_time, t
+      __change_spider_bpm_time_and_beat!(:link, t, 0)
     end
 
     def __change_spider_bpm_time_and_beat_to_next_link_phase(phase, quantum)
@@ -830,7 +830,7 @@ module SonicPi
     end
 
     def __set_default_user_thread_locals!
-      __change_spider_bpm_time_and_beat!(60.0, __get_spider_time, __get_spider_beat)
+
       __thread_locals.set :sonic_pi_spider_arg_bpm_scaling, true
       __thread_locals.set :sonic_pi_spider_new_thread_random_gen_idx, 0
       __system_thread_locals.set(:sonic_pi_spider_thread_priority, 0)
