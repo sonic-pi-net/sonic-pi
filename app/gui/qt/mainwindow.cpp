6efe57e1c4a3e721f464f7c84c4f133673347e81
Sam Aaron <samaaron@gmail.com>
2022-07-10 22:19:24 +0100

    Release - bump version to 4.0.1
    In preparation of an upcoming bugfix release...

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -130,7 +130,7 @@ MainWindow::MainWindow(QApplication& app, QSplashScreen* splash)
     show_rec_icon_a = false;
     restoreDocPane = false;
     focusMode = false;
-    version = "4.0.0";
+    version = "4.0.1";
     latest_version = "";
     version_num = 0;
     latest_version_num = 0;
