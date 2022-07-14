91312bfd4da62fc7232249b483f57472df42ac3c
Lily Foster <lily@lily.flowers>
2022-07-14 11:28:34 -0400

    Build - use vendored aubio when USE_SYSTEM_LIBS=OFF

--- app/external/aubio/config.h
+++ app/external/aubio/config.h
@@ -2,13 +2,13 @@
 
 #define HAVE_STDLIB_H 1
 #define HAVE_STDIO_H 1
-#define HAVE_STRING_H 1
 #define HAVE_MATH_H 1
-#define HAVE_ERRNO_H 1
+#define HAVE_STRING_H 1
 #define HAVE_LIMITS_H 1
 #define HAVE_STDARG_H 1
-#define HAVE_MEMCPY_HACKS 1
+#define HAVE_ERRNO_H 1
 #define HAVE_C99_VARARGS_MACROS 1
+#define HAVE_MEMCPY_HACKS 1
 
 #ifdef WIN32
 #define HAVE_WIN_HACKS 1
