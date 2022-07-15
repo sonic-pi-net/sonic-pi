5cc2d62d76850924d2b4359749089d0fe5d024ab
Sam Aaron <samaaron@gmail.com>
2022-07-15 13:30:59 +0100

    Revert "Build - also remove external/build dir when cleaning"
    This reverts commit 16dec61e1ab8e239a6714160348cdf53d4dafc72.
    
    (The external/build dir is no longer created as part of the cmake process - I just happened to oddly have an old one hanging around)

--- app/linux-clean.sh
+++ app/linux-clean.sh
@@ -7,9 +7,6 @@ cd "${SCRIPT_DIR}"
 echo "Cleaning out build dir...."
 rm -rf build
 
-echo "Cleaning out external build dir..."
-rm -rf external/build
-
 echo "Cleaning out BEAM distribution..."
 rm -rf server/beam/tau/_build
 rm -f server/beam/tau/priv/*.{so,dylib,dll}
