a596622ea50fafffc1f81287b2cd234cb9a758bc
Sam Aaron <samaaron@gmail.com>
2022-08-10 13:04:10 +0100

    Tau - livelog - use md5 hash for html ids
    Previously we were concatenating the timestamp and full log message string

--- app/server/beam/tau/lib/tau_web/live/log_live.ex
+++ app/server/beam/tau/lib/tau_web/live/log_live.ex
@@ -13,9 +13,10 @@ defmodule TauWeb.LogLive do
 
   @impl true
   def handle_info(%{topic: "room:logs", payload: state}, socket) do
-
+    hash = :crypto.hash(:md5, Kernel.inspect(state)) |> Base.encode16
+    {level, ts, msg, md} = state
     # {:noreply, assign(socket, logs: [socket.assigns.logs | inspect(state)])}
-    {:noreply, assign(socket, logs: [state])}
+    {:noreply, assign(socket, logs: [{level, ts, msg, md, hash}])}
   end
 
 
@@ -25,10 +26,10 @@ defmodule TauWeb.LogLive do
     <.container max_width="full">
       <div id="log-messages" phx-update="prepend" class="bg-black text-white flex flex-col space-y-0 ">
 
-        <%= for {level, ts, msg, _md} <- @logs do %>
-          <pre class="whitespace-pre" id={inspect(ts <> msg)}>
-<code class={"#{tw_ts}"}><%=ts%></code> <code class={"#{tw_level(level)}"}><%=pad_level(level)%></code> <block class={"#{tw_msg}"}><%=pad_newlines(msg)%></block>
-          </pre>
+        <%= for {level, ts, msg, _md, hash} <- @logs do %>
+<pre class="whitespace-pre pa0s" id={hash}>
+  <code class={"#{tw_ts}"}><%=ts%></code> <code class={"#{tw_level(level)}"}><%=pad_level(level)%></code> <block class={"#{tw_msg}"}><%=pad_newlines(msg)%></block>
+</pre>
         <% end %>
 
       </div>
