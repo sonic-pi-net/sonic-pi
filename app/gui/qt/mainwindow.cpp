48d180d305dd870a4d1a1cda955bd98364836480
Sam Aaron <samaaron@gmail.com>
2022-09-12 13:32:32 +0100

    Version - label v4.2.0-beta-2

--- app/gui/qt/mainwindow.cpp
+++ app/gui/qt/mainwindow.cpp
@@ -130,7 +130,7 @@ MainWindow::MainWindow(QApplication& app, QSplashScreen* splash)
     show_rec_icon_a = false;
     restoreDocPane = false;
     focusMode = false;
-    version = "4.2.0-beta-1";
+    version = "4.2.0-beta-2";
     latest_version = "";
     version_num = 0;
     latest_version_num = 0;
