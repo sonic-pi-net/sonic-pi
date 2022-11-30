7faf22de3b7e50124078289c478d782dce466688
Benson Muite <bkmgit@users.noreply.github.com>
2022-11-30 11:16:45 +0300

    Rubygem interception not required

--- app/server/ruby/bin/compile-extensions.rb
+++ app/server/ruby/bin/compile-extensions.rb
@@ -66,10 +66,8 @@ native_ext_dirs.each do |ext_dir|
   end
   puts "Compiling native extension in #{ext_dir}"
   Dir.chdir(ext_dir) do
-    `"#{RbConfig.ruby}" extconf.rb`
-    if not (ext_dir.include? "interception")   
-      `make clean`
-    end
+    `"#{RbConfig.ruby}" extconf.rb`   
+    `make clean`
     `make`
   end
 
