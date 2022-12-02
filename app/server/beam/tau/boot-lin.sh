81faf1af48eca2c469fb2a0f1bd9f6c103dbb8d5
Sam Aaron <samaaron@gmail.com>
2022-12-02 09:24:34 +0000

    Tau - improve boot scripts

--- app/server/beam/tau/boot-lin.sh
+++ app/server/beam/tau/boot-lin.sh
@@ -7,13 +7,24 @@ echo "Booting Tau on Linux..."
 
 if [ $TAU_ENV = "prod" ]
 then
+  # Ensure prod env has been setup with:
+  # export MIX_ENV=dev
+  # mix tau.release
+
+  export MIX_ENV=prod
   _build/prod/rel/tau/bin/tau start > /dev/null 2>&1
 elif [ $TAU_ENV = "dev" ]
 then
+  # Ensure prod env has been setup with:
+  # export MIX_ENV=dev
+  # mix setup.dev
+
+  export MIX_ENV=dev
   mix assets.deploy.dev
   mix run --no-halt > log/tau_stdout.log 2>&1
 elif [ $TAU_ENV = "test" ]
 then
+  export MIX_ENV=test
   export TAU_MIDI_ENABLED=false
   export TAU_LINK_ENABLED=false
   mix run --no-halt > log/tau_stdout.log 2>&1
--- app/server/beam/tau/boot-win.bat
+++ app/server/beam/tau/boot-win.bat
@@ -1,21 +1,44 @@
 cd %~dp0
+@echo off
 
-rem Ensure release has been created with:
-rem   mix release
+echo Booting Tau on Windows...
 
-@echo Booting Tau on Windows...
+IF /I "%TAU_ENV%" == "prod" (
+  echo Tau mode: prod
+  set MIX_ENV=prod
 
-IF "%TAU_ENV%" == "prod" (
+  rem Ensure release has been created with:
+  rem SET MIX_ENV=prod
+  rem mix tau.release
+
   _build\prod\rel\tau\bin\tau start > NUL 2>&1
+  goto Exit
 )
 
-IF "%TAU_ENV%" == "dev" (
+IF /I "%TAU_ENV%" == "dev" (
+
+  echo Tau mode: dev
+  set MIX_ENV=dev
+
+  rem Ensure dev env has been setup with:
+  rem SET MIX_ENV=dev
+  rem mix setup.dev
+
   mix assets.deploy.dev
   mix run --no-halt > log\tau_stdout.log 2>&1
+  goto Exit
 )
 
-IF "%TAU_ENV%" == "test" (
+IF /I "%TAU_ENV%" == "test" (
+  echo Tau mode: test
+  set MIX_ENV=test
+
   set TAU_MIDI_ENABLED=false
   set TAU_LINK_ENABLED=false
   mix run --no-halt > log\tau_stdout.log 2>&1
+  goto Exit
 )
+
+echo Unknown TAU_ENV environment variable value. Expected one of prod, dev or test.
+
+:Exit
