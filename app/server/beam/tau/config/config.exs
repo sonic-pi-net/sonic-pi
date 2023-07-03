6f5defa81ef6f5e1883a4f42cfb4f88f812fb6cd
Sam Aaron <samaaron@gmail.com>
2023-07-03 09:18:59 +0100

    Tau - bump esbuild version to 0.18.11

--- app/server/beam/tau/config/config.exs
+++ app/server/beam/tau/config/config.exs
@@ -25,7 +25,7 @@ config :logger, :console,
 if config_env() != :test do
   # Configure esbuild (the version is required)
   config :esbuild,
-    version: "0.17.19",
+    version: "0.18.11",
     default: [
       args:
         ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/* --loader:.js=jsx),
