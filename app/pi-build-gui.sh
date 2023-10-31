6d5112f0911e843a0ff145d9e2ee99c7c33afd04
Robin Newman <robin.newman@gmail.com>
2023-10-31 23:31:40 +0000

    Add qt install location
    Need to set an environmental variable so Qt6 can be found when the gui is built.

--- app/pi-build-gui.sh
+++ app/pi-build-gui.sh
@@ -3,7 +3,7 @@ set -e # Quit script on error
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORKING_DIR="$(pwd)"
 
-"${SCRIPT_DIR}"/linux-build-gui.sh "$@"
+QT_INSTALL_LOCATION=/usr/lib/qt6 "${SCRIPT_DIR}"/linux-build-gui.sh "$@"
 
 # Restore working directory as it was prior to this script running...
 cd "${WORKING_DIR}"
