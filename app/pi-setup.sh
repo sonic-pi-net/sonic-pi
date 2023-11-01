f478e7ab666c0c8440bc2dfa02b7a7f4d25ddb0c
Sam Aaron <samaaron@gmail.com>
2023-11-01 08:42:02 +0000

    Build - update deb dependencies for Pi

--- app/pi-setup.sh
+++ app/pi-setup.sh
@@ -3,7 +3,4 @@
 echo "Fetching dependencies via apt..."
 
 sudo apt-get update
-sudo apt-get install -y curl build-essential libssl-dev git ruby-dev elixir erlang-dev erlang-xmerl qttools5-dev qttools5-dev-tools libqt5svg5-dev supercollider-server sc3-plugins-server alsa-utils jackd2 libjack-jackd2-0 pulseaudio-module-jack libasound2-dev clang cmake ninja-build
-
-# Dependencies for building Erlang/Elixir via asdf...
-sudo apt-get -y install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop
+sudo apt-get install -y build-essential git libssl-dev ruby-dev elixir erlang-dev erlang-xmerl qt6-tools-dev qt6-tools-dev-tools libqt6svg6-dev libqt6opengl6-dev supercollider-server sc3-plugins-server alsa-utils  libasound2-dev cmake ninja-build pipewire-jack libspa-0.2-jack qt6-wayland libwayland-dev libxkbcommon-dev libegl1-mesa-dev libx11-dev libxft-dev libxext-dev qpwgraph compton
