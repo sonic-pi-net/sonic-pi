6d302bccb31ea2dc76396dcbe747f03d55dbcf7f
Sam Aaron <samaaron@gmail.com>
2024-02-25 12:08:56 +0000

    Build - knock linux vcpkg back yet another version

--- app/linux-pre-vcpkg.sh
+++ app/linux-pre-vcpkg.sh
@@ -30,7 +30,7 @@ cd "${SCRIPT_DIR}"
 # Build vcpkg
 if [ ! -d "vcpkg" ]; then
     echo "Cloning vcpkg"
-    git clone --depth 1 --branch "${VCPKG_BRANCH:-2024.01.12}" https://github.com/microsoft/vcpkg.git vcpkg
+    git clone --depth 1 --branch "${VCPKG_BRANCH:-2023.12.12}" https://github.com/microsoft/vcpkg.git vcpkg
 fi
 
 export VCPKG_ROOT="$SCRIPT_DIR/vcpkg"
