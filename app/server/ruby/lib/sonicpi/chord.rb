0adacc367d1f5607392526ab3b6f4148c1278847
defcom5-rockchip <defcom5-rockchip@users.noreply.github.com>
2026-06-14 18:09:34 -0700

    Also add :minor6 and :major6 long-form aliases
    Complete the long-form set: :minor6 (== :m6) and :major6 (== "6") so every
    common chord with a short name also has the expected long name.

--- app/server/ruby/lib/sonicpi/chord.rb
+++ app/server/ruby/lib/sonicpi/chord.rb
@@ -98,6 +98,8 @@ module SonicPi
         "m7b5"           => halfdim,
         "m7-5"           => halfdim,
         # long-form aliases for extended chords (consistency with :minor7 / :major7)
+        :minor6          => [0, 3, 7, 9],
+        :major6          => [0, 4, 7, 9],
         :minor9          => [0, 3, 7, 10, 14],
         :major9          => [0, 4, 7, 11, 14],
         :minor11         => [0, 3, 7, 10, 14, 17],
