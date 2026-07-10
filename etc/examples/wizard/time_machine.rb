e6de04f9f5aaaafe6ab11f47b5fb044189e94225
Sam Aaron <samaaron@gmail.com>
2026-07-10 22:52:37 +0100

    Examples - update time_machine

--- etc/examples/wizard/time_machine.rb
+++ etc/examples/wizard/time_machine.rb
@@ -1,25 +1,24 @@
 # Coded by Sam Aaron
 
-use_debug false
-
 live_loop :time do
-  synth :tb303, release: 8, note: :e1, cutoff: (range 90, 60, -10).tick
+  synth :tb303, release: 8, note: :e1, cutoff: (range 90, 60, -10).tick, amp: 0.8
   sleep 8
 end
 
 live_loop :machine do
-  sample :loop_garzul, rate: (knit 1, 3, -1, 1).tick
+  sample :loop_garzul, rate: (knit 1, 3, -1, 1).tick, hpf: 80
   sleep 8
 end
 
 live_loop :vortex, auto_cue: false do
   use_synth [:pulse, :beep].choose
-  sleep 0.125 / 2
-  play scale(:e1, :minor_pentatonic).tick, attack: 0.125, release: 0, amp: 2, cutoff: (ring 70, 90, 100, 130).look
-  sleep 0.125 / 2
+  with_fx :hpf, cutoff: 42, amp: 0.8 do
+    play scale(:e1, :minor_pentatonic).tick, release: 0.1, amp: 2, cutoff: (ring 70, 90, 100, 130).choose
+  end
+  sleep 0.125
 end
 
 live_loop :moon_bass, auto_cue: false do
-  sample :bd_haus, amp: 1.5
+  sample :bd_haus, amp: 1.5, lpf: 110
   sleep 0.5
 end
